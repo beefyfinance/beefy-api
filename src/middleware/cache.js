ae36489de7dc982b5b210716b2adb84b880d3f36
superbeefyboy <superbeefyboy@pm.me>
2020-10-04 16:54:56 -0300

    prettier

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -6,22 +6,22 @@ async function cache(ctx, next) {
   if (ctx.method !== 'GET') {
     return await next();
   }
-  
+
   const cached = ctx.cache[ctx.url];
 
-  if (cached !== undefined && cached.ts && (cached.ts + TTL * 1000) > Date.now()) {
+  if (cached !== undefined && cached.ts && cached.ts + TTL * 1000 > Date.now()) {
     ctx.status = 200;
     ctx.body = cached.body;
     return;
   }
-  
+
   await next();
 
   ctx.set('Cache-Control', `public, max-age=${TTL}`);
   ctx.cache[ctx.url] = {
     ts: Date.now(),
     body: ctx.body,
-  } 
+  };
 }
 
 module.exports = cache;
