72813596b087eb686a8f6497d3e3bc4a1c8eb48f
jpopxfile <raynold.sim@gmail.com>
2020-12-10 00:19:55 +0900

    add daily earnings apy

--- src/router.js
+++ src/router.js
@@ -7,10 +7,12 @@ const noop = require('./api/noop');
 const stats = require('./api/stats');
 const price = require('./api/price');
 const proxy = require('./api/proxy');
+const gov = require('./api/stats/gov');
 
 router.get('/apy', stats.apy);
 router.get('/pancake/price', proxy.pancake);
 router.get('/pancake/lps', price.lpPrices);
+router.get('/earnings', gov.dailyEarn);
 router.get('/', noop);
 
 module.exports = router;
