f479f52654b2915eb4770dfb9e677c2bfb55da3d
superbeefyboy <71347549+superbeefyboy@users.noreply.github.com>
2020-12-11 12:18:18 -0300

    Update getHolders.js
    minor fix, removed log

--- src/utils/getHolders.js
+++ src/utils/getHolders.js
@@ -4,12 +4,9 @@ const getHolders = async () => {
   let response = await axios.get('https://beefy-vote-api.herokuapp.com/api/0xCa3F508B8e4Dd382eE878A314789373D80A5190A/snapshot/holders');
 
   const ipfsHash = response.data["holders"];
-  console.log(ipfsHash)
-
   response = await axios.get(`https://ipfs.io/ipfs/${ipfsHash}`);
 
   let holders = response.data;
-
   holders = Object.keys(holders).filter(key => parseFloat(holders[key]) > 0 );
 
   return {"holderCount" : holders.length};
