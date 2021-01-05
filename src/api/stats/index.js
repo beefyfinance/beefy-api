9db57e297a3d55883d935f813ce1b202b0f31ad8
sirbeefalot <sirbeefalot@pm.me>
2021-01-05 13:08:06 -0300

    Remove EOL pools

--- src/api/stats/index.js
+++ src/api/stats/index.js
@@ -9,10 +9,6 @@ async function apy(ctx) {
     ctx.request.socket.setTimeout(TIMEOUT);
     let apys = await getApys();
 
-    apys['cake-syrup-ctk'] = 0;
-    apys['cake-syrup-twt'] = 0;
-    apys['cake-syrup-inj'] = 0;
-
     ctx.status = 200;
     ctx.body = apys;
   } catch (err) {
