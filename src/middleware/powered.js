8fb36dff6121fccbbce9fb6c5c9236f46cd163f6
superbeefyboy <superbeefyboy@pm.me>
2021-01-19 01:08:44 -0300

    wip

--- src/middleware/powered.js
+++ src/middleware/powered.js
@@ -1,7 +1,7 @@
 'use strict';
 
 async function rt(ctx, next) {
-  console.log('origin', ctx.request.origin);
+  console.log('origin', ctx.request.headers['X-Saved-Origin']);
   await next();
   ctx.set('X-Powered-By', 'moo!');
 }
