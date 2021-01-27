491ee78f96e8f9302f7f4f1750c7f2385c2e8200
shibacrypt <>
2021-01-27 10:04:02 +0900

    Extract cmc env vars

--- constants.js
+++ constants.js
@@ -1,6 +1,8 @@
 const DEFAULT_BSC_RPC = 'https://bsc-dataseed.binance.org'
 const BSC_RPC = process.env.BSC_RPC_3 || process.env.BSC_RPC_2 || process.env.BSC_RPC || DEFAULT_BSC_RPC
 
+const REWARDER_PRIVATE_KEY = process.env.REWARDER_PRIVATE_KEY
+
 const BASE_HPY = process.env.BASE_HPY
 const HOURLY_HPY = process.env.HOURLY_HPY
 const DAILY_HPY = process.env.DAILY_HPY
@@ -11,7 +13,7 @@ const FORTUBE_REQ_MARKETS = process.env.FORTUBE_REQ_MARKETS
 const FORTUBE_API_TOKEN = process.env.FORTUBE_API_TOKEN
 
 module.exports = { 
-  BSC_RPC, 
+  BSC_RPC, REWARDER_PRIVATE_KEY,
   BASE_HPY, HOURLY_HPY, DAILY_HPY, WEEKLY_HPY, 
   FORTUBE_REQ_TOKENS, FORTUBE_REQ_MARKETS, FORTUBE_API_TOKEN
 };
