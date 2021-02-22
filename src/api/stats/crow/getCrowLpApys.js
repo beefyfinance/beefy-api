262cea185d7b067ca13b0b682724462a7fc222a2
superbeefyboy <superbeefyboy@pm.me>
2021-02-22 13:54:57 -0300

    crow + bts/btd restored. thugs api down

--- src/api/stats/crow/getCrowLpApys.js
+++ src/api/stats/crow/getCrowLpApys.js
@@ -2,7 +2,7 @@ const BigNumber = require('bignumber.js');
 const { bscWeb3: web3 } = require('../../../utils/web3');
 
 const MasterChef = require('../../../abis/CrowMasterChef.json');
-const { fetchPrice } = require('../../../utils/fetchPrice');
+const fetchPrice = require('../../../utils/fetchPrice');
 const pools = require('../../../data/crowLpPools.json');
 const { compound } = require('../../../utils/compound');
 const { getTotalLpStakedInUsd } = require('../../../utils/getTotalStakedInUsd');
