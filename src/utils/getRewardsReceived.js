3fb0c396c3deff9207866706b507fec971539af4
RM <roman-monk@users.noreply.github.com>
2021-02-28 20:21:54 +0800

    Use pre-calculated data for total bnb rewards

--- src/utils/getRewardsReceived.js
+++ src/utils/getRewardsReceived.js
@@ -8,17 +8,22 @@ const RPC_QUERY_LIMIT = 5000;
 const RPC_QUERY_INTERVAL = 250;
 const FIRST_REWARD_BLOCK = 1457038;
 
+// pre-calculated rewards for specific block to get them fetched faster
+// can be updated with the values from the "getRewardsReceived" log below
+const CACHED_REWARDS = '8045722945031863322581'
+const CACHED_REWARD_BLOCK = 5267149
+
 const REWARD_POOL = '0x453D4Ba9a2D594314DF88564248497F7D74d6b2C';
 const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 
 const getRewardsReceived = async () => {
-  let result = new BigNumber(0);
+  let result = new BigNumber(CACHED_REWARDS);
 
   const transferTopic = getTopicFromSignature('Transfer(address,address,uint256)');
   const toTopic = getTopicFromAddress(REWARD_POOL);
   
   const lastBlock = await web3.eth.getBlockNumber();
-  let fromBlock = FIRST_REWARD_BLOCK;
+  let fromBlock = CACHED_REWARD_BLOCK;
 
   while (fromBlock < lastBlock) {
     let toBlock = fromBlock + RPC_QUERY_LIMIT;
@@ -43,7 +48,7 @@ const getRewardsReceived = async () => {
     fromBlock = toBlock;
   }
 
-  console.log('> getRewardsReceived');
+  console.log('> getRewardsReceived', lastBlock, result.toFixed());
 
   return Number(result.dividedBy('1e18'));
 };
