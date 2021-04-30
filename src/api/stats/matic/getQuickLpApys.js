2babdf1dc0751dfee014f4b6249b383e34f05465
Moonster <moonster.bsc@gmail.com>
2021-04-30 13:09:17 -0400

    Fix mistake where USDC-USDT Reward pool was set to LP pool

--- src/api/stats/matic/getQuickLpApys.js
+++ src/api/stats/matic/getQuickLpApys.js
@@ -18,7 +18,7 @@ const maticQuickRewardPool = '0x7Ca29F0DB5Db8b88B332Aa1d67a2e89DfeC85E7E'; // Ma
 const ethMaticRewardPool = '0x8FF56b5325446aAe6EfBf006a4C1D88e4935a914'; // Eth-Matic
 const ethBtcRewardPool = '0x070D182EB7E9C3972664C959CE58C5fC6219A7ad'; // Eth-Btc
 const ethUsdtRewardPool = '0xB26bfcD52D997211C13aE4C35E82ced65AF32A02'; // Eth-Usdt
-const usdcUsdtRewardPool = '0x2cf7252e74036d1da831d11089d326296e64a728' // Usdc-Usdt
+const usdcUsdtRewardPool = '0x251d9837a13F38F3Fe629ce2304fa00710176222' // Usdc-Usdt
 
 const getQuickLpApys = async () => {
   let poolMaticQuick = pools.filter(pool => pool.name === 'quick-quick-matic')[0];
