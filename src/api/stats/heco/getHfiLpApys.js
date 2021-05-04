5a48a23a6cc05c8184ad7116830fa9c123416288
MirthFutures <admin@betswapexchange.com>
2021-05-04 15:31:11 -0400

    Organize HECO, Organize Polygon, HFI

--- src/api/stats/heco/getHfiLpApys.js
+++ src/api/stats/heco/getHfiLpApys.js
@@ -1,19 +1,19 @@
 const BigNumber = require('bignumber.js');
 const { hecoWeb3: web3 } = require('../../../utils/web3');
 
-const LavaPool = require('../../../abis/LavaPool.json');
+const HecoPool = require('../../../abis/HFIChef.json');
 const fetchPrice = require('../../../utils/fetchPrice');
-const pools = require('../../../data/lavaLpPools.json');
+const pools = require('../../../data/heco/hfiLpPools.json');
 const { compound } = require('../../../utils/compound');
 const { getTotalLpStakedInUsd } = require('../../../utils/getTotalStakedInUsd');
 const { BASE_HPY } = require('../../../constants');
 
-const getLavaLpApys = async () => {
+const getHfiLpApys = async () => {
   let apys = {};
-  const lavaPool = '0x9d90609B6C90cC378FE15BFA9EDf249d3f3ABf6e';
+  const hecoPool = '0x23159F45Db93729d03a54ff9965F91764b91eC4C';
 
   let promises = [];
-  pools.forEach(pool => promises.push(getPoolApy(lavaPool, pool)));
+  pools.forEach(pool => promises.push(getPoolApy(hecoPool, pool)));
   const values = await Promise.all(promises);
 
   for (item of values) {
@@ -23,35 +23,35 @@ const getLavaLpApys = async () => {
   return apys;
 };
 
-const getPoolApy = async (lavaPool, pool) => {
+const getPoolApy = async (hecoPool, pool) => {
   const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
-    getYearlyRewardsInUsd(lavaPool, pool),
-    getTotalLpStakedInUsd(lavaPool, pool, pool.chainId),
+    getYearlyRewardsInUsd(hecoPool, pool),
+    getTotalLpStakedInUsd(hecoPool, pool, pool.chainId),
   ]);
   const simpleApy = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
   const apy = compound(simpleApy, BASE_HPY, 1, 0.955);
   return { [pool.name]: apy };
 };
 
-const getYearlyRewardsInUsd = async (lavaPool, pool) => {
-  const lavaPoolContract = new web3.eth.Contract(LavaPool, lavaPool);
+const getYearlyRewardsInUsd = async (hecoPool, pool) => {
+  const hecoPoolContract = new web3.eth.Contract(HecoPool, hecoPool);
 
-  const blockRewards = new BigNumber(await lavaPoolContract.methods.sushiPerBlock().call());
+  const blockRewards = new BigNumber(await hecoPoolContract.methods.sushiPerBlock().call());
 
-  let { allocPoint } = await lavaPoolContract.methods.poolInfo(pool.poolId).call();
+  let { allocPoint } = await hecoPoolContract.methods.poolInfo(pool.poolId).call();
   allocPoint = new BigNumber(allocPoint);
 
-  const totalAllocPoint = new BigNumber(await lavaPoolContract.methods.totalAllocPoint().call());
+  const totalAllocPoint = new BigNumber(await hecoPoolContract.methods.totalAllocPoint().call());
   const poolBlockRewards = blockRewards.times(allocPoint).dividedBy(totalAllocPoint);
 
   const secondsPerBlock = 3;
   const secondsPerYear = 31536000;
   const yearlyRewards = poolBlockRewards.dividedBy(secondsPerBlock).times(secondsPerYear);
 
-  const lavaPrice = await fetchPrice({ oracle: 'tokens', id: 'LAVA' });
-  const yearlyRewardsInUsd = yearlyRewards.times(lavaPrice).dividedBy('1e18');
+  const mdxPrice = await fetchPrice({ oracle: 'tokens', id: 'HFI' });
+  const yearlyRewardsInUsd = yearlyRewards.times(mdxPrice).dividedBy('1e18');
 
   return yearlyRewardsInUsd;
 };
 
-module.exports = getLavaLpApys;
+module.exports = getHfiLpApys;
--- src/api/stats/heco/getLavaLpApys.js
+++ src/api/stats/heco/getLavaLpApys.js
@@ -1,19 +1,19 @@
 const BigNumber = require('bignumber.js');
 const { hecoWeb3: web3 } = require('../../../utils/web3');
 
-const HecoPool = require('../../../abis/HFIChef.json');
+const LavaPool = require('../../../abis/LavaPool.json');
 const fetchPrice = require('../../../utils/fetchPrice');
-const pools = require('../../../data/hfiLpPools.json');
+const pools = require('../../../data/heco/lavaLpPools.json');
 const { compound } = require('../../../utils/compound');
 const { getTotalLpStakedInUsd } = require('../../../utils/getTotalStakedInUsd');
 const { BASE_HPY } = require('../../../constants');
 
-const getHfiLpApys = async () => {
+const getLavaLpApys = async () => {
   let apys = {};
-  const hecoPool = '0x23159F45Db93729d03a54ff9965F91764b91eC4C';
+  const lavaPool = '0x9d90609B6C90cC378FE15BFA9EDf249d3f3ABf6e';
 
   let promises = [];
-  pools.forEach(pool => promises.push(getPoolApy(hecoPool, pool)));
+  pools.forEach(pool => promises.push(getPoolApy(lavaPool, pool)));
   const values = await Promise.all(promises);
 
   for (item of values) {
@@ -23,35 +23,35 @@ const getHfiLpApys = async () => {
   return apys;
 };
 
-const getPoolApy = async (hecoPool, pool) => {
+const getPoolApy = async (lavaPool, pool) => {
   const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
-    getYearlyRewardsInUsd(hecoPool, pool),
-    getTotalLpStakedInUsd(hecoPool, pool, pool.chainId),
+    getYearlyRewardsInUsd(lavaPool, pool),
+    getTotalLpStakedInUsd(lavaPool, pool, pool.chainId),
   ]);
   const simpleApy = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
   const apy = compound(simpleApy, BASE_HPY, 1, 0.955);
   return { [pool.name]: apy };
 };
 
-const getYearlyRewardsInUsd = async (hecoPool, pool) => {
-  const hecoPoolContract = new web3.eth.Contract(HecoPool, hecoPool);
+const getYearlyRewardsInUsd = async (lavaPool, pool) => {
+  const lavaPoolContract = new web3.eth.Contract(LavaPool, lavaPool);
 
-  const blockRewards = new BigNumber(await hecoPoolContract.methods.sushiPerBlock().call());
+  const blockRewards = new BigNumber(await lavaPoolContract.methods.sushiPerBlock().call());
 
-  let { allocPoint } = await hecoPoolContract.methods.poolInfo(pool.poolId).call();
+  let { allocPoint } = await lavaPoolContract.methods.poolInfo(pool.poolId).call();
   allocPoint = new BigNumber(allocPoint);
 
-  const totalAllocPoint = new BigNumber(await hecoPoolContract.methods.totalAllocPoint().call());
+  const totalAllocPoint = new BigNumber(await lavaPoolContract.methods.totalAllocPoint().call());
   const poolBlockRewards = blockRewards.times(allocPoint).dividedBy(totalAllocPoint);
 
   const secondsPerBlock = 3;
   const secondsPerYear = 31536000;
   const yearlyRewards = poolBlockRewards.dividedBy(secondsPerBlock).times(secondsPerYear);
 
-  const mdxPrice = await fetchPrice({ oracle: 'tokens', id: 'HFI' });
-  const yearlyRewardsInUsd = yearlyRewards.times(mdxPrice).dividedBy('1e18');
+  const lavaPrice = await fetchPrice({ oracle: 'tokens', id: 'LAVA' });
+  const yearlyRewardsInUsd = yearlyRewards.times(lavaPrice).dividedBy('1e18');
 
   return yearlyRewardsInUsd;
 };
 
-module.exports = getHfiLpApys;
+module.exports = getLavaLpApys;
