c855a8c179f5f69a5119c37759170e02c32b8542
RM <roman-monk@users.noreply.github.com>
2021-05-20 18:26:21 +0800

    Non-parallel getFantomApys

--- src/api/stats/fantom/index.js
+++ src/api/stats/fantom/index.js
@@ -8,16 +8,25 @@ const getApys = [getSpookyLpApys, getFroyoLpApys, getEsterApys, getSpookyBooApy]
 const getFantomApys = async () => {
   let apys = {};
 
-  let promises = [];
-  getApys.forEach(getApy => promises.push(getApy()));
-  const results = await Promise.allSettled(promises);
+  // let promises = [];
+  // getApys.forEach(getApy => promises.push(getApy()));
+  // const results = await Promise.allSettled(promises);
+  //
+  // for (const result of results) {
+  //   if (result.status !== 'fulfilled') {
+  //     console.warn('getFantomApys error', result.reason);
+  //     continue;
+  //   }
+  //   apys = { ...apys, ...result.value };
+  // }
 
-  for (const result of results) {
-    if (result.status !== 'fulfilled') {
-      console.warn('getFantomApys error', result.reason);
-      continue;
+  for (const getApy of getApys) {
+    try {
+      const apy = await getApy();
+      apys = { ...apys, ...apy };
+    } catch (e) {
+      console.error('FantomApy error', getApy.name, e);
     }
-    apys = { ...apys, ...result.value };
   }
 
   return apys;
