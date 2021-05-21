32fb5f2f3d853f14d832a60f94af8b80c1c3dc7e
Moonster <moonster.bsc@gmail.com>
2021-05-20 23:00:48 -0400

    eps apy

--- src/api/stats/ellipsis/getEllipsisSingleAssetApy.js
+++ src/api/stats/ellipsis/getEllipsisSingleAssetApy.js
@@ -10,7 +10,7 @@ const oracle = 'tokens';
 const oracleId = 'EPS';
 
 const outputOracleId = 'BUSD';
-const outputTokenAddress = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+const outputTokenAddress = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
 
 const DECIMALS = '1e18';
 const BLOCKS_PER_DAY = 28800;
--- src/api/stats/getApys.js
+++ src/api/stats/getApys.js
@@ -28,6 +28,7 @@ const getPumpyLpApys = require('./pumpy/getPumpyLpApys');
 const getAlpacaLpApys = require('./alpaca/getAlpacaLpApys');
 const getAlpacaApys = require('./alpaca/getAlpacaApys');
 const getEllipsisLpApys = require('./ellipsis/getEllipsisLpApys');
+const getEllipsisSingleAssetApy = require('./ellipsis/getEllipsisSingleAssetApy');
 const get1inchApy = require('./1inch/get1inchApy');
 const getSwirlLpApys = require('./swirl/getSwirlLpApys');
 const getMdexBscLpApys = require('./mdex/getMdexBscLpApys');
