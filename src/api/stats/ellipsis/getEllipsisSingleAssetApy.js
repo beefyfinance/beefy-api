cae6610d0c83036538dd5d882b2d7aed19a0c3f9
Moonster <moonster.bsc@gmail.com>
2021-05-20 23:25:44 -0400

    fix no return

--- src/api/stats/ellipsis/getEllipsisSingleAssetApy.js
+++ src/api/stats/ellipsis/getEllipsisSingleAssetApy.js
@@ -2,7 +2,7 @@ const { bscWeb3: web3 } = require('../../../utils/web3');
 
 const getMultiFeeDistributionSingleAssetApy = require('../common/getMultiFeeDistributionSingleAssetApy');
 
-const getEllipsisSingleAssetApy = async () => {
+const getEllipsisSingleAssetApy = async () =>
   await getMultiFeeDistributionSingleAssetApy({
     web3,
     multiFeeDistributionAddress: '0x4076CC26EFeE47825917D0feC3A79d0bB9a6bB5c',
@@ -11,6 +11,5 @@ const getEllipsisSingleAssetApy = async () => {
     outputTokenAddress: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
     poolName: 'ellipsis-eps',
   });
-};
 
 module.exports = getEllipsisSingleAssetApy;
--- src/api/stats/matic/getAddyApy.js
+++ src/api/stats/matic/getAddyApy.js
@@ -2,7 +2,7 @@ const { polygonWeb3: web3 } = require('../../../utils/web3');
 
 const getMultiFeeDistributionSingleAssetApy = require('../common/getMultiFeeDistributionSingleAssetApy');
 
-const getAddyApy = async () => {
+const getAddyApy = async () =>
   await getMultiFeeDistributionSingleAssetApy({
     web3,
     multiFeeDistributionAddress: '0x920f22E1e5da04504b765F8110ab96A20E6408Bd',
@@ -11,6 +11,5 @@ const getAddyApy = async () => {
     outputTokenAddress: '0x831753dd7087cac61ab5644b308642cc1c33dc13',
     poolName: 'adamant-addy',
   });
-};
 
 module.exports = getAddyApy;
