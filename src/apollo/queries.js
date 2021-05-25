5a74961b475f75966937e6b5d1cd57f1dc2fc1dc
Moonster <moonster.bsc@gmail.com>
2021-05-25 15:45:02 -0400

    fix sushi apys

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -1,119 +1,5 @@
 const gql = require('graphql-tag');
 
-const miniChefPoolQuery = gql`
-  query poolsQuery(
-    $first: Int! = 1000
-    $skip: Int! = 0
-    $orderBy: String! = "timestamp"
-    $orderDirection: String! = "desc"
-  ) {
-    pools(first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection) {
-      id
-      pair
-      rewarder {
-        id
-        rewardToken
-        rewardPerSecond
-      }
-      allocPoint
-      lastRewardTime
-      accSushiPerShare
-      slpBalance
-      userCount
-      miniChef {
-        id
-        sushiPerSecond
-        totalAllocPoint
-      }
-    }
-  }
-`;
-
-const blockFieldsQuery = gql`
-  fragment blockFields on Block {
-    id
-    number
-    timestamp
-  }
-`;
-
-const blockQuery = gql`
-  query blockQuery($start: Int!, $end: Int!) {
-    blocks(
-      first: 1
-      orderBy: timestamp
-      orderDirection: asc
-      where: { timestamp_gt: $start, timestamp_lt: $end }
-    ) {
-      ...blockFields
-    }
-  }
-  ${blockFieldsQuery}
-`;
-
-const pairTokenFieldsQuery = gql`
-  fragment pairTokenFields on Token {
-    id
-    name
-    symbol
-    totalSupply
-    derivedETH
-  }
-`;
-
-const pairFieldsQuery = gql`
-  fragment pairFields on Pair {
-    id
-    reserveUSD
-    reserveETH
-    volumeUSD
-    untrackedVolumeUSD
-    trackedReserveETH
-    token0 {
-      ...pairTokenFields
-    }
-    token1 {
-      ...pairTokenFields
-    }
-    reserve0
-    reserve1
-    token0Price
-    token1Price
-    totalSupply
-    txCount
-    timestamp
-  }
-  ${pairTokenFieldsQuery}
-`;
-
-const pairSubsetQuery = gql`
-  query pairSubsetQuery(
-    $first: Int! = 1000
-    $pairAddresses: [Bytes]!
-    $orderBy: String! = "trackedReserveETH"
-    $orderDirection: String! = "desc"
-  ) {
-    pairs(
-      first: $first
-      orderBy: $orderBy
-      orderDirection: $orderDirection
-      where: { id_in: $pairAddresses }
-    ) {
-      ...pairFields
-    }
-  }
-  ${pairFieldsQuery}
-`;
-
-const pairTimeTravelQuery = gql`
-  query pairTimeTravelQuery($id: String!, $block: Block_height!) {
-    pair(id: $id, block: $block) {
-      ...pairFields
-    }
-  }
-  ${pairFieldsQuery}
-`;
-
 const pairDayDataQuery = (pairs, startTimestamp) => {
   let pairsString = `[`;
   pairs.map(pair => {
@@ -137,10 +23,32 @@ const pairDayDataQuery = (pairs, startTimestamp) => {
   return gql(queryString);
 };
 
+const pairDayDataSushiQuery = (pairs, startTimestamp) => {
+  let pairsString = `[`;
+  pairs.map(pair => {
+    return (pairsString += `"${pair}"`);
+  });
+  pairsString += ']';
+  const queryString = `
+    query days {
+      pairs(where: { id_in: ${pairsString}}) {
+        dayData(first: 1000, orderBy: date, orderDirection: asc, where: { date_gt: ${startTimestamp} }) {
+          id
+          pair
+          date
+          volumeToken0
+          volumeToken1
+          volumeUSD
+          totalSupply
+          reserveUSD
+        }
+      }
+    } 
+`;
+  return gql(queryString);
+};
+
 module.exports = {
-  miniChefPoolQuery,
-  blockQuery,
-  pairTimeTravelQuery,
-  pairSubsetQuery,
   pairDayDataQuery,
+  pairDayDataSushiQuery,
 };
--- src/utils/getTradingFeeApr.js
+++ src/utils/getTradingFeeApr.js
@@ -1,5 +1,5 @@
 const { startOfMinute, subDays } = require('date-fns');
-const { pairDayDataQuery } = require('../apollo/queries');
+const { pairDayDataQuery, pairDayDataSushiQuery } = require('../apollo/queries');
 const BigNumber = require('bignumber.js');
 
 const getTradingFeeApr = async (client, pairAddresses, liquidityProviderFee) => {
@@ -25,6 +25,30 @@ const getTradingFeeApr = async (client, pairAddresses, liquidityProviderFee) => 
   return pairAddressToAprMap;
 };
 
+const getTradingFeeAprSushi = async (client, pairAddresses, liquidityProviderFee) => {
+  const date = startOfMinute(subDays(Date.now(), 1));
+  const start = Math.floor(Number(date) / 1000);
+
+  let queryResponse = await client.query({
+    query: pairDayDataSushiQuery(pairAddresses, start),
+  });
+
+  const pairDayDatas = queryResponse.data.pairs.map(pair => pair.dayData[0]);
+
+  const pairAddressToAprMap = {};
+  for (const pairDayData of pairDayDatas) {
+    const { id } = pairDayData;
+    const pairAddress = id.split('-')[0].toLowerCase();
+    pairAddressToAprMap[pairAddress] = new BigNumber(pairDayData.volumeUSD)
+      .times(liquidityProviderFee)
+      .times(365)
+      .dividedBy(pairDayData.reserveUSD);
+  }
+
+  return pairAddressToAprMap;
+};
+
 module.exports = {
   getTradingFeeApr,
+  getTradingFeeAprSushi,
 };
