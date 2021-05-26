c6913887fe199c578fc6b0ae11691cc08a937b04
Moonster <moonster.bsc@gmail.com>
2021-05-26 13:21:13 -0400

    use previous full day data

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -1,6 +1,6 @@
 const gql = require('graphql-tag');
 
-const pairDayDataQuery = (pairs, startTimestamp) => {
+const pairDayDataQuery = (pairs, startTimestamp, endTimestamp) => {
   let pairsString = `[`;
   pairs.map(pair => {
     return (pairsString += `"${pair}"`);
@@ -8,7 +8,7 @@ const pairDayDataQuery = (pairs, startTimestamp) => {
   pairsString += ']';
   const queryString = `
     query days {
-      pairDayDatas(first: 1000, orderBy: date, orderDirection: asc, where: { pairAddress_in: ${pairsString}, date_gt: ${startTimestamp} }) {
+      pairDayDatas(first: 1000, orderBy: date, orderDirection: asc, where: { pairAddress_in: ${pairsString}, date_gt: ${startTimestamp}, date_lt: ${endTimestamp} }) {
         id
         pairAddress 
         date
@@ -23,7 +23,7 @@ const pairDayDataQuery = (pairs, startTimestamp) => {
   return gql(queryString);
 };
 
-const pairDayDataSushiQuery = (pairs, startTimestamp) => {
+const pairDayDataSushiQuery = (pairs, startTimestamp, endTimestamp) => {
   let pairsString = `[`;
   pairs.map(pair => {
     return (pairsString += `"${pair}"`);
@@ -32,7 +32,7 @@ const pairDayDataSushiQuery = (pairs, startTimestamp) => {
   const queryString = `
     query days {
       pairs(where: { id_in: ${pairsString}}) {
-        dayData(first: 1000, orderBy: date, orderDirection: asc, where: { date_gt: ${startTimestamp} }) {
+        dayData(first: 1000, orderBy: date, orderDirection: asc, where: { date_gt: ${startTimestamp}, date_lt: ${endTimestamp} }) {
           id
           pair
           date
