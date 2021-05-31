57d433e36fa95409b34929b06b8df18dd7c13379
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2021-05-31 20:19:03 +0100

    Add etag support

--- src/app.js
+++ src/app.js
@@ -4,15 +4,20 @@ const Koa = require('koa');
 const helmet = require('koa-helmet');
 const body = require('koa-bodyparser');
 const cors = require('@koa/cors');
+const conditional = require('koa-conditional-get');
+const etag = require('koa-etag');
 
 const rt = require('./middleware/rt');
 const powered = require('./middleware/powered');
 const router = require('./router');
 
 const app = new Koa();
+
+app.use(rt);
+app.use(conditional());
+app.use(etag());
 app.use(helmet());
 app.use(cors({ origin: '*' }));
-app.use(rt);
 app.use(powered);
 app.use(body());
 
