0a70c96857883af1c43fdcb2d7106b7210548c7b
Moonster <moonster.bsc@gmail.com>
2021-06-07 11:01:17 -0400

    gbull init

---
+++ src/api/stats/matic/getGoldenbullLpApys.js
@@ -0,0 +1,29 @@
+const getMasterChefApys = require('./getMaticMasterChefApys');
+
+const MasterChefAbi = require('../../../abis/matic/PolyzapMasterChef.json');
+const pools = require('../../../data/matic/goldenbullLpPools.json');
+const { polyzapClient } = require('../../../apollo/client');
+const { addressBook } = require('blockchain-addressbook');
+const {
+  polygon: {
+    platforms: { goldenbull },
+    tokens: { GBULL },
+  },
+} = addressBook;
+
+const getGoldenbullLpApys = async () =>
+  await getMasterChefApys({
+    masterchef: goldenbull.masterchef,
+    masterchefAbi: MasterChefAbi,
+    tokenPerBlock: 'pZapPerBlock',
+    hasMultiplier: true,
+    pools: pools,
+    oracleId: 'PZAP',
+    oracle: 'tokens',
+    decimals: '1e18',
+    // log: true,
+    tradingFeeInfoClient: polyzapClient,
+    liquidityProviderFee: 0.002,
+  });
+
+module.exports = getGoldenbullLpApys;
