4951e77a0327f24479a4cddde00921b292393671
MirthFutures <admin@betswapexchange.com>
2021-06-14 21:32:39 -0400

    Add more fantom pools

--- src/api/stats/getAmmPrices.js
+++ src/api/stats/getAmmPrices.js
@@ -89,6 +89,7 @@ const waultPools = require('../../data/waultLpPools.json');
 const tenfiPools = require('../../data/tenfiLpPools.json');
 const burgerPools = require('../../data/burgerLpPools.json');
 const tombPools = require('../../data/fantom/tombLpPools.json');
+const spiritPools = require('../../data/fantom/spiritPools.json');
 
 const INIT_DELAY = 0 * 60 * 1000;
 const REFRESH_INTERVAL = 5 * 60 * 1000;
@@ -96,6 +97,7 @@ const REFRESH_INTERVAL = 5 * 60 * 1000;
 // FIXME: if this list grows too big we might hit the ratelimit on initialization everytime
 // Implement in case of emergency -> https://github.com/beefyfinance/beefy-api/issues/103
 const pools = [
+  ...spiritPools,
   ...tombPools,
   ...burgerPools,
   ...waultPools,
