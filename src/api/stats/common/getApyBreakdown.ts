601041d8ce34197c5115977e4ae5e910cf27f417
Broken Robot <loewen.nathan@gmail.com>
2021-06-20 17:15:09 -0500

    getApyBreakdown: make performance fee a parameter

--- src/api/stats/common/getApyBreakdown.ts
+++ src/api/stats/common/getApyBreakdown.ts
@@ -1,4 +1,6 @@
-import BigNumber from "bignumber.js";
+'use strict';
+
+import BigNumber from 'bignumber.js';
 
 import getFarmWithTradingFeesApy from '../../../utils/getFarmWithTradingFeesApy';
 import { compound } from '../../../utils/compound';
@@ -6,46 +8,59 @@ import { compound } from '../../../utils/compound';
 import { BASE_HPY } from '../../../constants';
 
 const beefyPerformanceFee = 0.045;
-const shareAfterBeefyPerformanceFee = 1 - beefyPerformanceFee;
 
 interface ApyBreakdown {
-    vaultApr:number,
-    compoundingsPerYear:number,
-    beefyPerformanceFee:number,
-    vaultApy:number,
-    lpFee:number,
-    tradingApr:number,
-    totalApy:number,
+  vaultApr?: number;
+  compoundingsPerYear?: number;
+  beefyPerformanceFee?: number;
+  vaultApy?: number;
+  lpFee?: number;
+  tradingApr?: number;
+  totalApy?: number;
 }
 
-export default function(pools:{name:string,address:string}[], tradingAprs:BigNumber[], farmAprs:BigNumber[], providerFee:number) {
-  const result:{
-        apys:Record<string,number>,
-        apyBreakdowns:Record<string,ApyBreakdown>
-    } = {
-        apys:{},
-        apyBreakdowns:{}
-    };
+export default function (
+  pools: { name: string; address: string }[],
+  tradingAprs: Record<string, BigNumber>,
+  farmAprs: BigNumber[],
+  providerFee: number,
+  performanceFee: number = beefyPerformanceFee
+) {
+  const result: {
+    apys: Record<string, number>;
+    apyBreakdowns: Record<string, ApyBreakdown>;
+  } = {
+    apys: {},
+    apyBreakdowns: {},
+  };
+
+  const shareAfterPerformanceFee = 1 - beefyPerformanceFee;
 
   pools.forEach((pool, i) => {
-    const simpleApr = farmAprs[i];
-    const vaultApr = simpleApr?.times(shareAfterBeefyPerformanceFee);
-    const vaultApy = compound(simpleApr, BASE_HPY, 1, shareAfterBeefyPerformanceFee);
-    const tradingApr = tradingAprs[pool.address.toLowerCase()];
-    const totalApy = getFarmWithTradingFeesApy(simpleApr, tradingApr, BASE_HPY, 1, 0.955);
+    const simpleApr = farmAprs[i]?.toNumber();
+    const vaultApr = simpleApr * shareAfterPerformanceFee;
+    const vaultApy = compound(simpleApr, BASE_HPY, 1, shareAfterPerformanceFee);
+    const tradingApr = tradingAprs[pool.address.toLowerCase()]?.toNumber();
+    const totalApy = getFarmWithTradingFeesApy(
+      simpleApr,
+      tradingApr,
+      BASE_HPY,
+      1,
+      shareAfterPerformanceFee
+    );
 
     // Add token to APYs object
     result.apys[pool.name] = totalApy;
     result.apyBreakdowns[pool.name] = {
-        vaultApr: vaultApr?.toNumber(),
-        compoundingsPerYear: BASE_HPY,
-        beefyPerformanceFee: beefyPerformanceFee,
-        vaultApy: vaultApy,
-        lpFee: providerFee,
-        tradingApr: tradingApr?.toNumber(),
-        totalApy: totalApy,
+      vaultApr: vaultApr,
+      compoundingsPerYear: BASE_HPY,
+      beefyPerformanceFee: performanceFee,
+      vaultApy: vaultApy,
+      lpFee: providerFee,
+      tradingApr: tradingApr,
+      totalApy: totalApy,
     };
   });
 
   return result;
-};
+}
