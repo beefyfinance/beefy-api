8d12b393390aae096dd4af404e72da6d9a35960c
Moonster <moonster.bsc@gmail.com>
2021-06-27 18:52:36 -0400

    upgrade addressbook and create new folder and index

---
+++ src/api/stats/bifibuyback/index.ts
@@ -0,0 +1,17 @@
+const TIMEOUT = 5 * 60 * 1000;
+
+export async function bifibuyback(ctx) {
+  try {
+    ctx.request.socket.setTimeout(TIMEOUT);
+    let bifibuyback = 0;
+
+    if (!bifibuyback) {
+      throw 'There is no bifibuyback data yet';
+    }
+
+    ctx.status = 200;
+    ctx.body = bifibuyback;
+  } catch (err) {
+    ctx.throw(500, err);
+  }
+}
