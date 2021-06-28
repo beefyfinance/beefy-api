dc6a5fec387980d1b85e35d9df7a00284996a669
Moonster <moonster.bsc@gmail.com>
2021-06-27 21:18:31 -0400

    token var

--- src/api/stats/bifibuyback/getBifiBuyback.ts
+++ src/api/stats/bifibuyback/getBifiBuyback.ts
@@ -24,7 +24,7 @@ const getOneDayBlocksFromEtherscan = async (scanUrl: string, apiToken: string) =
 
 const getBlockFromEtherscan = async (scanUrl: string, timestamp: number, apiToken?: string) => {
   const token = apiToken ? apiToken : 'YourApiKeyToken';
-  const url = `${scanUrl}/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=after&apikey=YourApiKeyToken`;
+  const url = `${scanUrl}/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=after&apikey=${token}`;
   const resp = await fetch(url);
   const json: BlockApiResponse = await resp.json();
   return json.result;
