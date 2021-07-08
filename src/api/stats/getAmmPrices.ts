353a70576709a8d2cc666ca7117694be0cb42d21
RM <roman-monk@users.noreply.github.com>
2021-07-08 23:53:07 +0800

    bump addres-book

--- src/api/stats/getAmmPrices.ts
+++ src/api/stats/getAmmPrices.ts
@@ -114,6 +114,7 @@ const REFRESH_INTERVAL = 5 * 60 * 1000;
 // FIXME: if this list grows too big we might hit the ratelimit on initialization everytime
 // Implement in case of emergency -> https://github.com/beefyfinance/beefy-api/issues/103
 const pools = [
+  ...maiPools,
   ...boneswapApePools,
   ...boneswapSushiPools,
   ...boneswapQuickPools,
@@ -217,7 +218,6 @@ const pools = [
   ...thugsPools,
   ...cakeLpV1Pools,
   ...cakeLpPools,
-  ...maiPools,
 ];
 
 const knownPrices = {
