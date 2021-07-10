72e053bb98347c66a665038b900f85bebb1a175c
Moonster <moonster.bsc@gmail.com>
2021-07-10 14:08:48 -0400

    LpPool.ts

---
+++ src/types/LpPool.ts
@@ -0,0 +1,19 @@
+import { addressBookByChainId, ChainId } from 'blockchain-addressbook';
+
+export interface LpPool {
+  name: string;
+  platform?: string;
+  address: string;
+  decimals: string;
+  poolId?: number;
+  chainId: ChainId;
+  lp0: LpToken;
+  lp1: LpToken;
+}
+
+export interface LpToken {
+  address: string;
+  oracle: 'tokens';
+  oracleId: string;
+  decimals: string;
+}
