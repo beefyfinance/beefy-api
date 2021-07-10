f2b5984e530e32262d79ccded5183604c2272707
Moonster <moonster.bsc@gmail.com>
2021-07-10 15:41:49 -0400

    comment

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,5 +1,7 @@
 const { chainRandomClients, _web3Factory, _multicallAddress } = require('./web3Helpers');
 
+// keep backwards compat for commonJs export system, but still get types in code by importing from web3Helpers.js
+
 module.exports = {
   get bscWeb3() {
     return chainRandomClients.bscRandomClient();
