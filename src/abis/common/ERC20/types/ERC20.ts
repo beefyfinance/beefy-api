3d5f44e5f4ea77a521061ea547994e874cee9fa5
Moonster <moonster.bsc@gmail.com>
2021-07-11 18:46:04 -0400

    erc20

---
+++ src/abis/common/ERC20/types/ERC20.ts
@@ -0,0 +1,167 @@
+import BN from 'bn.js';
+import BigNumber from 'bignumber.js';
+import {
+  PromiEvent,
+  TransactionReceipt,
+  EventResponse,
+  EventData,
+  Web3ContractContext,
+} from 'ethereum-abi-types-generator';
+
+export interface CallOptions {
+  from?: string;
+  gasPrice?: string;
+  gas?: number;
+}
+
+export interface SendOptions {
+  from: string;
+  value?: number | string | BN | BigNumber;
+  gasPrice?: string;
+  gas?: number;
+}
+
+export interface EstimateGasOptions {
+  from?: string;
+  value?: number | string | BN | BigNumber;
+  gas?: number;
+}
+
+export interface MethodPayableReturnContext {
+  send(options: SendOptions): PromiEvent<TransactionReceipt>;
+  send(
+    options: SendOptions,
+    callback: (error: Error, result: any) => void
+  ): PromiEvent<TransactionReceipt>;
+  estimateGas(options: EstimateGasOptions): Promise<number>;
+  estimateGas(
+    options: EstimateGasOptions,
+    callback: (error: Error, result: any) => void
+  ): Promise<number>;
+  encodeABI(): string;
+}
+
+export interface MethodConstantReturnContext<TCallReturn> {
+  call(): Promise<TCallReturn>;
+  call(options: CallOptions): Promise<TCallReturn>;
+  call(
+    options: CallOptions,
+    callback: (error: Error, result: TCallReturn) => void
+  ): Promise<TCallReturn>;
+  encodeABI(): string;
+}
+
+export interface MethodReturnContext extends MethodPayableReturnContext {}
+
+export type ContractContext = Web3ContractContext<
+  ERC20,
+  ERC20MethodNames,
+  ERC20EventsContext,
+  ERC20Events
+>;
+export type ERC20Events = 'Approval' | 'Transfer';
+export interface ERC20EventsContext {
+  Approval(
+    parameters: {
+      filter?: { owner?: string | string[]; spender?: string | string[] };
+      fromBlock?: number;
+      toBlock?: 'latest' | number;
+      topics?: string[];
+    },
+    callback?: (error: Error, event: EventData) => void
+  ): EventResponse;
+  Transfer(
+    parameters: {
+      filter?: { from?: string | string[]; to?: string | string[] };
+      fromBlock?: number;
+      toBlock?: 'latest' | number;
+      topics?: string[];
+    },
+    callback?: (error: Error, event: EventData) => void
+  ): EventResponse;
+}
+export type ERC20MethodNames =
+  | 'name'
+  | 'approve'
+  | 'totalSupply'
+  | 'transferFrom'
+  | 'decimals'
+  | 'balanceOf'
+  | 'symbol'
+  | 'transfer'
+  | 'allowance';
+export interface ERC20 {
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   */
+  name(): MethodConstantReturnContext<string>;
+  /**
+   * Payable: false
+   * Constant: false
+   * StateMutability: nonpayable
+   * Type: function
+   * @param _spender Type: address, Indexed: false
+   * @param _value Type: uint256, Indexed: false
+   */
+  approve(_spender: string, _value: string): MethodReturnContext;
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   */
+  totalSupply(): MethodConstantReturnContext<string>;
+  /**
+   * Payable: false
+   * Constant: false
+   * StateMutability: nonpayable
+   * Type: function
+   * @param _from Type: address, Indexed: false
+   * @param _to Type: address, Indexed: false
+   * @param _value Type: uint256, Indexed: false
+   */
+  transferFrom(_from: string, _to: string, _value: string): MethodReturnContext;
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   */
+  decimals(): MethodConstantReturnContext<string>;
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   * @param _owner Type: address, Indexed: false
+   */
+  balanceOf(_owner: string): MethodConstantReturnContext<string>;
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   */
+  symbol(): MethodConstantReturnContext<string>;
+  /**
+   * Payable: false
+   * Constant: false
+   * StateMutability: nonpayable
+   * Type: function
+   * @param _to Type: address, Indexed: false
+   * @param _value Type: uint256, Indexed: false
+   */
+  transfer(_to: string, _value: string): MethodReturnContext;
+  /**
+   * Payable: false
+   * Constant: true
+   * StateMutability: view
+   * Type: function
+   * @param _owner Type: address, Indexed: false
+   * @param _spender Type: address, Indexed: false
+   */
+  allowance(_owner: string, _spender: string): MethodConstantReturnContext<string>;
+}
