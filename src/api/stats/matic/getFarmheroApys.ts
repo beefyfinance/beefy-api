2326b6feeefb635f3ab86f8127d0915351de1951
Moonster <moonster.bsc@gmail.com>
2021-07-11 19:03:29 -0400

    cleanup

--- src/api/stats/matic/getFarmheroApys.ts
+++ src/api/stats/matic/getFarmheroApys.ts
@@ -4,7 +4,7 @@ import { polygonWeb3 as web3, multicallAddress } from '../../../utils/web3';
 
 // abis
 import { ContractContext as FarmHeroChef, FarmHeroChef_ABI } from '../../../abis/matic/FarmHero';
-import { ERC20_ABI, ERC20_MethodName } from '../../../abis/ERC20';
+import { ContractContext as ERC20, ERC20_ABI } from '../../../abis/common/ERC20';
 // json data
 import _pools from '../../../data/matic/farmheroPools.json';
 const pools = _pools as LpPool[];
@@ -14,7 +14,7 @@ import { POLYGON_CHAIN_ID, QUICK_LPF } from '../../../constants';
 import { getTradingFeeApr } from '../../../utils/getTradingFeeApr';
 import { quickClient } from '../../../apollo/client';
 import getApyBreakdown from '../common/getApyBreakdown';
-import { addressBook } from 'blockchain-addressbook';
+import { addressBook } from '../../../../packages/address-book/address-book/';
 import { getEDecimals } from '../../../utils/getEDecimals';
 import { LpPool } from '../../../types/LpPool';
 
@@ -40,10 +40,9 @@ export const getFarmheroApys = async () => {
 
 const getFarmApys = async (pools: LpPool[]) => {
   const apys = [];
-  const chefContract = (new web3.eth.Contract(ABI, chef) as unknown) as FarmHeroChef;
-  const methods: Record<FarmHeroChef_MethodName, any> = chefContract.methods;
-  const heroPerSecond = new BigNumber(await methods.HERORewardPerSecond().call());
-  const totalAllocPoint = new BigNumber(await chefContract.methods.totalAllocPoint().call());
+  const chefContract = (new web3.eth.Contract(FarmHeroChef_ABI, chef) as unknown) as FarmHeroChef;
+  const heroPerSecond = new BigNumber(await chefContract.methods.HERORewardPerSecond().call());
+  const totalAllocPoint = new BigNumber(await chefContract.methods.totalAllocPoint(0).call()); //  enum PoolType { ERC20, ERC721, ERC1155 }
 
   const tokenPrice = await fetchPrice({ oracle, id: oracleId });
   const { balances, allocPoints } = await getPoolsData(pools);
@@ -64,19 +63,17 @@ const getFarmApys = async (pools: LpPool[]) => {
 };
 
 const getPoolsData = async (pools: LpPool[]) => {
-  const chefContract = new web3.eth.Contract(FarmHeroChef_ABI, chef);
-  const chefMethods: Record<FarmHeroChef_MethodName, any> = chefContract.methods;
+  const chefContract = (new web3.eth.Contract(FarmHeroChef_ABI, chef) as unknown) as FarmHeroChef;
 
   const balanceCalls = [];
   const allocPointCalls = [];
   pools.forEach(pool => {
-    const tokenContract = new web3.eth.Contract(ERC20_ABI, pool.address);
-    const erc20Methods: Record<ERC20_MethodName, any> = tokenContract.methods;
+    const tokenContract = (new web3.eth.Contract(ERC20_ABI, pool.address) as unknown) as ERC20;
     balanceCalls.push({
-      balance: erc20Methods.balanceOf(chef),
+      balance: tokenContract.methods.balanceOf(chef),
     });
     allocPointCalls.push({
-      allocPoint: chefMethods.poolInfo(pool.poolId),
+      allocPoint: chefContract.methods.poolInfo(pool.poolId.toString()),
     });
   });
 
@@ -84,6 +81,7 @@ const getPoolsData = async (pools: LpPool[]) => {
   const res = await multicall.all([balanceCalls, allocPointCalls]);
 
   const balances = res[0].map(v => new BigNumber(v.balance));
-  const allocPoints = res[1].map(v => v.allocPoint['2']);
+  const allocPointPropertyPosition = '2';
+  const allocPoints = res[1].map(v => v.allocPoint[allocPointPropertyPosition]);
   return { balances, allocPoints };
 };
