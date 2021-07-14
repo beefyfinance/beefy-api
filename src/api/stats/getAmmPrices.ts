2ccb914e51675a30521c5ae4d890363231d2d11e
Moonster <moonster.bsc@gmail.com>
2021-07-14 01:48:38 -0400

    add to amm prices

--- src/api/stats/getAmmPrices.ts
+++ src/api/stats/getAmmPrices.ts
@@ -110,6 +110,7 @@ import maiPools from '../../data/matic/maiLpPools.json';
 import jetswapPolyPools from '../../data/matic/jetswapLpPools.json';
 import ironSwapPools from '../../data/matic/ironSwapLpPools.json';
 import ooePools from '../../data/ooeLpPools.json';
+import telxchangePools from '../../data/matic/telxchangePools.json';
 
 const INIT_DELAY = 0 * 60 * 1000;
 const REFRESH_INTERVAL = 5 * 60 * 1000;
@@ -117,6 +118,7 @@ const REFRESH_INTERVAL = 5 * 60 * 1000;
 // FIXME: if this list grows too big we might hit the ratelimit on initialization everytime
 // Implement in case of emergency -> https://github.com/beefyfinance/beefy-api/issues/103
 const pools = [
+  ...telxchangePools,
   ...ooePools,
   ...ironSwapPools,
   ...jetswapPolyPools,
