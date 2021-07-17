69c5c8c72404721a34c29452489e6466016e95bb
Moonster <moonster.bsc@gmail.com>
2021-07-17 00:36:05 -0400

    xYELD address-book

--- packages/address-book/address-book/polygon/platforms/polyyeld.ts
+++ packages/address-book/address-book/polygon/platforms/polyyeld.ts
@@ -1,3 +1,7 @@
 export const polyyeld = {
   masterchef: '0x2DC11B394BD0f1CC6AC0a269cfe3CC0b333601B4',
 } as const;
+
+export const polyyeld_xyeld = {
+  masterchef: '0x54aC698DbA046247B8D081774005A434f03d329D',
+};
