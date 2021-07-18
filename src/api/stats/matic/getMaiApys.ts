4ef2106299947ae0bf6b9abfe09c76cb668d687b
Moonster-BSC <83176690+Moonster-BSC@users.noreply.github.com>
2021-07-18 03:41:41 -0400

    Fix getMaiApys (#301)

--- src/api/stats/matic/getMaiApys.ts
+++ src/api/stats/matic/getMaiApys.ts
@@ -1,15 +1,16 @@
-import getMasterChefApys from './getMaticMasterChefApys';
+import { getMasterChefApys } from './getMaticMasterChefApys';
 import MasterChefAbi from '../../../abis/matic/MaiFarmChef.json';
 import pools from '../../../data/matic/maiLpPools.json';
 import { quickClient } from '../../../apollo/client';
 import { addressBook } from '../../../../packages/address-book/address-book';
+import { AbiItem } from 'ethereum-abi-types-generator';
 
 const mai = addressBook.polygon.platforms.mai;
 
 const getMaiApys = async () =>
   getMasterChefApys({
     masterchef: mai.chef,
-    masterchefAbi: MasterChefAbi,
+    masterchefAbi: MasterChefAbi as AbiItem[],
     tokenPerBlock: 'rewardPerBlock',
     hasMultiplier: false,
     pools: pools,
