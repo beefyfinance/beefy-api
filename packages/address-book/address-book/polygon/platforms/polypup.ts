ec20c9d73876c390d54406404022ea9320a75b95
Moonster <moonster.bsc@gmail.com>
2021-07-18 16:12:38 -0400

    add polypup timelocks

--- packages/address-book/address-book/polygon/platforms/polypup.ts
+++ packages/address-book/address-book/polygon/platforms/polypup.ts
@@ -1,7 +1,9 @@
 export const polypup = {
   masterchef: '0xCc7E7c9FC775D25176e9Bfc5A400EdAc212aa81C',
+  timelock: '0xFA256781ff98ac55f1E531d8217A7EB7Fd54F7a2',
 } as const;
 
 export const polypupBone = {
   masterchef: '0x9DcB2D5e7b5212fAF98e4a152827fd76bD55f68b',
+  timelock: '0xdfe4665328DD5C5485A05c2A5252Bb44Bf6E65dc',
 } as const;
