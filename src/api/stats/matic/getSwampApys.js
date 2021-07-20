991e8626058909aafb650fa622a0183fe9383565
exleyk <51703190+exleyk@users.noreply.github.com>
2021-07-20 20:13:00 +0100

    Create getSwampApys.js

---
+++ src/api/stats/matic/getSwampApys.js
@@ -0,0 +1,74 @@
+const { getMasterChefApys } = require('./getMaticMasterChefApys');
+const {
+  addressBook: {
+    polygon: {
+      tokens: { pSWAMP },
+      platforms: { swamp },
+    },
+  },
+} = require('../../../../packages/address-book/address-book');
+
+const { QUICK_LPF } = require('../../../constants');
+const MasterChefAbi = require('../../../abis/matic/SwampMasterChef.json');
+const singlePools = require('../../../data/matic/polypupSinglePools.json');
+const { quickClient } = require('../../../apollo/client');
+const { getEDecimals } = require('../../../utils/getEDecimals');
+
+const getPolypupApys = async () => {
+  const all = getMasterChefApys({
+    masterchef: swamp.masterchef,
+    masterchefAbi: MasterChefAbi,
+    tokenPerBlock: 'SwampPerBlock',
+    hasMultiplier: false,
+    singlePools: singlePools,
+    oracle: 'tokens',
+    oracleId: pSWAMP.symbol,
+    decimals: getEDecimals(pSWAMP.decimals),
+    tradingFeeInfoClient: quickClient,
+    liquidityProviderFee: QUICK_LPF,
+    // log: true,
+  });
+
+  let apys = {};
+  let apyBreakdowns = {};
+
+  let promises = [all];
+  const results = await Promise.allSettled(promises);
+
+  for (const result of results) {
+    if (result.status !== 'fulfilled') {
+      console.warn('getSwampApys error', result.reason);
+      continue;
+    }
+
+    // Set default APY values
+    let mappedApyValues = result.value;
+    let mappedApyBreakdownValues = {};
+
+    // Loop through key values and move default breakdown format
+    // To require totalApy key
+    for (const [key, value] of Object.entries(result.value)) {
+      mappedApyBreakdownValues[key] = {
+        totalApy: value,
+      };
+    }
+
+    // Break out to apy and breakdowns if possible
+    let hasApyBreakdowns = 'apyBreakdowns' in result.value;
+    if (hasApyBreakdowns) {
+      mappedApyValues = result.value.apys;
+      mappedApyBreakdownValues = result.value.apyBreakdowns;
+    }
+
+    apys = { ...apys, ...mappedApyValues };
+
+    apyBreakdowns = { ...apyBreakdowns, ...mappedApyBreakdownValues };
+  }
+
+  return {
+    apys,
+    apyBreakdowns,
+  };
+};
+
+module.exports = { getSwampApys };
