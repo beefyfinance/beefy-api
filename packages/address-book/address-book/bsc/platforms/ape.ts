ab8f502c99e656a60973ed9dbd7ddf721d0d884b
0xww <86496823+0xww@users.noreply.github.com>
2021-07-24 12:20:27 -0300

    addressbook / add apeswap to bsc (#307)
    * add cakev2-c98-wbnb
    
    * fix addresses
    
    * add apeswap to bsc

---
+++ packages/address-book/address-book/bsc/platforms/ape.ts
@@ -0,0 +1,6 @@
+export const ape = {
+  masterape: '0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9',
+  router: '0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7',
+  factory: '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6',
+  bifiBananaLp: '0x2ce820319047c407cb952060Df5f7fb3D9A9a688',
+} as const;
