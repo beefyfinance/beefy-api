a2968299d1e0cda61260df0cd71ba153ebfd0f2b
0xww <mynameiswalterwhite@pm.me>
2021-07-27 00:07:37 -0300

    Added bakery addresses

--- packages/address-book/address-book/bsc/index.ts
+++ packages/address-book/address-book/bsc/index.ts
@@ -1,4 +1,5 @@
 import { ape } from './platforms/ape';
+import { bakery } from './platforms/bakery';
 import { beefyfinance } from './platforms/beefyfinance';
 import { pancake } from './platforms/pancake';
 import { ironfinance } from './platforms/ironfinance';
@@ -12,6 +13,7 @@ import { ConstInterface } from '../../types/const';
 const _bsc = {
   platforms: {
     ape,
+    bakery,
     beefyfinance,
     pancake,
     ironfinance,
