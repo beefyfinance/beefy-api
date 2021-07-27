da82ab21d8198ca37fb3ec78a4443aa97c165e9f
0xww <86496823+0xww@users.noreply.github.com>
2021-07-27 00:27:05 -0300

    Added kebab addresses (#311)
    Co-authored-by: Moonster-BSC <83176690+Moonster-BSC@users.noreply.github.com>

--- packages/address-book/address-book/bsc/index.ts
+++ packages/address-book/address-book/bsc/index.ts
@@ -4,6 +4,7 @@ import { pancake } from './platforms/pancake';
 import { ironfinance } from './platforms/ironfinance';
 import { farmhero } from './platforms/farmhero';
 import { ellipsis } from './platforms/ellipsis';
+import { kebab } from './platforms/kebab';
 import { jet } from './platforms/jet';
 import { mdex } from './platforms/mdex';
 import { tokens } from './tokens/tokens';
@@ -19,6 +20,7 @@ const _bsc = {
     ironfinance,
     farmhero,
     ellipsis,
+    kebab,
     jet,
     mdex,
   },
