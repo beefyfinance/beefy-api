97b98c5fdad8efcf4c0a0f45df2da484118bfd7e
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-08-30 13:07:05 +0100

    add elk (#371)

--- packages/address-book/address-book/bsc/index.ts
+++ packages/address-book/address-book/bsc/index.ts
@@ -5,6 +5,7 @@ import { pancake } from './platforms/pancake';
 import { ironfinance } from './platforms/ironfinance';
 import { farmhero } from './platforms/farmhero';
 import { ellipsis } from './platforms/ellipsis';
+import { elk } from './platforms/elk';
 import { wault } from './platforms/wault';
 import { kebab } from './platforms/kebab';
 import { jet } from './platforms/jet';
@@ -23,6 +24,7 @@ const _bsc = {
     ironfinance,
     farmhero,
     ellipsis,
+    elk,
     wault,
     kebab,
     jet,
---
+++ packages/address-book/address-book/bsc/platforms/elk.ts
@@ -0,0 +1,4 @@
+export const elk = {
+  router: '0xA63B831264183D755756ca9AE5190fF5183d65D6',
+  factory: '0x31aFfd875e9f68cd6Cd12Cee8943566c9A4bBA13',
+} as const;
