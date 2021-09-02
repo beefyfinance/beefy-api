aebd8e2265921272f7abe18efaa3150d4f3d20ea
sirbeefalot <sirbeefalot@pm.me>
2021-09-01 22:53:23 -0300

    add multisig on polygon

--- packages/address-book/address-book/polygon/platforms/beefyfinance.ts
+++ packages/address-book/address-book/polygon/platforms/beefyfinance.ts
@@ -2,7 +2,7 @@ const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
 
 export const beefyfinance = {
   strategyOwner: '0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F',
-  vaultOwner: '0x4E2a43a0Bf6480ee8359b7eAE244A9fBe9862Cdf',
+  vaultOwner: '0x09dc95959978800E57464E962724a34Bb4Ac1253',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
   treasurer: '0x565EB5e5B21F97AE9200D121e77d2760FFf106cb',
