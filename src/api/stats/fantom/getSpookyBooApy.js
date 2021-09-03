c84b8fc5b2a12e92622e9eb26394043652cd3229
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-09-03 17:17:17 +0100

    add spooky xboo

--- src/api/stats/fantom/getSpookyBooApy.js
+++ src/api/stats/fantom/getSpookyBooApy.js
@@ -1,58 +1,90 @@
 const BigNumber = require('bignumber.js');
-const { fantomWeb3: web3, web3Factory } = require('../../../utils/web3');
+const { fantomWeb3: web3 } = require('../../../utils/web3');
 
-const MasterChef = require('../../../abis/fantom/SpookyIFO.json');
-const fetchPrice = require('../../../utils/fetchPrice');
-const { compound } = require('../../../utils/compound');
-
+const xBOOChefAbi = require('../../../abis/fantom/xBOOChef.json');
 const ERC20 = require('../../../abis/ERC20.json');
+const fetchPrice = require('../../../utils/fetchPrice');
+const pool = require('../../../data/fantom/spookySinglePool.json');
+const { BASE_HPY } = require('../../../constants');
+const { compound } = require('../../../utils/compound');
+const getFarmWithTradingFeesApy = require('../../../utils/getFarmWithTradingFeesApy');
+const { getYearlyPlatformTradingFees } = require('../../../utils/getTradingFeeApr');
+const { spookyClient } = require('../../../apollo/client');
 
-const masterchef = '0xACACa07e398d4946AD12232F40f255230e73Ca72';
-const boo = '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE';
-const oracleId = 'BOO';
 const oracle = 'tokens';
+const oracleId = 'BOO';
+const BOO = '0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE'
+const xBOOChefAddress = '0x2352b745561e7e6FCD03c093cE7220e3e126ace0';
+const xBOO = '0xa48d959AE2E88f1dAA7D5F611E01908106dE7598';
 const DECIMALS = '1e18';
 
+const SECONDS_PER_YEAR = 31536000;
+
+const liquidityProviderFee = 0.0003;
+const beefyPerformanceFee = 0.045;
+const shareAfterBeefyPerformanceFee = 1 - beefyPerformanceFee;
+
 const getSpookyBooApy = async () => {
-  const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
-    getYearlyRewardsInUsd(),
-    getTotalStakedInUsd(),
-  ]);
-  const simpleApy = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
-  const apy = compound(simpleApy, process.env.BASE_HPY, 1, 0.955);
-  //console.log('boo-boo', simpleApy.valueOf(), apy, totalStakedInUsd.valueOf(), yearlyRewardsInUsd.valueOf());
-  return { 'boo-boo': apy };
+  const BOOPrice = await fetchPrice({ oracle, id: oracleId });
+  const { balance, rewardRate } = await getPoolData();
+
+  const BOOContract = new web3.eth.Contract(ERC20, BOO);
+  const totalStakedInxBOO = await BOOContract.methods.balanceOf(xBOO).call();
+  const totalStakedInxBOOInUsd = new BigNumber(totalStakedInxBOO)
+    .times(BOOPrice)
+    .dividedBy(DECIMALS);
+
+  const xBOOContract = new web3.eth.Contract(ERC20, xBOO);
+  const xBOOTotalSupply = await xBOOContract.methods.totalSupply().call();
+  const xBOOPrice = totalStakedInxBOOInUsd.dividedBy(xBOOTotalSupply).times(DECIMALS);
+
+  const yearlyTradingFees = await getYearlyPlatformTradingFees(spookyClient, liquidityProviderFee);
+
+  const totalStakedInUsd = balance.times(xBOOPrice).dividedBy(DECIMALS);
+
+  const rewardPrice = await fetchPrice({ oracle, id: pool.rewardTokenName });
+  const yearlyRewards = rewardRate.times(SECONDS_PER_YEAR);
+  const yearlyRewardsInUsd = yearlyRewards.times(rewardPrice).dividedBy(pool.rewardDecimals);
+
+  const simpleApr = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
+  const vaultApr = simpleApr.times(shareAfterBeefyPerformanceFee);
+  const vaultApy = compound(simpleApr, BASE_HPY, 1, shareAfterBeefyPerformanceFee);
+  const tradingApr = yearlyTradingFees.div(totalStakedInxBOOInUsd);
+  const totalApy = getFarmWithTradingFeesApy(
+    simpleApr,
+    tradingApr,
+    BASE_HPY,
+    1,
+    shareAfterBeefyPerformanceFee
+  );
+  const apys = { 'boo-boo': totalApy };
+
+  // Create reference for breakdown /apy
+  const apyBreakdowns = {
+    'boo-boo': {
+      vaultApr: vaultApr.toNumber(),
+      compoundingsPerYear: BASE_HPY,
+      beefyPerformanceFee: beefyPerformanceFee,
+      vaultApy: vaultApy,
+      lpFee: liquidityProviderFee,
+      tradingApr: tradingApr.toNumber(),
+      totalApy: totalApy,
+    },
+  };
+
+  return {
+    apys,
+    apyBreakdowns,
+  };
 };
 
-const getYearlyRewardsInUsd = async () => {
-  const masterchefContract = new web3.eth.Contract(MasterChef, masterchef);
+const getPoolData = async () => {
+  const xBOOChef = new web3.eth.Contract(xBOOChefAbi, xBOOChefAddress);
+  const rewardPool = await xBOOChef.methods.poolInfo(pool.poolId).call();
+  const rewardRate = new BigNumber(rewardPool.RewardPerSecond);
+  const balance = new BigNumber(rewardPool.xBooStakedAmount);
 
-  const rewards = new BigNumber(await masterchefContract.methods.booPerSecond().call());
-
-  let { allocPoint } = await masterchefContract.methods.poolInfo(0).call();
-  allocPoint = new BigNumber(allocPoint);
-
-  const totalAllocPoint = new BigNumber(await masterchefContract.methods.totalAllocPoint().call());
-  const poolBlockRewards = rewards.times(allocPoint).dividedBy(totalAllocPoint);
-
-  const secondsPerBlock = 1;
-  const secondsPerYear = 31536000;
-  const yearlyRewards = poolBlockRewards.dividedBy(secondsPerBlock).times(secondsPerYear);
-
-  const tokenPrice = await fetchPrice({ oracle, id: oracleId });
-  const yearlyRewardsInUsd = yearlyRewards.times(tokenPrice).dividedBy(DECIMALS);
-
-  return yearlyRewardsInUsd;
-};
-
-const getTotalStakedInUsd = async () => {
-  const web3 = web3Factory(250);
-
-  const tokenContract = new web3.eth.Contract(ERC20, boo);
-  const totalStaked = new BigNumber(await tokenContract.methods.balanceOf(masterchef).call());
-  const tokenPrice = await fetchPrice({ oracle, id: oracleId });
-  const totalStakedInUsd = totalStaked.times(tokenPrice).dividedBy('1e18');
-  return totalStakedInUsd;
+  return { balance, rewardRate };
 };
 
 module.exports = getSpookyBooApy;
