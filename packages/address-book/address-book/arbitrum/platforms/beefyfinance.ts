06b9b0f7106e32e594a6ef3df74e7b03bf6a81fd
sirbeefalot <sirbeefalot@pm.me>
2021-09-24 20:54:25 -0300

    update arbitrum address-book

--- packages/address-book/address-book/arbitrum/platforms/beefyfinance.ts
+++ packages/address-book/address-book/arbitrum/platforms/beefyfinance.ts
@@ -7,7 +7,7 @@ export const beefyfinance = {
   rewarder: cowllector,
   treasurer: '0x565EB5e5B21F97AE9200D121e77d2760FFf106cb',
   launchpoolOwner: cowllector,
-  rewardPool: '0x3f385082Ee3dFf58ca0a6a7fe44Ea0B5d6b4168E',
+  rewardPool: '0x48F4634c8383aF01BF71AefBC125eb582eb3C74D',
   treasury: '0xc3a4fdcba79DB04b4C3e352b1C467B3Ba909D84A',
   beefyFeeRecipient: '0x107Dbf9c9C0EF2Df114159e5C7DC2baf7C444cFF',
   multicall: '0x13aD51a6664973EbD0749a7c84939d973F247921',
--- packages/address-book/address-book/arbitrum/tokens/tokens.ts
+++ packages/address-book/address-book/arbitrum/tokens/tokens.ts
@@ -19,7 +19,7 @@ const _tokens = {
   BIFI: {
     name: 'Beefy.Finance',
     symbol: 'BIFI',
-    address: '0x97513e975a7fA9072c72C92d8000B0dB90b163c5',
+    address: '0x99C409E5f62E4bd2AC142f17caFb6810B8F0BAAE',
     chainId: 42161,
     decimals: 18,
     website: 'https://www.beefy.finance/',
