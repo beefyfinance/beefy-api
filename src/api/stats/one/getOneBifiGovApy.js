9acc8664f9479b4c9d44cd3238b6f24e62381530
Sebastian Guido <sebastianezequielguido@gmail.com>
2021-10-03 22:41:10 -0300

    add gov pool apy fetching for every chain

---
+++ src/api/stats/one/getOneBifiGovApy.js
@@ -0,0 +1,56 @@
+const BigNumber = require('bignumber.js');
+const { oneWeb3: web3, web3Factory } = require('../../../utils/web3');
+
+const IRewardPool = require('../../../abis/IRewardPool.json');
+const fetchPrice = require('../../../utils/fetchPrice');
+const ERC20 = require('../../../abis/ERC20.json');
+
+const BIFI = '0x6ab6d61428fde76768d7b45d8bfeec19c6ef91a8';
+const REWARDS = '0x5b96bbaca98d777cb736dd89a519015315e00d02';
+const ORACLE = 'tokens';
+const ORACLE_ID = 'BIFI';
+const DECIMALS = '1e18';
+const BLOCKS_PER_DAY = 28800;
+
+const getOneBifiGovApy = async () => {
+  const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
+    getYearlyRewardsInUsd(),
+    getTotalStakedInUsd(),
+  ]);
+
+  const apr = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
+
+  return {
+    apys: {
+      'one-bifi-gov': apr,
+    },
+    apyBreakdowns: {
+      'one-bifi-gov': {
+        vaultApr: apr,
+      },
+    },
+  };
+};
+
+const getYearlyRewardsInUsd = async () => {
+  const onePrice = await fetchPrice({ oracle: ORACLE, id: 'ONE' });
+
+  const rewardPool = new web3.eth.Contract(IRewardPool, REWARDS);
+  const rewardRate = new BigNumber(await rewardPool.methods.rewardRate().call());
+  const yearlyRewards = rewardRate.times(3).times(BLOCKS_PER_DAY).times(365);
+  const yearlyRewardsInUsd = yearlyRewards.times(onePrice).dividedBy(DECIMALS);
+
+  return yearlyRewardsInUsd;
+};
+
+const getTotalStakedInUsd = async () => {
+  const web3 = web3Factory(1666600000);
+
+  const tokenContract = new web3.eth.Contract(ERC20, BIFI);
+  const totalStaked = new BigNumber(await tokenContract.methods.balanceOf(REWARDS).call());
+  const tokenPrice = await fetchPrice({ oracle: ORACLE, id: ORACLE_ID });
+
+  return totalStaked.times(tokenPrice).dividedBy(DECIMALS);
+};
+
+module.exports = getOneBifiGovApy;
