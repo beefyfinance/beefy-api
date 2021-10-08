7591878a188a4fe714f4b0fd5aff02fad7795b21
sircryptomaxi <87971154+sircryptomaxi@users.noreply.github.com>
2021-10-08 12:09:20 +0100

    add file for old lps

--- src/api/stats/getAmmPrices.ts
+++ src/api/stats/getAmmPrices.ts
@@ -149,6 +149,7 @@ import pacocaPools from '../../data/degens/pacocaLpPools.json';
 import jetswapFantomPools from '../../data/fantom/jetswapLpPools.json';
 import tetuPools from '../../data/matic/tetuLpPools.json';
 import geistPools from '../../data/fantom/geistLpPools.json';
+import oldPools from '../../data/archive/oldLpPools.json';
 
 const INIT_DELAY = 0 * 60 * 1000;
 const REFRESH_INTERVAL = 5 * 60 * 1000;
@@ -156,6 +157,7 @@ const REFRESH_INTERVAL = 5 * 60 * 1000;
 // FIXME: if this list grows too big we might hit the ratelimit on initialization everytime
 // Implement in case of emergency -> https://github.com/beefyfinance/beefy-api/issues/103
 const pools = [
+  ...oldPools,
   ...geistPools,
   ...jetswapFantomPools,
   ...tetuPools,
