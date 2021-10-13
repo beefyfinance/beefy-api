e09feea596311bccf5e3ddd796a78825be5a3d26
RM <roman-monk@users.noreply.github.com>
2021-10-13 14:53:45 +0800

    Remove getFantomMasterChefApys

--- src/api/stats/fantom/getSpiritApy.js
+++ src/api/stats/fantom/getSpiritApy.js
@@ -5,52 +5,27 @@ import { MultiCall } from 'eth-multicall';
 import { multicallAddress } from '../../../utils/web3';
 import IRewardPool from '../../../abis/IRewardPool.json';
 import BigNumber from 'bignumber.js';
-import ERC20 from '../../../abis/ERC20.json';
 
-const getMasterChefApys = require('./getFantomMasterChefApys');
-
-const MasterChefAbi = require('../../../abis/fantom/SpiritChef.json');
+const { fantomWeb3: web3 } = require('../../../utils/web3');
+const { FANTOM_CHAIN_ID: chainId, SPIRIT_LPF } = require('../../../constants');
+const { getMasterChefApys } = require('../common/getMasterChefApys');
 const spiritPools = require('../../../data/fantom/spiritPools.json');
 const { spiritClient } = require('../../../apollo/client');
-const { fantomWeb3: web3 } = require('../../../utils/web3');
-const { getRewardPoolApys } = require('../common/getRewardPoolApys');
-const { FANTOM_CHAIN_ID: chainId, SPIRIT_LPF } = require('../../../constants');
 const pools = require('../../../data/fantom/spiritGauges.json');
 
 const getSpiritApys = async () => {
-  const single = getMasterChefApys({
-    masterchef: '0x9083EA3756BDE6Ee6f27a6e996806FBD37F6F093',
-    masterchefAbi: MasterChefAbi,
-    tokenPerBlock: 'spiritPerBlock',
-    hasMultiplier: true,
-    singlePools: [
-      {
-        name: 'spirit-spirit',
-        poolId: 0,
-        address: '0x5Cc61A78F164885776AA610fb0FE1257df78E59B',
-        oracle: 'tokens',
-        oracleId: 'SPIRIT',
-        decimals: '1e18',
-      },
-    ],
-    oracle: 'tokens',
-    oracleId: 'SPIRIT',
-    decimals: '1e18',
-    // log: true,
-  });
-
   const spirit = getMasterChefApys({
+    web3: web3,
+    chainId: chainId,
     masterchef: '0x9083EA3756BDE6Ee6f27a6e996806FBD37F6F093',
-    masterchefAbi: MasterChefAbi,
     tokenPerBlock: 'spiritPerBlock',
-    hasMultiplier: true,
     pools: spiritPools.filter(pool => !!pool.poolId),
     oracleId: 'SPIRIT',
     oracle: 'tokens',
     decimals: '1e18',
-    // log: true,
     tradingFeeInfoClient: spiritClient,
     liquidityProviderFee: SPIRIT_LPF,
+    // log: true,
   });
 
   const gaugeAPYs = getGaugeAPYs();
@@ -58,37 +33,14 @@ const getSpiritApys = async () => {
   let apys = {};
   let apyBreakdowns = {};
 
-  let promises = [spirit, single, gaugeAPYs];
-  const results = await Promise.allSettled(promises);
-
+  const results = await Promise.allSettled([spirit, gaugeAPYs]);
   for (const result of results) {
     if (result.status !== 'fulfilled') {
       console.warn('getSpiritApys error', result.reason);
-      continue;
+    } else {
+      apys = { ...apys, ...result.value.apys };
+      apyBreakdowns = { ...apyBreakdowns, ...result.value.apyBreakdowns };
     }
-
-    // Set default APY values
-    let mappedApyValues = result.value;
-    let mappedApyBreakdownValues = {};
-
-    // Loop through key values and move default breakdown format
-    // To require totalApy key
-    for (const [key, value] of Object.entries(result.value)) {
-      mappedApyBreakdownValues[key] = {
-        totalApy: value,
-      };
-    }
-
-    // Break out to apy and breakdowns if possible
-    let hasApyBreakdowns = 'apyBreakdowns' in result.value;
-    if (hasApyBreakdowns) {
-      mappedApyValues = result.value.apys;
-      mappedApyBreakdownValues = result.value.apyBreakdowns;
-    }
-
-    apys = { ...apys, ...mappedApyValues };
-
-    apyBreakdowns = { ...apyBreakdowns, ...mappedApyBreakdownValues };
   }
 
   return {
