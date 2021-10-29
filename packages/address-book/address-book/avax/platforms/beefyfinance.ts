d60f0352b0f94762b9649951d920a3f441fab956
sirb <sirbeefalot@pm.me>
2021-10-29 16:05:03 -0300

    deploy vault owner timelock for avalanceh

--- packages/address-book/address-book/avax/platforms/beefyfinance.ts
+++ packages/address-book/address-book/avax/platforms/beefyfinance.ts
@@ -2,7 +2,7 @@ const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
 
 export const beefyfinance = {
   strategyOwner: '0x37DC61A76113E7840d4A8F1c1B799cC9ac5Aa854',
-  vaultOwner: '0x3Eb7fB70C03eC4AEEC97C6C6C1B59B014600b7F7',
+  vaultOwner: '0x690216f462615b749bEEB5AA3f1d89a2BEc45Ecf',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
   treasurer: '0x3Eb7fB70C03eC4AEEC97C6C6C1B59B014600b7F7',
