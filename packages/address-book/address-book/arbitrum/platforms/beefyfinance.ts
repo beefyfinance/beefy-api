fdf54d317750fd8fe1e518264f0b703a032f0df9
sirb <sirbeefalot@pm.me>
2021-10-29 13:37:20 -0300

    new arbitrum vault owner

--- packages/address-book/address-book/arbitrum/platforms/beefyfinance.ts
+++ packages/address-book/address-book/arbitrum/platforms/beefyfinance.ts
@@ -3,7 +3,7 @@ const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
 export const beefyfinance = {
   devMultisig: '0xf7EC8986c660Fa8269f6440A631B22337f398Ccd',
   strategyOwner: '0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F',
-  vaultOwner: '0x4E2a43a0Bf6480ee8359b7eAE244A9fBe9862Cdf',
+  vaultOwner: '0x9A94784264AaAE397441c1e47fA132BE4e61BdaD',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
   treasurer: '0x3f5eddad52C665A4AA011cd11A21E1d5107d7862',
