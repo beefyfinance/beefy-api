779cc74f990cb5e84da85f85a04b564faefce8e4
sirb <sirbeefalot@pm.me>
2021-11-01 21:41:44 -0300

    Add placeholders for missing moonriver infra

--- packages/address-book/address-book/moonriver/platforms/beefyfinance.ts
+++ packages/address-book/address-book/moonriver/platforms/beefyfinance.ts
@@ -1,15 +1,15 @@
 const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
 
 export const beefyfinance = {
-  devMultisig: '0xf7EC8986c660Fa8269f6440A631B22337f398Ccd',
-  strategyOwner: '0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F',
-  vaultOwner: '0x9A94784264AaAE397441c1e47fA132BE4e61BdaD',
+  devMultisig: cowllector,
+  strategyOwner: '0xc8BD4Ae3d3A69f0d75e3788d2ee557E66EBC98D8',
+  vaultOwner: '0xabCF33106937Ba7f53986F2c339Dd7F1953CE136',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
-  treasurer: '0x3f5eddad52C665A4AA011cd11A21E1d5107d7862',
+  treasurer: '0x3Eb7fB70C03eC4AEEC97C6C6C1B59B014600b7F7',
   launchpoolOwner: cowllector,
-  rewardPool: '0x48F4634c8383aF01BF71AefBC125eb582eb3C74D',
+  rewardPool: '0x4Aabd0d73181325DD1609Ce696eF048702DE7153',
   treasury: '0xB6Fb58eea08b5539f371A744bb9Ef86283F1B3c2',
-  beefyFeeRecipient: '0xabCF33106937Ba7f53986F2c339Dd7F1953CE136',
+  beefyFeeRecipient: cowllector,
   multicall: '0x55f46144bC62e9Af4bAdB71842B62162e2194E90',
 } as const;
