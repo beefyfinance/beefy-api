e0061145140eee1e5656bf4b18c120a08a6e246b
Quinten De Swaef <quintendeswaef@gmail.com>
2021-11-08 10:35:41 +0100

    Feature/joe mai avax (#471)
    * second mai added
    
    * Add ampl
    
    * add to the joe dual pools
    
    * fix joe rewards without rewarder contract
    
    * fixed for no rewarders
    
    * joeLPPools
    
    * yarn lock put back, removed package lock
    
    * as per cryptomaxi's comment: renamed new dual pool
    
    * identical

--- packages/address-book/address-book/avax/platforms/joe.ts
+++ packages/address-book/address-book/avax/platforms/joe.ts
@@ -1,4 +1,5 @@
 export const joe = {
   router: '0x60aE616a2155Ee3d9A68541Ba4544862310933d4',
   chef: '0xd6a4F121CA35509aF06A0Be99093d08462f53052',
+  masterchefV3: '0x188bED1968b795d5c9022F6a0bb5931Ac4c18F00',
 } as const;
