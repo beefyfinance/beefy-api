7cea6379674e198fc8fad34c0d3979b0d89f8906
sirb <sirbeefalot@pm.me>
2021-11-09 16:48:37 -0300

    Update fantom fee recipient

--- packages/address-book/address-book/fantom/platforms/beefyfinance.ts
+++ packages/address-book/address-book/fantom/platforms/beefyfinance.ts
@@ -10,7 +10,7 @@ export const beefyfinance = {
   launchpoolOwner: cowllector,
   rewardPool: '0x7fB900C14c9889A559C777D016a885995cE759Ee',
   treasury: '0xe6CcE165Aa3e52B2cC55F17b1dBC6A8fe5D66610',
-  beefyFeeRecipient: '0x4f8865A1FcE2877cCB55264600D4759d222E8fEB',
+  beefyFeeRecipient: '0x502C107ae28d300fDAedE1CBd7ee8096C1ab4a3C',
   multicall: '0xC9F6b1B53E056fd04bE5a197ce4B2423d456B982',
   bifiMaxiStrategy: '0x230691a28C8290A553BFBC911Ab2AbA0b2df152D',
 } as const;
