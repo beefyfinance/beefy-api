365d4d4bfed1f5fbe577e3f2e72a8c771b28988f
MirthFutures <mirthfutures@gmail.com>
2021-11-17 10:29:11 -0500

    Fix Cronos Gov

--- src/api/stats/cronos/getCronosBifiGovApy.ts
+++ src/api/stats/cronos/getCronosBifiGovApy.ts
@@ -4,14 +4,12 @@ import { cronosWeb3 as web3 } from '../../../utils/web3';
 import IRewardPool from '../../../abis/IRewardPool.json';
 import { ERC20, ERC20_ABI } from '../../../abis/common/ERC20';
 
-import fetchPrice from '../../../utils/fetchPrice';
-import getBlockTime from '../../../utils/getBlockTime';
-
 const BIFI = '0xe6801928061CDbE32AC5AD0634427E140EFd05F9';
 const REWARDS = '0x107Dbf9c9C0EF2Df114159e5C7DC2baf7C444cFF';
 const ORACLE = 'tokens';
 const ORACLE_ID = 'BIFI';
 const DECIMALS = '1e18';
+const SECONDS_PER_YEAR = 31536000;
 
 export const getCronosBifiGovApy = async () => {
   const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
@@ -36,13 +34,9 @@ export const getCronosBifiGovApy = async () => {
 const getYearlyRewardsInUsd = async () => {
   const celoPrice = await fetchPrice({ oracle: ORACLE, id: 'WCRO' });
 
-  const secondsPerYear = new BigNumber(31536000);
-  const secondsPerBlock = await getBlockTime(25);
-  const blocksPerDay = new BigNumber(secondsPerYear.dividedBy(secondsPerBlock));
-
   const rewardPool = new web3.eth.Contract(IRewardPool as any, REWARDS);
   const rewardRate = new BigNumber(await rewardPool.methods.rewardRate().call());
-  const yearlyRewards = rewardRate.times(3).times(blocksPerDay).times(365);
+  const yearlyRewards = rewardRate.times(SECONDS_PER_YEAR);
   const yearlyRewardsInUsd = yearlyRewards.times(celoPrice).dividedBy(DECIMALS);
 
   return yearlyRewardsInUsd;
