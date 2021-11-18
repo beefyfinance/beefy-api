7918a5b159ae4fa5619aa18ed266ad82bb0a330b
Moonster <moonster.bsc@gmail.com>
2021-11-18 11:59:33 -0500

    fix api urls

--- src/api/stats/bifibuyback/etherscanApiUrlMap.ts
+++ src/api/stats/bifibuyback/etherscanApiUrlMap.ts
@@ -1,3 +1,6 @@
+// make sure to sure https://api.* url. The endpoint for api data is different than for the UI data
+// i.e, for https://bscscan.com, use https://api.bscscan.com
+
 export const etherscanApiUrlMap = {
   bsc: {
     url: 'https://api.bscscan.com',
@@ -20,11 +23,11 @@ export const etherscanApiUrlMap = {
     apiToken: 'RAQVQ1H9766QWV3JYF49CCYCB6WAK8STVB',
   },
   moonriver: {
-    url: 'https://moonriver.moonscan.io',
+    url: 'https://api.moonriver.moonscan.io',
     apiToken: 'ERD8CW11NEI89IFQPJ1I5SJTCIDEG9SZCW',
   },
   arbitrum: {
-    url: 'https://arbiscan.io',
+    url: 'https://api.arbiscan.io',
     apiToken: 'QR9KETWZEU64T1TQN4V8VD7H6HIE3X95UW',
   },
 } as const;
