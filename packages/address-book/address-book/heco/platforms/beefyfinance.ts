977f4ef6f9c9bc224a7082d9929880660b0b3b42
sirb <sirbeefalot@pm.me>
2021-11-18 01:58:24 -0300

    Update beefy fee recipients for avas/heco/polygon.

--- packages/address-book/address-book/heco/platforms/beefyfinance.ts
+++ packages/address-book/address-book/heco/platforms/beefyfinance.ts
@@ -9,7 +9,7 @@ export const beefyfinance = {
   launchpoolOwner: cowllector,
   rewardPool: '0x5f7347fedfD0b374e8CE8ed19Fc839F59FB59a3B',
   treasury: '0xf4859A3f36fBcA24BF8299bf56359fB441b03034',
-  beefyFeeRecipient: '0x183D1aaEf1a86De6f16B2737c30eF94a6d2A9308',
+  beefyFeeRecipient: '0x250EB557D7a767ee711c7C11988470669a5306BB',
   multicall: '0x2776CF9B6E2Fa7B33A37139C3CB1ee362Ff0356e',
   bifiMaxiStrategy: '0xfdF1088F5B9Bd338D5197aCfB00b468f04bCC374',
 } as const;
