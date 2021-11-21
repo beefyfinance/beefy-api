447b796145cbaffc51cf75817cad3f29ed9d6d67
sirb <sirbeefalot@pm.me>
2021-11-21 20:53:21 -0300

    Update Beefy addresses for Aurora

--- packages/address-book/address-book/aurora/platforms/beefyfinance.ts
+++ packages/address-book/address-book/aurora/platforms/beefyfinance.ts
@@ -1,16 +1,19 @@
 const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
+const zeroAddress = '0x0000000000000000000000000000000000000000';
+const treasury = '0x1103626d3D0C30B9E2B42181282f23eCE995Bc71';
 
 export const beefyfinance = {
-  devMultisig: '0xf7EC8986c660Fa8269f6440A631B22337f398Ccd',
-  strategyOwner: '0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F',
-  vaultOwner: '0x9A94784264AaAE397441c1e47fA132BE4e61BdaD',
+  devMultisig: zeroAddress,
+  treasuryMultisig: zeroAddress,
+  strategyOwner: '0xc8BD4Ae3d3A69f0d75e3788d2ee557E66EBC98D8',
+  vaultOwner: '0x8B157c6DAeC439b447337A2F08ef055F5182B817',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
   treasurer: '0x3Eb7fB70C03eC4AEEC97C6C6C1B59B014600b7F7',
   launchpoolOwner: cowllector,
-  rewardPool: '0x48F4634c8383aF01BF71AefBC125eb582eb3C74D',
-  treasury: '0xE99c8A590c98c7Ae9FB3B7ecbC115D2eBD533B50',
-  beefyFeeRecipient: '0xabCF33106937Ba7f53986F2c339Dd7F1953CE136',
+  rewardPool: zeroAddress,
+  treasury,
+  beefyFeeRecipient: treasury,
   multicall: '0x1198f78efd67DFc917510aaA07d49545f4B24f11',
-  bifiMaxiStrategy: '0x6207536011918F1A0D8a53Bc426f4Fd54df2E5a8',
+  bifiMaxiStrategy: zeroAddress,
 } as const;
