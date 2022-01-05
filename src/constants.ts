31eeedd02cf3450e92c32b376b8b93c578e81ce2
Aaron Shames <aaron@1coin.io>
2022-01-05 07:26:41 -0500

    fixed

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ const FORTUBE_REQ_MARKETS = 'https://bsc.for.tube/api/v1/bank/markets?mode=exten
 const FORTUBE_API_TOKEN = process.env.FORTUBE_API_TOKEN;
 
 const MAINNET_BSC_RPC_ENDPOINTS = [
-  'https://bsc1.bomb.money',
+  // 'https://bsc1.bomb.money',
   'https://bsc-dataseed1.defibit.io',
   'https://bsc-dataseed1.ninicoin.io',
   'https://bsc-dataseed2.defibit.io',
@@ -35,30 +35,30 @@ const BSC_RPC_ENDPOINTS = CUSTOM_BSC_RPC_ENDPOINTS.length
   : MAINNET_BSC_RPC_ENDPOINTS;
 
 const BSC_RPC = process.env.BSC_RPC || BSC_RPC_ENDPOINTS[0];
-const HECO_RPC = process.env.HECO_RPC || 'https://http-mainnet.hecochain.com';
-const AVAX_RPC = process.env.AVAX_RPC || 'https://api.avax.network/ext/bc/C/rpc';
-const POLYGON_RPC = process.env.POLYGON_RPC || 'https://rpc-mainnet.maticvigil.com/';
-const FANTOM_RPC = process.env.FANTOM_RPC || 'https://rpc.ftm.tools';
-const ONE_RPC = process.env.ONE_RPC || 'https://api.s0.t.hmny.io/';
-const ARBITRUM_RPC = process.env.ARBITRUM_RPC || 'https://arb1.arbitrum.io/rpc';
-const CELO_RPC = process.env.CELO_RPC || 'https://forno.celo.org';
-const MOONRIVER_RPC = process.env.MOONRIVER_RPC || 'https://rpc.moonriver.moonbeam.network';
-const CRONOS_RPC = process.env.CRONOS_RPC || 'https://evm-cronos.crypto.org';
-const AURORA_RPC =
-  process.env.AURORA_RPC ||
-  'https://mainnet.aurora.dev/Fon6fPMs5rCdJc4mxX4kiSK1vsKdzc3D8k6UF8aruek';
+// const HECO_RPC = process.env.HECO_RPC || 'https://http-mainnet.hecochain.com';
+// const AVAX_RPC = process.env.AVAX_RPC || 'https://api.avax.network/ext/bc/C/rpc';
+// const POLYGON_RPC = process.env.POLYGON_RPC || 'https://rpc-mainnet.maticvigil.com/';
+// const FANTOM_RPC = process.env.FANTOM_RPC || 'https://rpc.ftm.tools';
+// const ONE_RPC = process.env.ONE_RPC || 'https://api.s0.t.hmny.io/';
+// const ARBITRUM_RPC = process.env.ARBITRUM_RPC || 'https://arb1.arbitrum.io/rpc';
+// const CELO_RPC = process.env.CELO_RPC || 'https://forno.celo.org';
+// const MOONRIVER_RPC = process.env.MOONRIVER_RPC || 'https://rpc.moonriver.moonbeam.network';
+// const CRONOS_RPC = process.env.CRONOS_RPC || 'https://evm-cronos.crypto.org';
+// const AURORA_RPC =
+//   process.env.AURORA_RPC ||
+//   'https://mainnet.aurora.dev/Fon6fPMs5rCdJc4mxX4kiSK1vsKdzc3D8k6UF8aruek';
 
 const BSC_CHAIN_ID = ChainId.bsc;
-const HECO_CHAIN_ID = ChainId.heco;
-const POLYGON_CHAIN_ID = ChainId.polygon;
-const AVAX_CHAIN_ID = ChainId.avax;
-const FANTOM_CHAIN_ID = ChainId.fantom;
-const ONE_CHAIN_ID = ChainId.one;
-const ARBITRUM_CHAIN_ID = ChainId.arbitrum;
-const CELO_CHAIN_ID = ChainId.celo;
-const MOONRIVER_CHAIN_ID = ChainId.moonriver;
-const CRONOS_CHAIN_ID = ChainId.cronos;
-const AURORA_CHAIN_ID = ChainId.aurora;
+// const HECO_CHAIN_ID = ChainId.heco;
+// const POLYGON_CHAIN_ID = ChainId.polygon;
+// const AVAX_CHAIN_ID = ChainId.avax;
+// const FANTOM_CHAIN_ID = ChainId.fantom;
+// const ONE_CHAIN_ID = ChainId.one;
+// const ARBITRUM_CHAIN_ID = ChainId.arbitrum;
+// const CELO_CHAIN_ID = ChainId.celo;
+// const MOONRIVER_CHAIN_ID = ChainId.moonriver;
+// const CRONOS_CHAIN_ID = ChainId.cronos;
+//const AURORA_CHAIN_ID = ChainId.aurora;
 
 const DFYN_LPF = 0.003;
 const SUSHI_LPF = 0.0025;
@@ -74,16 +74,16 @@ const SOLAR_LPF = 0.002;
 
 const MULTICHAIN_RPC: Record<ChainId, string> = {
   [ChainId.bsc]: BSC_RPC,
-  [ChainId.heco]: HECO_RPC,
-  [ChainId.polygon]: POLYGON_RPC,
-  [ChainId.avax]: AVAX_RPC,
-  [ChainId.fantom]: FANTOM_RPC,
-  [ChainId.one]: ONE_RPC,
-  [ChainId.arbitrum]: ARBITRUM_RPC,
-  [ChainId.celo]: CELO_RPC,
-  [ChainId.moonriver]: MOONRIVER_RPC,
-  [ChainId.cronos]: CRONOS_RPC,
-  [ChainId.aurora]: AURORA_RPC,
+  // [ChainId.heco]: HECO_RPC,
+  // [ChainId.polygon]: POLYGON_RPC,
+  // [ChainId.avax]: AVAX_RPC,
+  // [ChainId.fantom]: FANTOM_RPC,
+  // [ChainId.one]: ONE_RPC,
+  // [ChainId.arbitrum]: ARBITRUM_RPC,
+  // [ChainId.celo]: CELO_RPC,
+  // [ChainId.moonriver]: MOONRIVER_RPC,
+  // [ChainId.cronos]: CRONOS_RPC,
+  // [ChainId.aurora]: AURORA_RPC,
 };
 
 const BSC_VAULTS_ENDPOINT =
@@ -134,35 +134,35 @@ export {
   BSC_RPC_ENDPOINTS,
   BSC_CHAIN_ID,
   BSC_VAULTS_ENDPOINT,
-  HECO_RPC,
-  HECO_CHAIN_ID,
+  // HECO_RPC,
+  // HECO_CHAIN_ID,
   //HECO_VAULTS_ENDPOINT,
-  AVAX_RPC,
-  AVAX_CHAIN_ID,
+  // AVAX_RPC,
+  // AVAX_CHAIN_ID,
   //  AVAX_VAULTS_ENDPOINT,
-  POLYGON_RPC,
-  POLYGON_CHAIN_ID,
+  // POLYGON_RPC,
+  // POLYGON_CHAIN_ID,
   //  POLYGON_VAULTS_ENDPOINT,
-  FANTOM_RPC,
-  FANTOM_CHAIN_ID,
+  // FANTOM_RPC,
+  // FANTOM_CHAIN_ID,
   // FANTOM_VAULTS_ENDPOINT,
-  ONE_RPC,
-  ONE_CHAIN_ID,
+  // ONE_RPC,
+  // ONE_CHAIN_ID,
   // ONE_VAULTS_ENDPOINT,
-  ARBITRUM_RPC,
-  ARBITRUM_CHAIN_ID,
+  // ARBITRUM_RPC,
+  // ARBITRUM_CHAIN_ID,
   // ARBITRUM_VAULTS_ENDPOINT,
-  CELO_RPC,
-  CELO_CHAIN_ID,
+  // CELO_RPC,
+  // CELO_CHAIN_ID,
   // CELO_VAULTS_ENDPOINT,
-  MOONRIVER_RPC,
-  MOONRIVER_CHAIN_ID,
+  // MOONRIVER_RPC,
+  // MOONRIVER_CHAIN_ID,
   // MOONRIVER_VAULTS_ENDPOINT,
-  CRONOS_RPC,
-  CRONOS_CHAIN_ID,
+  // CRONOS_RPC,
+  // CRONOS_CHAIN_ID,
   // CRONOS_VAULTS_ENDPOINT,
-  AURORA_RPC,
-  AURORA_CHAIN_ID,
+  // AURORA_RPC,
+  // AURORA_CHAIN_ID,
   // AURORA_VAULTS_ENDPOINT,
   BASE_HPY,
   MINUTELY_HPY,
