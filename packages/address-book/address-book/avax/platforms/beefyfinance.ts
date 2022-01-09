c0f2232a96016d08b6bde175d66cb7bb058eb60b
sirb <sirbeefalot@pm.me>
2022-01-08 21:11:14 -0300

    update avalanche treasurer

--- packages/address-book/address-book/avax/platforms/beefyfinance.ts
+++ packages/address-book/address-book/avax/platforms/beefyfinance.ts
@@ -1,11 +1,13 @@
 const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
+const treasuryMultisig = '0x26dE4EBffBE8d3d632A292c972E3594eFc2eCeEd';
 
 export const beefyfinance = {
   strategyOwner: '0x37DC61A76113E7840d4A8F1c1B799cC9ac5Aa854',
   vaultOwner: '0x690216f462615b749bEEB5AA3f1d89a2BEc45Ecf',
   keeper: '0x10aee6B5594942433e7Fc2783598c979B030eF3D',
   rewarder: cowllector,
-  treasurer: '0x3Eb7fB70C03eC4AEEC97C6C6C1B59B014600b7F7',
+  treasuryMultisig,
+  treasurer: treasuryMultisig,
   launchpoolOwner: cowllector,
   devMultisig: '0x3A0b8B7a3ea8D1670e000b1Da5bD41373bF8da42',
   rewardPool: '0x86d38c6b6313c5A3021D68D1F57CF5e69197592A',
