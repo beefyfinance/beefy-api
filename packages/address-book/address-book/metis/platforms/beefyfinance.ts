e17e320fe8a3a212eb9db9ff02e01df70fa46809
MirthFutures <weso@DESKTOP-SIUGBAI.localdomain>
2022-01-15 14:33:07 -0500

    Add Multisigs

--- packages/address-book/address-book/metis/platforms/beefyfinance.ts
+++ packages/address-book/address-book/metis/platforms/beefyfinance.ts
@@ -1,9 +1,9 @@
 const cowllector = '0xd529b1894491a0a26B18939274ae8ede93E81dbA';
 const zeroAddress = '0x0000000000000000000000000000000000000000';
-const treasuryMultisig = '0x0000000000000000000000000000000000000000';
+const treasuryMultisig = '0x0f9602B7E7146a9BaE16dB948281BebDb7C2D095';
 
 export const beefyfinance = {
-  devMultisig: zeroAddress,
+  devMultisig: '0xFf9810A3dA8a554B84Ed79D67461eCA6Eb3fA9BD',
   treasuryMultisig,
   strategyOwner: zeroAddress,
   vaultOwner: zeroAddress,
@@ -12,7 +12,7 @@ export const beefyfinance = {
   treasurer: treasuryMultisig,
   launchpoolOwner: cowllector,
   rewardPool: zeroAddress,
-  treasury: zeroAddress,
+  treasury: treasuryMultisig,
   beefyFeeRecipient: zeroAddress,
   multicall: '0x13C6bCC2411861A31dcDC2f990ddbe2325482222',
   bifiMaxiStrategy: zeroAddress,
