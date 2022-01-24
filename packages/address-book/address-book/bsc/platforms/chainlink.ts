97a98b73868abb8ea89eb526b2f42ca887993bed
0xww <mynameiswalterwhite@pm.me>
2022-01-24 15:04:34 -0300

    add chainlink platform

---
+++ packages/address-book/address-book/bsc/platforms/chainlink.ts
@@ -0,0 +1,4 @@
+export const chainlink = {
+  VRFCoordinator: '0x747973a5A2a4Ae1D3a8fDF5479f1514F65Db9C31',
+  keyhash: "0xc251acd21ec4fb7f31bb8868288bfdbaeb4fbfec2df3735ddbd4f7dc8d60103c",
+} as const;
---
+++ packages/address-book/address-book/polygon/platforms/chainlink.ts
@@ -0,0 +1,4 @@
+export const chainlink = {
+  VRFCoordinator: '0x3d2341ADb2D31f1c5530cDC622016af293177AE0',
+  keyhash: "0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da",
+} as const;
