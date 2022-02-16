db19fea8bdd7416443602ff6eb1eb9a8f6ebce98
MirthFutures <mirthfutures@gmail.com>
2022-02-16 07:27:21 -0500

    Fetch Solidly Pools

--- src/api/stats/getAmmPrices.ts
+++ src/api/stats/getAmmPrices.ts
@@ -208,6 +208,8 @@ import creditumPools from '../../data/fantom/creditumPools.json';
 import ripaePools from '../../data/fantom/ripaeLpPools.json';
 import beamswapPools from '../../data/moonbeam/beamswapLpPools.json';
 import darkCryptoPools from '../../data/cronos/darkCryptoLpPools.json';
+import solidlyPools from '../../data/fantom/solidlyLpPools.json';
+import { solidly } from '../../../packages/address-book/address-book/fantom/platforms/solidly';
 
 const INIT_DELAY = 0 * 60 * 1000;
 const REFRESH_INTERVAL = 5 * 60 * 1000;
@@ -215,6 +217,7 @@ const REFRESH_INTERVAL = 5 * 60 * 1000;
 // FIXME: if this list grows too big we might hit the ratelimit on initialization everytime
 // Implement in case of emergency -> https://github.com/beefyfinance/beefy-api/issues/103
 const pools = [
+  ...solidlyPools,
   ...darkCryptoPools,
   ...beamswapPools,
   ...ripaePools,
