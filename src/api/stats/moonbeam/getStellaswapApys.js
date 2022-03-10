9c4b7daa57af0988bc036c42de1ce70e894e4d10
andreb.0x <andreb.0x00@gmail.com>
2022-03-10 16:10:27 -0500

    getstellaawp

---
+++ src/api/stats/moonbeam/getStellaswapApys.js
@@ -0,0 +1,19 @@
+const { moonbeamWeb3: web3 } = require('../../../utils/web3');
+import { MOONBEAM_CHAIN_ID as chainId } from '../../../constants';
+import { getMultiRewardMasterChefApys } from '../common/getMultiRewardMasterChefApys';
+const pools = require('../../../data/moonbeam/stellaswapLpPools.json');
+
+const getSolarFlareApys = async () =>
+  await getMultiRewardMasterChefApys({
+    web3: web3,
+    chainId: chainId,
+    masterchef: '0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E',
+    secondsPerBlock: 1,
+    pools: pools,
+    oracleId: 'STELLA',
+    oracle: 'tokens',
+    decimals: '1e18',
+    // log: true,
+  });
+
+module.exports = { getStellaswapApys };
