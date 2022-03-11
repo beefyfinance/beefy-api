cc6abe8439a646853a4dd515490c267aedd44c39
andreb.0x <andreb.0x00@gmail.com>
2022-03-11 10:05:37 -0500

    platform addrsbook

--- packages/address-book/address-book/moonbeam/platforms/stellaswap.ts
+++ packages/address-book/address-book/moonbeam/platforms/stellaswap.ts
@@ -1,4 +1,4 @@
-export const solarflare = {
+export const stellaswap = {
   router: '0xd0A01ec574D1fC6652eDF79cb2F880fd47D34Ab1', // UniswapV2Router02
-  masterchef: '0x995da7dfB96B4dd1e2bd954bE384A1e66cBB4b8c',
+  masterchef: '0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E',
 } as const;
