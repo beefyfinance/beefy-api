6cfe71c59691ffc93df103e97c2c20815a9fbb30
andreb0x <98777324+andreb0x@users.noreply.github.com>
2022-03-14 08:40:19 -0400

    Update getStellaswapApys.js

--- src/api/stats/moonbeam/getStellaswapApys.js
+++ src/api/stats/moonbeam/getStellaswapApys.js
@@ -3,7 +3,7 @@ import { MOONBEAM_CHAIN_ID as chainId } from '../../../constants';
 import { getMultiRewardMasterChefApys } from '../common/getMultiRewardMasterChefApys';
 const pools = require('../../../data/moonbeam/stellaswapLpPools.json');
 
-const getSolarFlareApys = async () =>
+const getStellaswapApys = async () =>
   await getMultiRewardMasterChefApys({
     web3: web3,
     chainId: chainId,
