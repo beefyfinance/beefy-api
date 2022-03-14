41329945ff2bb5f0cebf222406a86f52993108b5
andreb.0x <andreb.0x00@gmail.com>
2022-03-14 16:18:17 -0400

    fix getapy

--- src/api/stats/moonbeam/getStellaswapApys.js
+++ src/api/stats/moonbeam/getStellaswapApys.js
@@ -1,18 +1,23 @@
 const { moonbeamWeb3: web3 } = require('../../../utils/web3');
 import { MOONBEAM_CHAIN_ID as chainId } from '../../../constants';
-import { getMultiRewardMasterChefApys } from '../common/getMultiRewardMasterChefApys';
+import { getMasterChefApys } from '../common/getMasterChefApys';
 const pools = require('../../../data/moonbeam/stellaswapLpPools.json');
+const getBlockTime = require('../../../utils/getBlockTime');
 
 const getStellaswapApys = async () =>
-  await getMultiRewardMasterChefApys({
+  await getMasterChefApys({
     web3: web3,
     chainId: chainId,
     masterchef: '0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E',
-    secondsPerBlock: 1,
+    tokenPerBlock: 'stellaPerBlock',
+    hasMultiplier: false,
+    secondsPerBlock: await getBlockTime(1284),
     pools: pools,
     oracleId: 'STELLA',
     oracle: 'tokens',
     decimals: '1e18',
+    tradingFeeInfoClient: 'None',
+    liquidityProviderFee: 0.005,
     // log: true,
   });
 
