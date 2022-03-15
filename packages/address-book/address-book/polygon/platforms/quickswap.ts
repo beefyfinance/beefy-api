b133bda1ab032271d9d3c7b9a247f857d3490750
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-03-14 23:16:40 -0400

    add quickswap stakingRewards to address-book

--- packages/address-book/address-book/polygon/platforms/quickswap.ts
+++ packages/address-book/address-book/polygon/platforms/quickswap.ts
@@ -1,4 +1,6 @@
 export const quickswap = {
   router: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff', // UniswapV2Router02
+  stakingDualRewards: '0x3C943fB971BCaB9C4919e5FA71612655Ebd663e5',
+  stakingRewards: '0xee61B0C32ADf887d265236f57AC0a2449CC931C7',
   wethBifiLp: '0x8b80417D92571720949fC22404200AB8FAf7775f',
 } as const;
