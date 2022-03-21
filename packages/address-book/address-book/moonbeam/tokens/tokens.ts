8c3f8511946299d390b125cc885ef69939e5b083
andreb0x <98777324+andreb0x@users.noreply.github.com>
2022-03-21 05:17:10 -0400

    remaining stella vaults (#693)
    * add remaining stella farms
    
    * include matic
    
    * Update tokens.ts

--- packages/address-book/address-book/moonbeam/tokens/tokens.ts
+++ packages/address-book/address-book/moonbeam/tokens/tokens.ts
@@ -149,6 +149,17 @@ const _tokens = {
     description:
       'Fantom is a fast, high-throughput open-source smart contract platform for digital assets and dApps.',
   },
+  MATIC: {
+    name: 'Matic',
+    symbol: 'MATIC',
+    address: '0x3405A1bd46B85c5C029483FbECf2F3E611026e45',
+    chainId: 1284,
+    decimals: 18,
+    logoURI: 'None',
+    website: 'https://polygon.technology/',
+    description:
+      'Polygon believes in Web3 for all. Polygon is a decentralised Ethereum scaling platform that enables developers to build scalable user-friendly dApps with low transaction fees without ever sacrificing on security.',
+  },
   AVAX: {
     name: 'Avalanche',
     symbol: 'AVAX',
@@ -232,6 +243,18 @@ const _tokens = {
     website: 'https://wbtc.network/',
     description:
       'Wrapped Bitcoin (WBTC) is the first ERC20 token backed 1:1 with Bitcoin. Completely transparent. 100% verifiable. Community led.',
+  },
+  aWBTC: {
+    name: 'Anyswap Wrapped BTC',
+    symbol: 'WBTC',
+    address: '0x922D641a426DcFFaeF11680e5358F34d97d112E1',
+    chainId: 1284,
+    decimals: 8,
+    logoURI:
+      'https://pancakeswap.finance/images/tokens/0x922D641a426DcFFaeF11680e5358F34d97d112E1.svg',
+    website: 'https://wbtc.network/',
+    description:
+      'Anyswap Wrapped Bitcoin (WBTC) is the first ERC20 token backed 1:1 with Bitcoin. Completely transparent. 100% verifiable. Community led.',
   },
   LUNA: {
     name: 'Axelar Wrapped LUNA',
