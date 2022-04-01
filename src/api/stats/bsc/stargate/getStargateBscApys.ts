50e29f38fd1ecfa7b5f413587fa9e829e4a4456b
pumpingGhost <fneustifter@gmail.com>
2022-04-01 22:13:21 +0200

    Clean up apy scripts + added matic and avax

--- src/api/stats/bsc/stargate/getStargateBscApys.ts
+++ src/api/stats/bsc/stargate/getStargateBscApys.ts
@@ -1,54 +1,35 @@
 import { bscWeb3 as web3 } from '../../../../utils/web3';
-import { spookyClient } from '../../../../apollo/client';
-import getApyBreakdown from '../../common/getApyBreakdown';
-import { MasterChefApysParams } from '../../common/getMasterChefApys';
-import { getStargateFarmApys } from '../../common/stargate/getStargateApyData';
+const { getMasterChefApys } = require('../../common/getMasterChefApys');
+import { BSC_CHAIN_ID } from '../../../../constants';
 
-const getStargateBscApys = async () => {
-  const masterchefParams: MasterChefApysParams = {
+const getStargateBscApys = async () =>
+  await getMasterChefApys({
     web3: web3,
-    chainId: 56,
+    chainId: BSC_CHAIN_ID,
     masterchef: '0x3052A0F6ab15b4AE1df39962d5DdEFacA86DaB47',
     tokenPerBlock: 'stargatePerBlock',
     hasMultiplier: false,
-    pools: [],
     singlePools: [
       {
         name: 'stargate-bsc-busd',
         poolId: 1,
         address: '0x98a5737749490856b401DB5Dc27F522fC314A4e1',
-        chainId: 56,
         oracle: 'tokens',
         oracleId: 'BUSD',
-        decimals: '1e18',
+        decimals: '1e6',
       },
       {
         name: 'stargate-bsc-usdt',
         poolId: 0,
         address: '0x9aA83081AA06AF7208Dcc7A4cB72C94d057D2cda',
-        chainId: 56,
         oracle: 'tokens',
         oracleId: 'USDT',
-        decimals: '1e18',
+        decimals: '1e6',
       },
     ],
     oracleId: 'STG',
     oracle: 'tokens',
     decimals: '1e18',
-    allocPointIndex: '1',
-    tradingFeeInfoClient: spookyClient,
-    liquidityProviderFee: 0.003,
-  };
-  masterchefParams.pools = [
-    ...(masterchefParams.pools ?? []),
-    ...(masterchefParams.singlePools ?? []),
-  ];
-
-  const farmApys = await getStargateFarmApys(masterchefParams);
-
-  const liquidityProviderFee = masterchefParams.liquidityProviderFee ?? 0.003;
-
-  return getApyBreakdown(masterchefParams.pools, {}, farmApys, liquidityProviderFee);
-};
+  });
 
 module.exports = getStargateBscApys;
--- src/api/stats/fantom/getStargateFantomApys.ts
+++ src/api/stats/fantom/getStargateFantomApys.ts
@@ -1,22 +1,17 @@
-import { fantomWeb3 as web3 } from '../../../utils/web3';
-import { spookyClient } from '../../../apollo/client';
-import getApyBreakdown from '../common/getApyBreakdown';
-import { MasterChefApysParams } from '../common/getMasterChefApys';
-import { getStargateFarmApys } from '../common/stargate/getStargateApyData';
+const { getMasterChefApys } = require('../common/getMasterChefApys');
+const { fantomWeb3: web3 } = require('../../../utils/web3');
 
-const getStargateFantomApys = async () => {
-  const masterchefParams: MasterChefApysParams = {
+const getStargateFantomApys = async () =>
+  await getMasterChefApys({
     web3: web3,
     chainId: 250,
     masterchef: '0x224D8Fd7aB6AD4c6eb4611Ce56EF35Dec2277F03',
     tokenPerBlock: 'stargatePerBlock',
     hasMultiplier: false,
-    pools: [],
     singlePools: [
       {
         name: 'stargate-fantom-usdc',
         poolId: 0,
-        chainId: 250,
         address: '0x12edeA9cd262006cC3C4E77c90d2CD2DD4b1eb97',
         oracle: 'tokens',
         oracleId: 'USDC',
@@ -26,20 +21,6 @@ const getStargateFantomApys = async () => {
     oracleId: 'STG',
     oracle: 'tokens',
     decimals: '1e18',
-    allocPointIndex: '1',
-    tradingFeeInfoClient: spookyClient,
-    liquidityProviderFee: 0.003,
-  };
-  masterchefParams.pools = [
-    ...(masterchefParams.pools ?? []),
-    ...(masterchefParams.singlePools ?? []),
-  ];
-
-  const farmApys = await getStargateFarmApys(masterchefParams);
-
-  const liquidityProviderFee = masterchefParams.liquidityProviderFee ?? 0.003;
-
-  return getApyBreakdown(masterchefParams.pools, {}, farmApys, liquidityProviderFee);
-};
+  });
 
 module.exports = getStargateFantomApys;
