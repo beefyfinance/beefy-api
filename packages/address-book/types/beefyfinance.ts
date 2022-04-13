9c37175236a075e87ca9d33ea345496b87c8d717
sirb <sirbeefalot@pm.me>
2022-04-13 17:14:24 -0300

    remove 'rewarder' from beefy type

--- packages/address-book/types/beefyfinance.ts
+++ packages/address-book/types/beefyfinance.ts
@@ -2,7 +2,6 @@ export interface BeefyFinance {
   strategyOwner: string;
   vaultOwner: string;
   keeper: string;
-  rewarder: string;
   treasurer: string;
   launchpoolOwner: string;
   rewardPool: string;
