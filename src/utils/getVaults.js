599420530efd820c8b71183b464ddeadfe335b57
chebiN <17909998+seguido@users.noreply.github.com>
2022-04-14 23:38:46 -0300

    only eval once

--- src/utils/getVaults.js
+++ src/utils/getVaults.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch');
 
+const cache = {};
+
 const getVaults = async vaultsEndpoint => {
+  if (vaultsEndpoint in cache) {
+    return cache[vaultsEndpoint];
+  }
   try {
     const response = await fetch(vaultsEndpoint).then(res => res.text());
     const data = response;
     let vaults = '[' + data.substring(data.indexOf('\n') + 1);
     vaults = eval(vaults);
+    cache[vaultsEndpoint] = vaults;
     return vaults;
   } catch (err) {
     console.error(err);
