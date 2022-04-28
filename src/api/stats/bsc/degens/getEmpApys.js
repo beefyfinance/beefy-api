427897a5f29f15e223b1930c3c53ff77b06f91c0
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-04-28 11:35:55 -0400

    Refactor getEmpApys to use common getMasterChefApys()

--- src/api/stats/bsc/degens/getEmpApys.js
+++ src/api/stats/bsc/degens/getEmpApys.js
@@ -1,67 +1,24 @@
 const { bscWeb3: web3 } = require('../../../../utils/web3');
-const BigNumber = require('bignumber.js');
-
-const RewardPool = require('../../../../abis/degens/EmpRewardPool.json');
+const { BSC_CHAIN_ID: chainId, PCS_LPF } = require('../../../../constants');
+const { getMasterChefApys } = require('../../common/getMasterChefApys');
 const pools = require('../../../../data/degens/empLpPools.json');
-const fetchPrice = require('../../../../utils/fetchPrice');
-const { getTotalLpStakedInUsd } = require('../../../../utils/getTotalStakedInUsd');
-const { getTradingFeeApr } = require('../../../../utils/getTradingFeeApr');
 const { cakeClient } = require('../../../../apollo/client');
-import { PCS_LPF } from '../../../../constants';
-import { getContract, getContractWithProvider } from '../../../../utils/contractHelper';
-import getApyBreakdown from '../../common/getApyBreakdown';
 
-const rewardPool = '0x97a68a7949ee30849d273b0c4450314ae26235b1';
-const oracleId = 'ESHARE';
-const oracle = 'tokens';
-const DECIMALS = '1e18';
+const getEmpLpApys = async () =>
+  await getMasterChefApys({
+    web3,
+    chainId,
+    masterchef: '0x97a68a7949ee30849d273b0c4450314ae26235b1',
+    tokenPerBlock: 'tSharePerSecond',
+    hasMultiplier: false,
+    pools,
+    oracleId: 'ESHARE',
+    oracle: 'tokens',
+    decimals: '1e18',
+    secondsPerBlock: 1,
+    tradingFeeInfoClient: cakeClient,
+    liquidityProviderFee: PCS_LPF,
+    // log: true,
+  });
 
-const getEmpApys = async () => {
-  let promises = [];
-  pools.forEach(pool => promises.push(getPoolApy(rewardPool, pool)));
-  const farmAprs = await Promise.all(promises);
-
-  const pairAddresses = pools.map(pool => pool.address);
-
-  const tradingAprs = await getTradingFeeApr(cakeClient, pairAddresses, PCS_LPF);
-
-  return getApyBreakdown(pools, tradingAprs, farmAprs, PCS_LPF);
-};
-
-const getPoolApy = async (rewardPool, pool) => {
-  const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
-    getYearlyRewardsInUsd(rewardPool, pool.poolId),
-    getTotalLpStakedInUsd(rewardPool, pool, pool.chainId),
-  ]);
-
-  return yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
-};
-
-const getYearlyRewardsInUsd = async (rewardPool, poolId) => {
-  const rewardPoolContract = getContractWithProvider(RewardPool, rewardPool, web3);
-
-  let { allocPoint } = await rewardPoolContract.methods.poolInfo(poolId).call();
-  allocPoint = new BigNumber(allocPoint);
-
-  const fromTime = Math.floor(Date.now() / 1000);
-  let [secondRewards, totalAllocPoint] = await Promise.all([
-    rewardPoolContract.methods.getGeneratedReward(fromTime, fromTime + 1).call(),
-    rewardPoolContract.methods.totalAllocPoint().call(),
-  ]);
-
-  secondRewards = new BigNumber(secondRewards);
-  totalAllocPoint = new BigNumber(totalAllocPoint);
-
-  const secondsPerYear = 31536000;
-  const yearlyRewards = secondRewards
-    .times(secondsPerYear)
-    .times(allocPoint)
-    .dividedBy(totalAllocPoint);
-
-  const price = await fetchPrice({ oracle: oracle, id: oracleId });
-  const yearlyRewardsInUsd = yearlyRewards.times(price).dividedBy(DECIMALS);
-
-  return yearlyRewardsInUsd;
-};
-
-module.exports = getEmpApys;
+module.exports = getEmpLpApys;
