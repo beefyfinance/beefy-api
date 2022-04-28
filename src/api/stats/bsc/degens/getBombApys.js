c9067c5ea06c02750211d0831fca7c04de1498db
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-04-28 11:32:10 -0400

    Update API to use common getMasterChefApys()

--- src/api/stats/bsc/degens/getBombApys.js
+++ src/api/stats/bsc/degens/getBombApys.js
@@ -1,67 +1,24 @@
 const { bscWeb3: web3 } = require('../../../../utils/web3');
-const BigNumber = require('bignumber.js');
-
-const RewardPool = require('../../../../abis/fantom/TombRewardPool.json');
+const { BSC_CHAIN_ID: chainId, PCS_LPF } = require('../../../../constants');
+const { getMasterChefApys } = require('../../common/getMasterChefApys');
 const pools = require('../../../../data/degens/bombLpPools.json');
-const fetchPrice = require('../../../../utils/fetchPrice');
-const { getTotalLpStakedInUsd } = require('../../../../utils/getTotalStakedInUsd');
-const { getTradingFeeApr } = require('../../../../utils/getTradingFeeApr');
 const { cakeClient } = require('../../../../apollo/client');
-import { PCS_LPF } from '../../../../constants';
-import { getContractWithProvider } from '../../../../utils/contractHelper';
-import getApyBreakdown from '../../common/getApyBreakdown';
 
-const rewardPool = '0x1083926054069AaD75d7238E9B809b0eF9d94e5B';
-const oracleId = 'BSHARE';
-const oracle = 'tokens';
-const DECIMALS = '1e18';
-
-const getBombLpApys = async () => {
-  let promises = [];
-  pools.forEach(pool => promises.push(getPoolApy(rewardPool, pool)));
-  const farmAprs = await Promise.all(promises);
-
-  const pairAddresses = pools.map(pool => pool.address);
-
-  const tradingAprs = await getTradingFeeApr(cakeClient, pairAddresses, PCS_LPF);
-
-  return getApyBreakdown(pools, tradingAprs, farmAprs, PCS_LPF);
-};
-
-const getPoolApy = async (rewardPool, pool) => {
-  const [yearlyRewardsInUsd, totalStakedInUsd] = await Promise.all([
-    getYearlyRewardsInUsd(rewardPool, pool.poolId),
-    getTotalLpStakedInUsd(rewardPool, pool, pool.chainId),
-  ]);
-
-  return yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
-};
-
-const getYearlyRewardsInUsd = async (rewardPool, poolId) => {
-  const rewardPoolContract = getContractWithProvider(RewardPool, rewardPool, web3);
-
-  let { allocPoint } = await rewardPoolContract.methods.poolInfo(poolId).call();
-  allocPoint = new BigNumber(allocPoint);
-
-  const fromTime = Math.floor(Date.now() / 1000);
-  let [secondRewards, totalAllocPoint] = await Promise.all([
-    rewardPoolContract.methods.getGeneratedReward(fromTime, fromTime + 1).call(),
-    rewardPoolContract.methods.totalAllocPoint().call(),
-  ]);
-
-  secondRewards = new BigNumber(secondRewards);
-  totalAllocPoint = new BigNumber(totalAllocPoint);
-
-  const secondsPerYear = 31536000;
-  const yearlyRewards = secondRewards
-    .times(secondsPerYear)
-    .times(allocPoint)
-    .dividedBy(totalAllocPoint);
-
-  const price = await fetchPrice({ oracle: oracle, id: oracleId });
-  const yearlyRewardsInUsd = yearlyRewards.times(price).dividedBy(DECIMALS);
-
-  return yearlyRewardsInUsd;
-};
+const getBombLpApys = async () =>
+  await getMasterChefApys({
+    web3,
+    chainId,
+    masterchef: '0x1083926054069AaD75d7238E9B809b0eF9d94e5B',
+    tokenPerBlock: 'tSharePerSecond',
+    hasMultiplier: false,
+    pools,
+    oracleId: 'BSHARE',
+    oracle: 'tokens',
+    decimals: '1e18',
+    secondsPerBlock: 1,
+    tradingFeeInfoClient: cakeClient,
+    liquidityProviderFee: PCS_LPF,
+    // log: true,
+  });
 
 module.exports = getBombLpApys;
