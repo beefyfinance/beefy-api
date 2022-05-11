da4101eb1c83e46f87d03605f42d09657af78280
MirthFutures <mirthfutures@gmail.com>
2022-05-11 18:50:04 -0400

    update emerald vault and strat owner

--- packages/address-book/address-book/emerald/platforms/beefyfinance.ts
+++ packages/address-book/address-book/emerald/platforms/beefyfinance.ts
@@ -4,8 +4,8 @@ const treasuryMultisig = '0x8FD0869271d26E6653f5d5650685630F75b6AEDf';
 export const beefyfinance = {
   devMultisig,
   treasuryMultisig,
-  strategyOwner: '0xfcDD5a02C611ba6Fe2802f885281500EC95805d7',
-  vaultOwner: '0xc8F3D9994bb1670F5f3d78eBaBC35FA8FdEEf8a2',
+  strategyOwner: '0xa9E6E271b27b20F65394914f8784B3B860dBd259',
+  vaultOwner: '0xaDB9DDFA24E326dC9d337561f6c7ba2a6Ecec697',
   keeper: '0x340465d9D2EbDE78F15a3870884757584F97aBB4',
   treasurer: treasuryMultisig,
   launchpoolOwner: devMultisig,
