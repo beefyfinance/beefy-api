1605e78aec71fe2b01200a930a57aa09323a9827
chebiN <17909998+seguido@users.noreply.github.com>
2022-06-22 17:26:16 -0300

    v1 of curve breakdown of lp data

--- src/api/stats/common/curve/getCurvePricesCommon.js
+++ src/api/stats/common/curve/getCurvePricesCommon.js
@@ -31,41 +31,116 @@ const getPoolPrice = async (web3, pools, pool, tokenPrices) => {
 };
 
 const getStablePoolPrice = async (web3, pool, tokenPrices) => {
-  const lpContract = getContractWithProvider(ICurvePool, pool.pool, web3);
-  const virtualPrice = new BigNumber(await lpContract.methods.get_virtual_price().call());
-  const tokenPrice = getTokenPrice(tokenPrices, pool.oracleId);
-  const price = virtualPrice.multipliedBy(tokenPrice).dividedBy(DECIMALS).toNumber();
-  return price;
+  try {
+    const lpContract = getContractWithProvider(ICurvePool, pool.pool, web3);
+
+    // Some stable pool totalSupplies can be found in the pool whilst others in the lp contract
+    const supplyContract = getContractWithProvider(ERC20, pool.token ?? pool.pool, web3);
+
+    let promises = [];
+    pool.tokens.forEach((_, index) =>
+      promises.push(getTokenBalanceAndAddress(web3, pool.pool, index))
+    );
+    promises.push(lpContract.methods.get_virtual_price().call());
+    promises.push(supplyContract.methods.totalSupply().call());
+
+    let results = [];
+    results = await Promise.all(promises);
+
+    let tokens = [];
+    let balances = [];
+    for (let i = 0; i < pool.tokens.length; i++) {
+      tokens.push(results[i].tokenAddress);
+      balances.push(
+        new BigNumber(results[i].balance).dividedBy(pool.tokens[i].decimals).toString(10)
+      );
+    }
+
+    const virtualPrice = new BigNumber(results[pool.tokens.length]);
+    const tokenPrice = getTokenPrice(tokenPrices, pool.oracleId);
+    const price = virtualPrice.multipliedBy(tokenPrice).dividedBy(DECIMALS).toNumber();
+
+    const totalSupply = new BigNumber(results[pool.tokens.length + 1])
+      .dividedBy(DECIMALS)
+      .toString(10);
+
+    return {
+      price,
+      tokens,
+      balances,
+      totalSupply,
+    };
+  } catch (err) {
+    console.log('error on pool ' + pool.name);
+    // console.log(pool);
+    throw err;
+  }
 };
 
 const getVolatilePoolPrice = async (web3, pools, pool, tokenPrices) => {
   const promises = [];
   pool.tokens.forEach((token, i) => {
-    promises.push(getTokenBalanceInUsd(web3, pools, pool.pool, token, i, tokenPrices));
+    promises.push(getTokenDetails(web3, pools, pool.pool, token, i, tokenPrices));
   });
   promises.push(getContractWithProvider(ERC20, pool.token, web3).methods.totalSupply().call());
+
   const results = await Promise.all(promises);
 
   let totalBalInUsd = new BigNumber(0);
+  let tokens = [];
+  let balances = [];
   for (let i = 0; i < pool.tokens.length; i++) {
-    totalBalInUsd = totalBalInUsd.plus(results[i]);
+    totalBalInUsd = totalBalInUsd.plus(results[i].usdBalance);
+    tokens.push(results[i].tokenAddress);
+    balances.push(
+      new BigNumber(results[i].balance).dividedBy(pool.tokens[i].decimals).toString(10)
+    );
   }
+
   const totalSupply = new BigNumber(results[pool.tokens.length]).dividedBy(DECIMALS);
   const price = totalBalInUsd.dividedBy(totalSupply).toNumber();
-  return price;
+
+  if (pool.name === 'curve-poly-atricrypto3') {
+    console.log('logging');
+    console.log(pool);
+    console.log('---');
+    console.log(results);
+    console.log('---');
+  }
+
+  return {
+    price,
+    tokens,
+    balances,
+    totalSupply: totalSupply.toString(10),
+  };
 };
 
-const getTokenBalanceInUsd = async (web3, pools, curvePool, token, index, tokenPrices) => {
+const getTokenBalanceAndAddress = async (web3, curvePool, index) => {
   const pool = getContractWithProvider(ICurvePool, curvePool, web3);
-  const balance = await pool.methods.balances(index).call();
+
+  let promises = [pool.methods.balances(index).call(), pool.methods.coins(index).call()];
+  let [balance, tokenAddress] = await Promise.all(promises);
+
+  return { balance, tokenAddress };
+};
+
+const getTokenDetails = async (web3, pools, curvePool, token, index, tokenPrices) => {
+  let { balance, tokenAddress } = await getTokenBalanceAndAddress(web3, curvePool, index);
+
   let price = 1;
   if (token.basePool) {
     const pool = pools.find(p => p.name === token.basePool);
-    price = await getStablePoolPrice(web3, pool, tokenPrices);
+    price = (await getStablePoolPrice(web3, pool, tokenPrices)).price;
   } else if (token.oracleId) {
     price = getTokenPrice(tokenPrices, token.oracleId);
   }
-  return new BigNumber(balance).times(price).dividedBy(token.decimals);
+
+  return {
+    usdBalance: new BigNumber(balance).times(price).dividedBy(token.decimals),
+    balance,
+    tokenAddress,
+  };
 };
 
 const getTokenPrice = (tokenPrices, oracleId) => {
