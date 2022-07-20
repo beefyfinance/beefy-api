a06077dbb8bd71ca43758191f9f64c92dcb5f7a0
RM <roman-monk@users.noreply.github.com>
2022-07-20 19:45:44 +0800

    Fix beamswap

--- src/api/stats/moonbeam/getBeamswapPrices.js
+++ src/api/stats/moonbeam/getBeamswapPrices.js
@@ -1,6 +1,6 @@
 const getStableSwapPrices = require('../common/getStableSwapPrices');
 const { moonbeamWeb3: web3 } = require('../../../utils/web3');
-const pools = require('../../../data/moonbeam/BeamswapStableLpPools.json');
+const pools = require('../../../data/moonbeam/beamswapStableLpPools.json');
 
 const getBeamswapPrices = async tokenPrices => {
   return await getStableSwapPrices(web3, pools, tokenPrices);
