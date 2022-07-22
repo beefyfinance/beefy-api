1e97a43081202034a31ed1ce4f5c60517f868013
lodelux <lodelux@live.it>
2022-07-22 10:10:08 +0200

    change to getMasterChef

--- src/api/stats/arbitrum/getRipaeApys.js
+++ src/api/stats/arbitrum/getRipaeApys.js
@@ -1,120 +1,25 @@
-const BigNumber = require('bignumber.js');
-const { MultiCall } = require('eth-multicall');
-const { arbitrumWeb3: web3, multicallAddress } = require('../../../utils/web3');
-
-const MasterChef = require('../../../abis/arbitrum/RipaeRewardPool.json');
-const ERC20 = require('../../../abis/ERC20.json');
-const fetchPrice = require('../../../utils/fetchPrice');
+const { arbitrumWeb3: web3 } = require('../../../utils/web3');
+const { ARBITRUM_CHAIN_ID: chainId } = require('../../../constants');
+const { getMasterChefApys } = require('../common/getMasterChefApys');
 const pools = require('../../../data/arbitrum/ripaeLpPools.json');
-const { BASE_HPY, ARBITRUM_CHAIN_ID } = require('../../../constants');
-const { getTradingFeeApr } = require('../../../utils/getTradingFeeApr');
-import { getFarmWithTradingFeesApy } from '../../../utils/getFarmWithTradingFeesApy';
-const { sushiArbitrumClient } = require('../../../apollo/client');
-const { compound } = require('../../../utils/compound');
-import { getContract, getContractWithProvider } from '../../../utils/contractHelper';
+import { sushiArbitrumClient } from '../../../apollo/client';
+import { SUSHI_LPF } from '../../../constants';
 
-const masterchef = '0x4d1D896FD501788d8605f672AD72fC05Fe5ab311';
-const oracleId = 'sETH';
-const oracle = 'tokens';
-const DECIMALS = '1e18';
-
-const secondsPerYear = 31536000;
-
-const liquidityProviderFee = 0.003;
-const beefyPerformanceFee = 0.045;
-const shareAfterBeefyPerformanceFee = 1 - beefyPerformanceFee;
-
-const getRipaeApys = async () => {
-  let apys = {};
-  let apyBreakdowns = {};
-
-  const secondsPerBlock = 1;
-  const tokenPrice = await fetchPrice({ oracle: oracle, id: oracleId });
-  const { rewardPerSecond, totalAllocPoint } = await getMasterChefData();
-  const { balances, allocPoints } = await getPoolsData(pools);
-
-  const pairAddresses = pools.map(pool => pool.address);
-  const tradingAprs = await getTradingFeeApr(
-    sushiArbitrumClient,
-    pairAddresses,
-    liquidityProviderFee
-  );
-
-  for (let i = 0; i < pools.length; i++) {
-    const pool = pools[i];
-
-    const lpPrice = await fetchPrice({ oracle: 'lps', id: pool.name });
-    const totalStakedInUsd = balances[i].times(lpPrice).dividedBy('1e18');
-
-    const poolBlockRewards = rewardPerSecond.times(allocPoints[i]).dividedBy(totalAllocPoint);
-    const yearlyRewards = poolBlockRewards.dividedBy(secondsPerBlock).times(secondsPerYear);
-    const yearlyRewardsInUsd = yearlyRewards.times(tokenPrice).dividedBy(DECIMALS);
-
-    const simpleApy = yearlyRewardsInUsd.dividedBy(totalStakedInUsd);
-    const vaultApr = simpleApy.times(shareAfterBeefyPerformanceFee);
-    const vaultApy = compound(simpleApy, BASE_HPY, 1, shareAfterBeefyPerformanceFee);
-
-    const tradingApr = tradingAprs[pool.address.toLowerCase()] ?? new BigNumber(0);
-    const totalApy = getFarmWithTradingFeesApy(
-      simpleApy,
-      tradingApr,
-      BASE_HPY,
-      1,
-      shareAfterBeefyPerformanceFee
-    );
-
-    const legacyApyValue = { [pool.name]: totalApy };
-
-    apys = { ...apys, ...legacyApyValue };
-
-    const componentValues = {
-      [pool.name]: {
-        vaultApr: vaultApr.toNumber(),
-        compoundingsPerYear: BASE_HPY,
-        beefyPerformanceFee: beefyPerformanceFee,
-        vaultApy: vaultApy,
-        lpFee: liquidityProviderFee,
-        tradingApr: tradingApr.toNumber(),
-        totalApy: totalApy,
-      },
-    };
-
-    apyBreakdowns = { ...apyBreakdowns, ...componentValues };
-  }
-
-  return {
-    apys,
-    apyBreakdowns,
-  };
-};
-
-const getMasterChefData = async () => {
-  const masterchefContract = getContractWithProvider(MasterChef, masterchef, web3);
-  const rewardPerSecond = new BigNumber(await masterchefContract.methods.rewardPerSecond(0).call());
-  const totalAllocPoint = new BigNumber(await masterchefContract.methods.totalAllocPoint().call());
-  return { rewardPerSecond, totalAllocPoint };
-};
-
-const getPoolsData = async pools => {
-  const masterchefContract = getContract(MasterChef, masterchef);
-  const multicall = new MultiCall(web3, multicallAddress(ARBITRUM_CHAIN_ID));
-  const balanceCalls = [];
-  const allocPointCalls = [];
-  pools.forEach(pool => {
-    const tokenContract = getContract(ERC20, pool.address);
-    balanceCalls.push({
-      balance: tokenContract.methods.balanceOf(pool.strat ?? masterchef),
-    });
-    allocPointCalls.push({
-      allocPoint: masterchefContract.methods.poolInfo(pool.poolId),
-    });
+const getRipaeApys = async () =>
+  await getMasterChefApys({
+    web3: web3,
+    chainId: chainId,
+    masterchef: '0x4d1D896FD501788d8605f672AD72fC05Fe5ab311',
+    tokenPerBlock: 'paePerSecond',
+    hasMultiplier: false,
+    pools: pools,
+    oracleId: 'sETH',
+    oracle: 'tokens',
+    decimals: '1e18',
+    secondsPerBlock: 1,
+    tradingFeeInfoClient: sushiArbitrumClient,
+    liquidityProviderFee: SUSHI_LPF,
+    // log: true,
   });
-
-  const res = await multicall.all([balanceCalls, allocPointCalls]);
-
-  const balances = res[0].map(v => new BigNumber(v.balance));
-  const allocPoints = res[1].map(v => v.allocPoint['1']);
-  return { balances, allocPoints };
-};
 
 module.exports = getRipaeApys;
