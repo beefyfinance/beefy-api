5b2b670849ab3048b2351c040fd495e37307666b
MirthFutures <mirthfutures@gmail.com>
2022-08-03 13:09:28 -0400

    Update feeRecipient on Polygon, Avax and Arbitrum

--- packages/address-book/address-book/polygon/platforms/beefyfinance.ts
+++ packages/address-book/address-book/polygon/platforms/beefyfinance.ts
@@ -11,7 +11,7 @@ export const beefyfinance = {
   launchpoolOwner: devMultisig,
   rewardPool: '0xDeB0a777ba6f59C78c654B8c92F80238c8002DD2',
   treasury: '0x09EF0e7b555599A9F810789FfF68Db8DBF4c51a0',
-  beefyFeeRecipient: '0x6FB16F7F2C1f1BDA14d4277530A92d4404b7Fb7e',
+  beefyFeeRecipient: '0x7313533ed72D2678bFD9393480D0A30f9AC45c1f',
   multicall: '0xC3821F0b56FA4F4794d5d760f94B812DE261361B',
   bifiMaxiStrategy: '0xD126BA764D2fA052Fc14Ae012Aef590Bc6aE0C4f',
   beefyFeeConverterETHtoWMATIC: '0x166Ea67fA3F2257B9bafF28AaF006D33674acA7e',
