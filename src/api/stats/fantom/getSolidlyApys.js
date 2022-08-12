103ce32470359779f060a28095a46316f5bfee92
schnarkus <markus.simianer@yahoo.de>
2022-08-12 10:44:49 +0200

    Remove double breakdown and imports, add USDC-FTM

--- src/api/stats/fantom/getSolidlyApys.js
+++ src/api/stats/fantom/getSolidlyApys.js
@@ -1,48 +1,20 @@
 const { fantomWeb3: web3 } = require('../../../utils/web3');
 const { FANTOM_CHAIN_ID: chainId } = require('../../../constants');
-import { getEDecimals } from '../../../utils/getEDecimals';
 const { getSolidlyGaugeApys } = require('../common/getSolidlyGaugeApys');
-
 const volatilePools = require('../../../data/fantom/solidlyLpPools.json');
 
-import { addressBook } from '../../../../packages/address-book/address-book';
-const {
-  fantom: {
-    tokens: { SOLID },
-  },
-} = addressBook;
-
 const pools = [...volatilePools];
-const getSolidlyApys = async () => {
-  const gaugeApys = getSolidlyGaugeApys({
+const getSolidlyApys = async () =>
+  getSolidlyGaugeApys({
     web3: web3,
     chainId: chainId,
     pools: pools,
     oracleId: 'SOLID',
     oracle: 'tokens',
-    decimals: getEDecimals(SOLID.decimals),
-    reward: SOLID.address,
+    decimals: '1e18',
+    reward: '0x888EF71766ca594DED1F0FA3AE64eD2941740A20',
     boosted: false,
     // log: true,
   });
-
-  let apys = {};
-  let apyBreakdowns = {};
-
-  const results = await Promise.allSettled([gaugeApys]);
-  for (const result of results) {
-    if (result.status !== 'fulfilled') {
-      console.warn('getSolidlyApys error', result.reason);
-    } else {
-      apys = { ...apys, ...result.value.apys };
-      apyBreakdowns = { ...apyBreakdowns, ...result.value.apyBreakdowns };
-    }
-  }
-
-  return {
-    apys,
-    apyBreakdowns,
-  };
-};
 
 module.exports = getSolidlyApys;
