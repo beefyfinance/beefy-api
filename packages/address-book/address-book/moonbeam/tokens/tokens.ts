cf916932c4e7a3dac1066a38b2a2d2b188d54fe4
pumpingGhost <fneustifter@gmail.com>
2022-09-12 17:24:42 +0200

    Add token description for POOP

--- packages/address-book/address-book/moonbeam/tokens/tokens.ts
+++ packages/address-book/address-book/moonbeam/tokens/tokens.ts
@@ -38,7 +38,8 @@ const _tokens = {
     logoURI:
       'https://app.stellaswap.com/_next/image?url=https%3A%2F%2Fraw.githubusercontent.com%2Fstellaswap%2Fassets%2Fmain%2Fbridge%2FPOOP.png&w=64&q=50',
     website: 'https://raresama.com/',
-    description: 'POOP is the Raresama.com governance token.',
+    description:
+      'Raresama is an artist and community focused NFT marketplace with great ambitions to quickly become the leading marketplace on Moonbeam. POOP is the Raresama.com (NFT Marketplace) governance token.',
   },
   xcACA: {
     name: 'Acala xcACA',
