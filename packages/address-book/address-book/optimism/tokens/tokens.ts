e16ae4db739b5e7e3aa6b8e7aced3ae5c392a481
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-09-12 16:46:33 -0400

    Add USDP (USD+) token for Optimism

--- packages/address-book/address-book/optimism/tokens/tokens.ts
+++ packages/address-book/address-book/optimism/tokens/tokens.ts
@@ -398,6 +398,16 @@ const _tokens = {
       'As a Rocket Pool staker, your role is to deposit ETH into the deposit pool which will enable a node operator to create a new Beacon Chain validator. You can stake as little as 0.01 ETH. In doing so, you will be given a token called rETH. rETH represents both how much ETH you deposited, and when you deposited it.',
     logoURI: '',
   },
+  USDP: {
+    name: 'USD+ Stablecoin',
+    symbol: 'USDP',
+    address: '0x73cb180bf0521828d8849bc8CF2B920918e23032',
+    chainId: 10,
+    decimals: 6,
+    website: 'https://overnight.fi/',
+    description: 'USD+ is USDC that pays you yield daily via rebase.',
+    logoURI: '',
+  },
 } as const;
 
 export const tokens: ConstRecord<typeof _tokens, Token> = _tokens;
