94806fbe9583bada6c32e37e612e2179520d5132
chebiN <17909998+seguido@users.noreply.github.com>
2022-10-20 17:46:41 -0300

    add support for jarvis lp breakdown data

--- src/api/stats/matic/getJarvisPrices.ts
+++ src/api/stats/matic/getJarvisPrices.ts
@@ -5,9 +5,20 @@ import rewardPool from '../../../data/matic/jarvisRewardPool.json';
 import { fetchDmmPrices } from '../../../utils/fetchDmmPrices';
 
 const getJarvisPrices = async tokenPrices => {
-  const curvePrices = await getCurvePricesCommon(web3, pools, tokenPrices, false);
-  const dmmPrices = await fetchDmmPrices(rewardPool, curvePrices);
-  return dmmPrices.tokenPrices;
+  const curvePrices = await getCurvePricesCommon(web3, pools, tokenPrices, true);
+  let onlyPrices = Object.keys(curvePrices).reduce((prices, current) => {
+    prices[current] = curvePrices[current].price;
+    return prices;
+  }, {});
+
+  const dmmPrices = await fetchDmmPrices(rewardPool, onlyPrices);
+
+  return {
+    ...dmmPrices.tokenPrices,
+    ...dmmPrices.poolPrices,
+    ...dmmPrices.lpsBreakdown,
+    ...curvePrices,
+  };
 };
 
 export default getJarvisPrices;
