5ea0bf248e185e0ea71a824dba041b7e9548933d
kexley <sircryptomaxi@gmail.com>
2022-10-26 10:12:53 -0400

    Update GMX Apys

--- src/api/stats/arbitrum/getGmxPrices.ts
+++ src/api/stats/arbitrum/getGmxPrices.ts
@@ -1,4 +1,12 @@
+const { arbitrumWeb3: web3 } = require('../../../utils/web3');
+const BigNumber = require('bignumber.js');
 import pools from '../../../data/arbitrum/gmxPools.json';
+import { getContractWithProvider } from '../../../utils/contractHelper';
+const GlpManager = require('../../../abis/arbitrum/GlpManager.json');
+import { MultiCall } from 'eth-multicall';
+import { multicallAddress } from '../../../utils/web3';
+import { ARBITRUM_CHAIN_ID } from '../../../constants';
+import { ERC20_ABI } from '../../../abis/common/ERC20';
 
 const getGmxArbitrumPrices = async tokenPrices => {
   let prices = {};
@@ -14,7 +22,13 @@ const getGmxArbitrumPrices = async tokenPrices => {
 };
 
 const getPrice = async (pool, tokenPrices) => {
-  const price = getTokenPrice(tokenPrices, pool.tokenId);
+  let price = 0;
+  if (pool.oracle == "lps") {
+    price = await getLpPrice(pool);
+  } else {
+    price = getTokenPrice(tokenPrices, pool.tokenId);
+  }
+
   return { [pool.name]: price };
 };
 
@@ -29,5 +43,22 @@ const getTokenPrice = (tokenPrices, oracleId) => {
   }
   return tokenPrice;
 };
+
+const getLpPrice = async (pool) => {
+  const multicall = new MultiCall(web3, multicallAddress(ARBITRUM_CHAIN_ID));
+  const managerCalls = [];
+  const glpCalls = [];
+  const glpManager = getContractWithProvider(GlpManager, pool.glpManager, web3);
+  const glp = getContractWithProvider(ERC20_ABI, pool.address, web3);
+  managerCalls.push({aum: glpManager.methods.getAumInUsdg(true)});
+  glpCalls.push({totalSupply: glp.methods.totalSupply()});
+
+  const res = await multicall.all([managerCalls, glpCalls]);
+
+  const aum = new BigNumber(res[0].map(v => v.aum));
+  const totalSupply = new BigNumber(res[1].map(v => v.totalSupply));
+
+  return aum.dividedBy(totalSupply).toNumber();
+}
 
 export default getGmxArbitrumPrices;
