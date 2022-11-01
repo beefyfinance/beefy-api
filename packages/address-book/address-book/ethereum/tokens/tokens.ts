226c90dfd6d65d8e2acbc94d492eadf41e0ec4aa
MirthFutures <mirthfutures@gmail.com>
2022-11-01 16:24:50 -0400

    ab

--- packages/address-book/address-book/ethereum/tokens/tokens.ts
+++ packages/address-book/address-book/ethereum/tokens/tokens.ts
@@ -8,7 +8,7 @@ const ETH = {
   decimals: 18,
   chainId: 1,
   website: 'https://weth.io/',
-  description: 'Ether or ETH is the native currency built on the Ethereum blockchain.',
+  description: 'Ether or ETH is the native currency built on the Ethereum blockchain. ',
   logoURI: 'https://arbiscan.io/token/images/weth_28.png',
   documentation: 'https://ethereum.org/en/developers/docs/',
 } as const;
