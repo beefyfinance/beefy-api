15da717458a5b96a6b1267a50e96eeb4ff026e07
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-11-05 10:10:26 -0400

    Adding MAI 4pool (#973)
    * Adding MAI 4pool
    
    * Adding token for stella4pool

--- packages/address-book/address-book/moonbeam/platforms/stellaswap.ts
+++ packages/address-book/address-book/moonbeam/platforms/stellaswap.ts
@@ -1,5 +1,6 @@
 export const stellaswap = {
-  router: '0xd0A01ec574D1fC6652eDF79cb2F880fd47D34Ab1', // UniswapV2Router02
+  routerV1: '0xd0A01ec574D1fC6652eDF79cb2F880fd47D34Ab1', // UniswapV2Router02
+  router: '0x70085a09D30D6f8C4ecF6eE10120d1847383BB57', // StellaswapV2Router02
   masterchef: '0xEDFB330F5FA216C9D2039B99C8cE9dA85Ea91c1E',
   masterchefV1distributorV2: '0xF3a5454496E26ac57da879bf3285Fa85DEBF0388',
 } as const;
