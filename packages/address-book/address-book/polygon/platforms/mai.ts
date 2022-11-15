42d3b6e843ea59ad3156abc75ef1557d73d19d46
kexley <sircryptomaxi@gmail.com>
2022-11-15 14:09:17 +0000

    Update Mai Poly Chef

--- packages/address-book/address-book/polygon/platforms/mai.ts
+++ packages/address-book/address-book/polygon/platforms/mai.ts
@@ -1,5 +1,5 @@
 export default {
   chef: '0x574Fe4E8120C4Da1741b5Fd45584de7A5b521F0F',
   chef2: '0x0635AF5ab29Fc7bbA007B8cebAD27b7A3d3D1958',
-  chef3: '0xFFD2AA58Cca3A44120aaA42CEA2852348A9c2eA6',
+  chef3: '0xcC54AfCeCD0d89e0B2db58f5d9e58468E7aD20dc',
 } as const;
