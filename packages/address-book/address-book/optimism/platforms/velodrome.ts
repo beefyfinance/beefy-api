3cb37a6db85ee524cfa43dbd0976537bc84e3275
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2023-01-29 09:42:12 -0500

    Add Velo wstETH-USDC and wstETH-OP Vaults (#1010)
    * add Velo wstETH-USDC and wstETH-OP pools
    
    * correct unirouter for velodrome on optimism
    
    * add vaultProxy for optimism

--- packages/address-book/address-book/optimism/platforms/velodrome.ts
+++ packages/address-book/address-book/optimism/platforms/velodrome.ts
@@ -1,5 +1,5 @@
 export const velodrome = {
-  router: '0xa132DAB612dB5cB9fC9Ac426A0Cc215A3423F9c9',
+  router: '0x9c12939390052919aF3155f41Bf4160Fd3666A6f',
   voter: '0x09236cfF45047DBee6B921e00704bed6D6B8Cf7e',
   bifiOpLp: '0x81f638E5d063618Fc5f6a976E48e9B803b3240C0',
 } as const;
