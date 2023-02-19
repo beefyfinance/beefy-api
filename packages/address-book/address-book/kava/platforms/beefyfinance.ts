0a469588501382c8b41cabd38caac88947073460
MirthFutures <mirthfutures@gmail.com>
2023-02-19 07:41:36 -0500

    Update Kava to multisigs

--- packages/address-book/address-book/kava/platforms/beefyfinance.ts
+++ packages/address-book/address-book/kava/platforms/beefyfinance.ts
@@ -1,17 +1,18 @@
 const zeroAddress = '0x0000000000000000000000000000000000000000';
 const hw = '0xdAec0E93A98b6184816dFDA318B1A01EAF026164';
+const treasuryMultisig = '0x07F29FE11FbC17876D9376E3CD6F2112e81feA6F';
 
 export const beefyfinance = {
-  devMultisig: zeroAddress,
-  treasuryMultisig: zeroAddress,
+  devMultisig: '0x7f1003EBfB807728d255F86eb3D9e0945fb05c69',
+  treasuryMultisig: treasuryMultisig,
   strategyOwner: '0x922f8807E781739DDefEe51df990457B522cBCf5',
   vaultOwner: '0xfcDD5a02C611ba6Fe2802f885281500EC95805d7',
   keeper: '0x4fED5491693007f0CD49f4614FFC38Ab6A04B619',
-  treasurer: hw,
+  treasurer: treasuryMultisig,
   launchpoolOwner: hw,
   rewardPool: '0xFE40f6eAD11099D91D51a945c145CFaD1DD15Bb8',
-  treasury: '0xFE40f6eAD11099D91D51a945c145CFaD1DD15Bb8',
-  beefyFeeRecipient: '0xFE40f6eAD11099D91D51a945c145CFaD1DD15Bb8',
+  treasury: treasuryMultisig,
+  beefyFeeRecipient: treasuryMultisig,
   multicall: '0x13C6bCC2411861A31dcDC2f990ddbe2325482222',
   // bifiMaxiStrategy: '0xd1bAb603eee03fA99A378d90d5d83186fEB81aA9',
   voter: '0x5e1caC103F943Cd84A1E92dAde4145664ebf692A',
