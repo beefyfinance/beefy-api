87731f3221d17677911e5ec1f21409e59a6c3130
kexley <87971154+sircryptomaxi@users.noreply.github.com>
2023-03-18 08:42:04 -0400

    Add Ramses (#1067)
    * Add Ramses
    
    * Update ab
    
    * Fix wrong gauge

---
+++ packages/address-book/address-book/arbitrum/platforms/ramses.ts
@@ -0,0 +1,6 @@
+export const ramses = {
+  router: '0xAAA87963EFeB6f7E0a2711F397663105Acb1805e', // Solidly Router
+  ve: '0xAAA343032aA79eE9a6897Dab03bef967c3289a06',
+  voter: '0xAAA2564DEb34763E3d05162ed3f5C2658691f499',
+  gaugeStaker: '0xfD7284f61027ce97E4777178bF402f2FFB2A6326',
+} as const;
---
+++ src/api/stats/arbitrum/getRamsesApys.js
@@ -0,0 +1,34 @@
+const { arbitrumWeb3: web3 } = require('../../../utils/web3');
+const { ARBITRUM_CHAIN_ID: chainId } = require('../../../constants');
+import { addressBook } from '../../../../packages/address-book/address-book';
+const { getSolidlyGaugeApys } = require('../common/getSolidlyGaugeApys');
+const stablePools = require('../../../data/arbitrum/ramsesStableLpPools.json');
+const volatilePools = require('../../../data/arbitrum/ramsesLpPools.json');
+
+const {
+  arbitrum: {
+    platforms: { ramses },
+    tokens: { RAM },
+  },
+} = addressBook;
+
+const pools = [...stablePools, ...volatilePools];
+const getRamsesApys = async () => {
+  return getSolidlyGaugeApys({
+    web3: web3,
+    chainId: chainId,
+    pools: pools,
+    oracleId: 'RAM',
+    oracle: 'tokens',
+    decimals: '1e18',
+    reward: RAM.address,
+    spirit: false,
+    boosted: true,
+    NFTid: 30,
+    ve: ramses.ve,
+    gaugeStaker: ramses.gaugeStaker,
+    // log: true,
+  });
+};
+
+module.exports = getRamsesApys;
