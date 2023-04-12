af3373a5da89023e58f764f3c63da4fb909d66a7
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2023-04-12 10:53:37 +0100

    Update get stake steak prices

--- src/api/stats/fantom/getStakeSteakPrices.ts
+++ src/api/stats/fantom/getStakeSteakPrices.ts
@@ -1,12 +1,11 @@
 import pools from '../../../data/fantom/stakesteakLpPools.json';
 import { fantomWeb3 as web3 } from '../../../utils/web3';
-import ERC20 from '../../../abis/ERC20.json';
+import StakeSteakPair from '../../../abis/fantom/StakeSteakPair.json';
 import BigNumber from 'bignumber.js';
 import Web3 from 'web3';
 import { MULTICALL_V3 } from '../../../utils/web3Helpers';
 import { ContractCallContext, Multicall, CallReturnContext } from 'ethereum-multicall';
 import { ChainId } from '../../../../packages/address-book/types/chainid';
-import { groupBy } from 'lodash';
 
 type PoolType = typeof pools[number];
 
@@ -36,58 +35,64 @@ async function getPoolsData(
     multicallCustomContractAddress: multicallAddress,
   });
 
-  const calls: Erc20Call[] = pools.reduce((acc: Erc20Call[], pool: PoolType) => {
-    acc.push({
-      reference: `${pool.name}_totalSupply`,
-      type: 'totalSupply',
-      contract: pool.address,
-    });
-    acc.push({
-      reference: `${pool.name}_balanceOf_lp0`,
-      type: 'balanceOf',
-      contract: pool.lp0.address,
-      target: pool.address,
-    });
-    acc.push({
-      reference: `${pool.name}_balanceOf_lp1`,
-      type: 'balanceOf',
-      contract: pool.lp1.address,
-      target: pool.address,
-    });
-    return acc;
-  }, []);
+  const calls: ContractCallContext[] = pools.map(pool => ({
+    reference: pool.name,
+    contractAddress: pool.address,
+    abi: StakeSteakPair,
+    calls: [
+      {
+        reference: `totalSupply`,
+        methodName: 'totalSupply',
+        methodParameters: [],
+      },
+      {
+        reference: `reserve0`,
+        methodName: 'reserve0',
+        methodParameters: [],
+      },
+      {
+        reference: `reserve1`,
+        methodName: 'reserve1',
+        methodParameters: [],
+      },
+    ],
+  }));
 
-  const multicallResults = await erc20Multicall(multicall, calls);
+  const multicallResults = await multicallResultsByReference(multicall, calls);
 
   return pools.reduce((poolData: Record<string, LpBreakdown>, pool: PoolType) => {
     const {
       [`${pool.name}_totalSupply`]: totalSupplyResult,
-      [`${pool.name}_balanceOf_lp0`]: lp0BalanceResult,
-      [`${pool.name}_balanceOf_lp1`]: lp1BalanceResult,
+      [`${pool.name}_reserve0`]: reserves0Result,
+      [`${pool.name}_reserve1`]: reserves1Result,
     } = multicallResults;
 
-    if (!totalSupplyResult.success || !lp0BalanceResult.success || !lp1BalanceResult.success) {
+    if (!totalSupplyResult.success || !reserves0Result.success || !reserves1Result.success) {
       console.error(`Failed fetching data for pool ${pool.name}`);
       return poolData;
     }
 
     const lp0Price = getTokenPrice(tokenPrices, pool.lp0.oracleId);
     const lp1Price = getTokenPrice(tokenPrices, pool.lp1.oracleId);
-    const lp0Bal = new BigNumber(lp0BalanceResult.returnValues[0].hex).dividedBy(pool.lp0.decimals);
-    const lp1Bal = new BigNumber(lp1BalanceResult.returnValues[0].hex).dividedBy(pool.lp1.decimals);
+    const reserves0 = new BigNumber(reserves0Result.returnValues[0].hex).dividedBy(
+      pool.lp0.decimals
+    );
+    const reserves1 = new BigNumber(reserves1Result.returnValues[0].hex).dividedBy(
+      pool.lp1.decimals
+    );
     const totalSupply = new BigNumber(totalSupplyResult.returnValues[0].hex).dividedBy(
       pool.decimals
     );
 
-    const lp0BalInUsd = lp0Bal.times(lp0Price);
-    const lp1BalInUsd = lp1Bal.times(lp1Price);
-    const totalBalInUsd = lp0BalInUsd.plus(lp1BalInUsd);
+    const reserves0InUsd = reserves0.times(lp0Price);
+    const reserves1InUsd = reserves1.times(lp1Price);
+    const totalBalInUsd = reserves0InUsd.plus(reserves1InUsd);
     const poolPrice = totalBalInUsd.dividedBy(totalSupply);
 
     poolData[pool.name] = {
       price: poolPrice.toNumber(),
       tokens: [pool.lp0.address, pool.lp1.address],
-      balances: [lp0Bal.toString(10), lp1Bal.toString(10)],
+      balances: [reserves0.toString(10), reserves1.toString(10)],
       totalSupply: totalSupply.toString(10),
     };
 
@@ -95,57 +100,16 @@ async function getPoolsData(
   }, {});
 }
 
-type TotalSupplyCall = {
-  type: 'totalSupply';
-  reference: string;
-  contract: string;
-};
-
-type BalanceOfCall = {
-  type: 'balanceOf';
-  reference: string;
-  contract: string;
-  target: string;
-};
-
-type Erc20Call = TotalSupplyCall | BalanceOfCall;
-
-async function erc20Multicall(
+async function multicallResultsByReference(
   multicall: Multicall,
-  calls: Erc20Call[]
+  calls: ContractCallContext[]
 ): Promise<Record<string, CallReturnContext>> {
-  const callsByContract = groupBy(calls, 'contract');
-
-  // potential improvement: de-dupe balanceOf calls
-  const contexts: ContractCallContext[] = Object.entries(callsByContract).map(
-    ([contract, calls]) => ({
-      reference: contract,
-      contractAddress: contract,
-      abi: ERC20,
-      calls: calls.map(call => {
-        switch (call.type) {
-          case 'totalSupply':
-            return {
-              reference: call.reference,
-              methodName: 'totalSupply',
-              methodParameters: [],
-            };
-          case 'balanceOf':
-            return {
-              reference: call.reference,
-              methodName: 'balanceOf',
-              methodParameters: [call.target],
-            };
-        }
-      }),
-    })
-  );
-
-  const multicallResult = await multicall.call(contexts);
+  const multicallResult = await multicall.call(calls);
   const results: Record<string, CallReturnContext> = {};
   for (const contractResult of Object.values(multicallResult.results)) {
     for (const callResult of contractResult.callsReturnContext) {
-      results[callResult.reference] = callResult;
+      results[`${contractResult.originalContractCallContext.reference}_${callResult.reference}`] =
+        callResult;
     }
   }
 
