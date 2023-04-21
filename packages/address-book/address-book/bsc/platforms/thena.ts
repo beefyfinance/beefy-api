32df7c49e9f3a472a20bb3b8166260fb4f7aea49
schnarkus <markus.simianer@yahoo.de>
2023-04-21 18:19:51 +0200

    Update Thena gauges

--- packages/address-book/address-book/bsc/platforms/thena.ts
+++ packages/address-book/address-book/bsc/platforms/thena.ts
@@ -1,5 +1,6 @@
 export const thena = {
   router: '0x20a304a7d126758dfe6B243D0fc515F83bCA8431', // Solidly Router
   ve: '0xfBBF371C9B0B994EebFcC977CEf603F7f31c070D',
-  voter: '0x62Ee96e6365ab515Ec647C065c2707d1122d7b26',
+  voter: '0x3A1D0952809F4948d15EBCe8d345962A282C4fCb',
+  oldVoter: '0x62Ee96e6365ab515Ec647C065c2707d1122d7b26',
 } as const;
