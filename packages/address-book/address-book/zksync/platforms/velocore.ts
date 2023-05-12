8d46c09a05ee2594dade2e0c6f286fb7cd13cc8f
shatterproof <4100078+shatterproof@users.noreply.github.com>
2023-05-12 14:51:27 -0700

    Update BIFI Buyback BNB Chain

--- packages/address-book/address-book/zksync/platforms/velocore.ts
+++ packages/address-book/address-book/zksync/platforms/velocore.ts
@@ -1,4 +1,5 @@
 export const velocore = {
   router: '0x46dbd39e26a56778d88507d7aEC6967108C0BD36',
   voter: '0xe09A60FAe6d77658b9767A70e2f361b46Dd3f16A',
+  bifiEthLp: '0x487b6645068E49Ad8cA8c082327DaB1F4Dc2ECbf',
 } as const;
