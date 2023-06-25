a1a994dfba7e15d4a63dc09e5799d706c8f82394
chebiN <17909998+seguido@users.noreply.github.com>
2023-06-25 17:32:59 -0300

    ferro apy migration

--- src/api/stats/cronos/getFerroApys.js
+++ src/api/stats/cronos/getFerroApys.js
@@ -1,18 +1,14 @@
-const { cronosWeb3: web3 } = require('../../../utils/web3');
-const { CRONOS_CHAIN_ID: chainId } = require('../../../constants');
+const { CRONOS_CHAIN_ID: chainId, CRONOS_CHAIN_ID } = require('../../../constants');
 const pools = require('../../../data/cronos/ferroPools.json');
-import { getContractWithProvider } from '../../../utils/contractHelper';
-import { multicallAddress } from '../../../utils/web3';
-import { MultiCall } from 'eth-multicall';
 import getBlockTime from '../../../utils/getBlockTime';
 import fetchPrice from '../../../utils/fetchPrice';
 import getApyBreakdown from '../common/getApyBreakdown';
 import BigNumber from 'bignumber.js';
 import fetch from 'node-fetch';
 import StrategyABI from '../../../abis/StrategyABI';
-
-const chefAbi = require('../../../abis/cronos/FerroFarm.json');
-const ERC20 = require('../../../abis/ERC20.json');
+import FerroFarm from '../../../abis/cronos/FerroFarm';
+import { fetchContract } from '../../rpc/client';
+import ERC20Abi from '../../../abis/ERC20Abi';
 
 const lpFee = 0.0004;
 const burn = 0.4;
@@ -24,8 +20,7 @@ const chef = '0xAB50FB1117778f293cc33aC044b5579fb03029D0';
 const factoryUrl = 'https://api.ferroprotocol.com/info/api/getApys';
 
 const getFerroApys = async () => {
-  const farmApys = await getFarmApys();
-  const tradingApys = await getTradingApys();
+  const [tradingApys, farmApys] = await Promise.all([getTradingApys(), getFarmApys()]);
 
   return getApyBreakdown(pools, tradingApys, farmApys, lpFee);
 };
@@ -61,11 +56,12 @@ const getFarmApys = async () => {
   let apys = [];
 
   const tokenPrice = await fetchPrice({ oracle: 'tokens', id: 'FER' });
-  const { blockRewards, totalAllocPoint, xFerAllocPoint, xFerToFer, xFerBoostTotalSupply } =
-    await getMasterChefData();
-  const { balances, allocPoints, strategyBalances, xFerBoostBalances } = await getPoolsData();
 
-  const secondsPerBlock = await getBlockTime(chainId);
+  const [
+    { blockRewards, totalAllocPoint, xFerAllocPoint, xFerToFer, xFerBoostTotalSupply },
+    { balances, allocPoints, strategyBalances, xFerBoostBalances },
+    secondsPerBlock,
+  ] = await Promise.all([getMasterChefData(), getPoolsData(), getBlockTime(chainId)]);
 
   for (let i = 0; i < pools.length; i++) {
     const pool = pools[i];
@@ -116,66 +112,60 @@ const getFarmApys = async () => {
 };
 
 const getMasterChefData = async () => {
-  const masterchefContract = getContractWithProvider(chefAbi, chef, web3);
-  const xFerContract = getContractWithProvider(ERC20, xFer, web3);
-  const ferContract = getContractWithProvider(ERC20, fer, web3);
-  const xFerBoostContract = getContractWithProvider(ERC20, xFerBoost, web3);
+  const masterchefContract = fetchContract(chef, FerroFarm, CRONOS_CHAIN_ID);
+  const xFerContract = fetchContract(xFer, ERC20Abi, CRONOS_CHAIN_ID);
+  const ferContract = fetchContract(fer, ERC20Abi, CRONOS_CHAIN_ID);
+  const xFerBoostContract = fetchContract(xFerBoost, ERC20Abi, CRONOS_CHAIN_ID);
 
-  const multicall = new MultiCall(web3, multicallAddress(chainId));
-  const chefCalls = [];
-  const xFerCalls = [];
-  const ferCalls = [];
-  const xFerBoostCalls = [];
+  const [
+    blockRewards,
+    totalAllocPoint,
+    xFerAllocPoint,
+    xFerTotalSupply,
+    ferBalance,
+    xFerBoostTotalSupply,
+  ] = await Promise.all([
+    masterchefContract.read.ferPerBlock().then(v => new BigNumber(v.toString())),
+    masterchefContract.read.totalAllocPoint().then(v => new BigNumber(v.toString())),
+    masterchefContract.read.poolInfo([0]).then(v => new BigNumber(v['1'].toString())),
+    xFerContract.read.totalSupply().then(v => new BigNumber(v.toString())),
+    ferContract.read.balanceOf([xFer]).then(v => new BigNumber(v.toString())),
+    xFerBoostContract.read.totalSupply().then(v => new BigNumber(v.toString())),
+  ]);
 
-  chefCalls.push({
-    blockRewards: masterchefContract.methods.ferPerBlock(),
-    totalAllocPoint: masterchefContract.methods.totalAllocPoint(),
-    xFerPoolInfo: masterchefContract.methods.poolInfo(0),
-  });
-  xFerCalls.push({ totalSupply: xFerContract.methods.totalSupply() });
-  ferCalls.push({ balance: ferContract.methods.balanceOf(xFer) });
-  xFerBoostCalls.push({ totalSupply: xFerBoostContract.methods.totalSupply() });
-
-  const res = await multicall.all([chefCalls, xFerCalls, ferCalls, xFerBoostCalls]);
-
-  const blockRewards = new BigNumber(res[0].map(v => v.blockRewards));
-  const totalAllocPoint = new BigNumber(res[0].map(v => v.totalAllocPoint));
-  const xFerAllocPoint = new BigNumber(res[0].map(v => v.xFerPoolInfo['1']));
-
-  const xFerTotalSupply = new BigNumber(res[1].map(v => v.totalSupply));
-  const ferBalance = new BigNumber(res[2].map(v => v.balance));
   const xFerToFer = ferBalance.dividedBy(xFerTotalSupply);
-
-  const xFerBoostTotalSupply = new BigNumber(res[3].map(v => v.totalSupply));
 
   return { blockRewards, totalAllocPoint, xFerAllocPoint, xFerToFer, xFerBoostTotalSupply };
 };
 
 const getPoolsData = async () => {
-  const masterchefContract = getContractWithProvider(chefAbi, chef, web3);
-  const xFerBoostContract = getContractWithProvider(ERC20, xFerBoost, web3);
-  const multicall = new MultiCall(web3, multicallAddress(chainId));
+  const masterchefContract = fetchContract(chef, FerroFarm, CRONOS_CHAIN_ID);
+  const xFerBoostContract = fetchContract(xFerBoost, ERC20Abi, CRONOS_CHAIN_ID);
   const balanceCalls = [];
   const allocPointCalls = [];
   const strategyCalls = [];
   const xFerBoostCalls = [];
 
   pools.forEach(pool => {
-    const tokenContract = getContractWithProvider(ERC20, pool.address, web3);
-    const strategyContract = getContractWithProvider(StrategyABI, pool.strategy, web3);
-
-    balanceCalls.push({ balance: tokenContract.methods.balanceOf(chef) });
-    allocPointCalls.push({ allocPoint: masterchefContract.methods.poolInfo(pool.poolId) });
-    strategyCalls.push({ balance: strategyContract.methods.balanceOfPool() });
-    xFerBoostCalls.push({ balance: xFerBoostContract.methods.balanceOf(pool.strategy) });
+    const tokenContract = fetchContract(pool.address, ERC20Abi, CRONOS_CHAIN_ID);
+    const strategyContract = fetchContract(pool.strategy, StrategyABI, CRONOS_CHAIN_ID);
+    balanceCalls.push(tokenContract.read.balanceOf([chef]));
+    allocPointCalls.push(masterchefContract.read.poolInfo([pool.poolId]));
+    strategyCalls.push(strategyContract.read.balanceOfPool());
+    xFerBoostCalls.push(xFerBoostContract.read.balanceOf([pool.strategy]));
   });
 
-  const res = await multicall.all([balanceCalls, allocPointCalls, strategyCalls, xFerBoostCalls]);
+  const res = await Promise.all([
+    Promise.all(balanceCalls),
+    Promise.all(allocPointCalls),
+    Promise.all(strategyCalls),
+    Promise.all(xFerBoostCalls),
+  ]);
 
-  const balances = res[0].map(v => new BigNumber(v.balance));
-  const allocPoints = res[1].map(v => v.allocPoint['1']);
-  const strategyBalances = res[2].map(v => new BigNumber(v.balance));
-  const xFerBoostBalances = res[3].map(v => new BigNumber(v.balance));
+  const balances = res[0].map(v => new BigNumber(v.toString()));
+  const allocPoints = res[1].map(v => new BigNumber(v['1'].toString()));
+  const strategyBalances = res[2].map(v => new BigNumber(v.toString()));
+  const xFerBoostBalances = res[3].map(v => new BigNumber(v.toString()));
 
   return { balances, allocPoints, strategyBalances, xFerBoostBalances };
 };
