5c07f0deed2c5a63a177d2dad06dd2f7df7f3c3e
chebiN <17909998+seguido@users.noreply.github.com>
2023-06-25 18:03:55 -0300

    ape staking apy

--- src/api/stats/ethereum/getApeStakingApy.js
+++ src/api/stats/ethereum/getApeStakingApy.js
@@ -1,15 +1,15 @@
-import { ethereumWeb3 as web3 } from '../../../utils/web3';
-import { getContractWithProvider } from '../../../utils/contractHelper';
-import IApeStaking from '../../../abis/ethereum/ApeStaking.json';
 import BigNumber from 'bignumber.js';
 import getApyBreakdown from '../common/getApyBreakdown';
+import IApeStaking from '../../../abis/ethereum/ApeStaking';
+import { fetchContract } from '../../rpc/client';
+import { ETH_CHAIN_ID } from '../../../constants';
 
 const staking = '0x5954aB967Bc958940b7EB73ee84797Dc8a2AFbb9';
 
 export const getApeStakingApy = async () => {
-  const apeStaking = getContractWithProvider(IApeStaking, staking, web3);
-  const poolsData = await apeStaking.methods.getPoolsUI().call();
-  const totalSupply = new BigNumber(poolsData[0].stakedAmount);
+  const apeStaking = fetchContract(staking, IApeStaking, ETH_CHAIN_ID);
+  const poolsData = await apeStaking.read.getPoolsUI();
+  const totalSupply = new BigNumber(poolsData[0].stakedAmount.toString());
   const periodFinish = Number(poolsData[0].currentTimeRange.endTimestampHour);
   const rewardsPerHour = new BigNumber(poolsData[0].currentTimeRange.rewardsPerHour);
 
