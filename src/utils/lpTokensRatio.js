72208f1affa00983432b5c9b359bfff61cc06c62
chebiN <17909998+seguido@users.noreply.github.com>
2023-06-25 20:07:39 -0300

    migrate lpTokens code

--- src/utils/lpTokensRatio.js
+++ src/utils/lpTokensRatio.js
@@ -1,16 +1,13 @@
 const BigNumber = require('bignumber.js');
-const { web3Factory } = require('./web3');
-const LPPair = require('../abis/LPPair.json');
-const { getContract } = require('./contractHelper');
+const { default: LPPairABI } = require('../abis/LPPair');
+const { fetchContract } = require('../api/rpc/client');
 
 const lpTokenRatio = async (lpTokenAddress, decimals0, decimals1, chainId = 56) => {
-  const web3 = web3Factory(chainId);
+  const tokenPairContract = fetchContract(lpTokenAddress, LPPairABI, chainId);
 
-  const tokenPairContract = getContract(LPPair, lpTokenAddress, web3);
-
-  let { _reserve0, _reserve1 } = await tokenPairContract.methods.getReserves().call();
-  const reserve0 = new BigNumber(_reserve0);
-  const reserve1 = new BigNumber(_reserve1);
+  const [_reserve0, _reserve1, ...rest] = await tokenPairContract.read.getReserves();
+  const reserve0 = new BigNumber(_reserve0.toString());
+  const reserve1 = new BigNumber(_reserve1.toString());
 
   const ratio = reserve0.div(decimals0).div(reserve1.div(decimals1));
 
