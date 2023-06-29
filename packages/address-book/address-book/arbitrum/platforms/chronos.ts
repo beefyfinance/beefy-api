9edccc74cc9634c7f6855f194a50b01d733ef137
shatterproof <4100078+shatterproof@users.noreply.github.com>
2023-06-29 05:26:55 -0700

    Add Chronos LPs (#1147)
    * Add Chronos LPs
    
    * Add chronos-jeur-ageur
    
    * Remove Chronos LPs
    
    * Remove Chronos sLPs

---
+++ packages/address-book/address-book/arbitrum/platforms/chronos.ts
@@ -0,0 +1,5 @@
+export const chronos = {
+  router: '0xE708aA9E887980750C040a6A2Cb901c37Aa34f3b',
+  voter: '0xC72b5C6D2C33063E89a50B2F77C99193aE6cEe6c',
+  bifiEthLp: '0x04c106eddDBe89f2Ed983f52020F33D126fA544b',
+} as const;
