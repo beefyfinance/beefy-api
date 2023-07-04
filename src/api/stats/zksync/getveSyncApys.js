eb18624b46f9abb109c02f02c0a7c31d3d9bc5f0
shatterproof <4100078+shatterproof@users.noreply.github.com>
2023-07-04 14:05:12 -0700

    Remove vesync-busd-usdc

--- src/api/stats/zksync/getveSyncApys.js
+++ src/api/stats/zksync/getveSyncApys.js
@@ -1,9 +1,7 @@
 const { zksyncWeb3: web3 } = require('../../../utils/web3');
 const { ZKSYNC_CHAIN_ID: chainId } = require('../../../constants');
-import { getEDecimals } from '../../../utils/getEDecimals';
 const { getSolidlyGaugeApys } = require('../common/getSolidlyGaugeApys');
 
-const stablePools = require('../../../data/zksync/veSyncStableLpPools.json');
 const volatilePools = require('../../../data/zksync/veSyncLpPools.json');
 import { addressBook } from '../../../../packages/address-book/address-book';
 const {
@@ -12,37 +10,18 @@ const {
   },
 } = addressBook;
 
-const pools = [...stablePools, ...volatilePools];
-const getveSyncApys = async () => {
-  const gaugeApys = getSolidlyGaugeApys({
+const pools = [...volatilePools];
+const getveSyncApys = async () =>
+  getSolidlyGaugeApys({
     web3: web3,
     chainId: chainId,
     pools: pools,
     oracleId: 'VSzk',
     oracle: 'tokens',
-    decimals: getEDecimals(VS.decimals),
+    decimals: '1e18',
     reward: VS.address,
     boosted: false,
     // log: true,
   });
-
-  let apys = {};
-  let apyBreakdowns = {};
-
-  const results = await Promise.allSettled([gaugeApys]);
-  for (const result of results) {
-    if (result.status !== 'fulfilled') {
-      console.warn('getveSyncApys error', result.reason);
-    } else {
-      apys = { ...apys, ...result.value.apys };
-      apyBreakdowns = { ...apyBreakdowns, ...result.value.apyBreakdowns };
-    }
-  }
-
-  return {
-    apys,
-    apyBreakdowns,
-  };
-};
 
 module.exports = getveSyncApys;
