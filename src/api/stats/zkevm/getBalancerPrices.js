c7ae3424527187480ce3aca05d9c97adf4629422
chebiN <17909998+seguido@users.noreply.github.com>
2023-07-19 17:17:55 -0300

    fix zkevm balancer price call

--- src/api/stats/zkevm/getBalancerPrices.js
+++ src/api/stats/zkevm/getBalancerPrices.js
@@ -1,12 +1,11 @@
 import getBalancerPrices from '../common/balancer/getBalancerPrices';
-import { zkevmWeb3 as web3 } from '../../../utils/web3';
 import { ZKEVM_CHAIN_ID as chainId } from '../../../constants';
 import balancerPools from '../../../data/zkevm/balancerPools.json';
 
 const pools = [...balancerPools];
 
 const getBalancerZkevmPrices = async tokenPrices => {
-  return await getBalancerPrices(web3, chainId, pools, tokenPrices);
+  return await getBalancerPrices(chainId, pools, tokenPrices);
 };
 
 export default getBalancerZkevmPrices;
