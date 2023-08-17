8b58cb28e490ce9aa26a41ccf0fbb91d013033b4
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2023-08-17 12:21:19 -0300

    Add ALB single

--- src/api/stats/base/getAlienBaseApys.js
+++ src/api/stats/base/getAlienBaseApys.js
@@ -9,6 +9,16 @@ const getAlienBaseApys = async () =>
     masterchef: '0x52eaeCAC2402633d98b95213d0b473E069D86590',
     secondsPerBlock: 1,
     pools,
+    singlePools: [
+      {
+        name: 'alienbase-alb',
+        poolId: 0,
+        address: '0x1dd2d631c92b1aCdFCDd51A0F7145A50130050C4',
+        oracle: 'tokens',
+        oracleId: 'ALB',
+        decimals: '1e18',
+      },
+    ],
     oracleId: 'ALB',
     oracle: 'tokens',
     decimals: '1e18',
