3720e5ca9cce7545f11e29ff2333936301c12e4d
RM <roman-monk@users.noreply.github.com>
2023-10-18 15:51:00 +0700

    Add StakeDao

--- src/api/stats/arbitrum/getCurveApys.js
+++ src/api/stats/arbitrum/getCurveApys.js
@@ -2,16 +2,41 @@ import getApyBreakdown from '../common/getApyBreakdown';
 import { getCurveBaseApys } from '../common/curve/getCurveApyData';
 import { getCurveApysCommon } from '../common/curve/getCurveApysCommon';
 import { ARBITRUM_CHAIN_ID } from '../../../constants';
+import { getStakeDaoApyCommon } from '../common/curve/getStakeDaoApyCommon';
 
-const pools = require('../../../data/arbitrum/curvePools.json').filter(p => p.gauge && !p.convex);
+const pools = require('../../../data/arbitrum/curvePools.json');
 const baseApyUrl = 'https://api.curve.fi/api/getSubgraphData/arbitrum';
 const tradingFees = 0.0002;
 
+const stakeDaoPools = [
+  {
+    name: 'stakedao-arb-2crv',
+    oracleId: 'curve-arb-2pool',
+    gauge: '0x044f4954937316db6502638065b95E921Fd28475',
+    rewards: [
+      {
+        token: '0x11cDb42B0EB46D95f990BeDD4695A6e3fA034978',
+        oracleId: 'CRV',
+      },
+      {
+        token: '0x912CE59144191C1204E64559FE8253a0e49E6548',
+        oracleId: 'ARB',
+      },
+    ],
+  },
+];
+
 export const getCurveApys = async () => {
-  const [baseApys, farmApys] = await Promise.all([
+  const curvePools = pools.filter(p => p.gauge && !p.convex);
+
+  const [baseApys, curveApys, sdApys] = await Promise.all([
     await getCurveBaseApys(pools, baseApyUrl),
-    await getCurveApysCommon(ARBITRUM_CHAIN_ID, pools),
+    await getCurveApysCommon(ARBITRUM_CHAIN_ID, curvePools),
+    await getStakeDaoApyCommon(ARBITRUM_CHAIN_ID, stakeDaoPools),
   ]);
-  const poolsMap = pools.map(p => ({ name: p.name, address: p.name }));
-  return getApyBreakdown(poolsMap, baseApys, farmApys, tradingFees);
+  const poolsMap = [
+    ...curvePools.map(p => ({ name: p.name, address: p.name })),
+    ...stakeDaoPools.map(p => ({ name: p.name, address: p.oracleId })),
+  ];
+  return getApyBreakdown(poolsMap, baseApys, [...curveApys, ...sdApys], tradingFees);
 };
---
+++ src/api/stats/common/curve/getStakeDaoApyCommon.js
@@ -0,0 +1,66 @@
+import BigNumber from 'bignumber.js';
+import fetchPrice from '../../../../utils/fetchPrice';
+import ICurveGauge from '../../../../abis/ICurveGauge';
+import { fetchContract } from '../../../rpc/client';
+
+const secondsPerYear = 31536000;
+
+export const getStakeDaoApyCommon = async (chainId, pools) => {
+  const apys = [];
+
+  const totalSupplyCalls = [],
+    extraCalls = [],
+    extraData = [];
+  pools.forEach(pool => {
+    const gauge = fetchContract(pool.gauge, ICurveGauge, chainId);
+    totalSupplyCalls.push(gauge.read.totalSupply());
+    pool.rewards?.forEach(reward => {
+      extraCalls.push(gauge.read.reward_data([reward.token]));
+      extraData.push({ pool: pool.name, token: reward.token });
+    });
+  });
+  const [totalSupplyResults, extraResults] = await Promise.all([
+    Promise.all(totalSupplyCalls),
+    Promise.all(extraCalls),
+  ]);
+
+  const totalSupplies = totalSupplyResults.map(r => new BigNumber(r.toString()));
+
+  const extras = extraResults.map((_, i) => ({
+    ...extraData[i],
+    periodFinish: Number(extraResults[i][2]),
+    rewardRate: new BigNumber(extraResults[i][3].toString()),
+  }));
+
+  for (let i = 0; i < pools.length; i++) {
+    const pool = pools[i];
+    const totalSupply = totalSupplies[i];
+
+    const lpPrice = await fetchPrice({ oracle: 'lps', id: pool.oracleId });
+    const totalStakedInUsd = totalSupply.times(lpPrice);
+
+    let rewardsInUsd = new BigNumber(0);
+    for (const extra of extras.filter(e => e.pool === pool.name)) {
+      if (extra.periodFinish < Date.now() / 1000) continue;
+      const poolExtra = pool.rewards.find(e => e.token === extra.token);
+      const price = await fetchPrice({
+        oracle: poolExtra.oracle ?? 'tokens',
+        id: poolExtra.oracleId,
+      });
+      const extraRewardsInUsd = extra.rewardRate
+        .times(secondsPerYear)
+        .times(price)
+        .times('1e18')
+        .div(poolExtra.decimals || '1e18');
+      rewardsInUsd = rewardsInUsd.plus(extraRewardsInUsd);
+
+      // console.log(pool.name, poolExtra.oracleId, extraRewardsInUsd.div(totalStakedInUsd).valueOf());
+    }
+    const apy = rewardsInUsd.div(totalStakedInUsd);
+    apys.push(apy);
+
+    // console.log(pool.name, apy.valueOf());
+  }
+
+  return apys;
+};
