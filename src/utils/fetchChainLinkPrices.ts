148210cd63f14ffde862953159b98426378896ab
RM <roman-monk@users.noreply.github.com>
2023-11-07 17:06:18 +0700

    Fix BAL heartbeat

--- src/utils/fetchChainLinkPrices.ts
+++ src/utils/fetchChainLinkPrices.ts
@@ -104,7 +104,7 @@ const oracles: Oracle[] = [
     oracleId: 'BAL',
     address: '0xdF2917806E30300537aEB49A7663062F4d1F2b5F',
     chain: 'ethereum',
-    heartbeat: 3600,
+    heartbeat: 86400,
   },
   {
     oracleId: 'FTM',
