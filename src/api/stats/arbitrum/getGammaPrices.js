69a373e7d95ea15b5b05e118f52520503c5133b9
MirthFutures <MirthFutures@gmail.com>
2023-11-27 18:49:24 -0500

    Add Pancake ARB-ETH Gamma

--- src/api/stats/arbitrum/getGammaPrices.js
+++ src/api/stats/arbitrum/getGammaPrices.js
@@ -2,9 +2,10 @@ const getGammaPrices = require('../common/getGammaPrices');
 const uniPools = require('../../../data/arbitrum/uniswapGammaPools.json');
 const uniChefPools = require('../../../data/arbitrum/uniswapGammaChefPools.json');
 const sushiPools = require('../../../data/arbitrum/sushiGammaPools.json');
+const pancakePools = require('../../../data/arbitrum/pancakeGammaPools.json');
 const { ARBITRUM_CHAIN_ID: chainId } = require('../../../constants');
 
-const pools = [...uniPools, ...uniChefPools, ...sushiPools];
+const pools = [...uniPools, ...uniChefPools, ...sushiPools, ...pancakePools];
 const getGammaArbPrices = async tokenPrices => {
   return await getGammaPrices(chainId, pools, tokenPrices);
 };
