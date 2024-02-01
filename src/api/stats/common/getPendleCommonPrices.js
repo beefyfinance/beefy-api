e1f681d9e583a79a70cb88aab5e92046333c399b
RM <roman-monk@users.noreply.github.com>
2024-02-01 17:27:13 +0800

    Fix expired pendle prices

--- src/api/stats/common/getPendleCommonPrices.js
+++ src/api/stats/common/getPendleCommonPrices.js
@@ -9,18 +9,29 @@ const routerStatic = {
   [ETH_CHAIN_ID]: '0x263833d47eA3fA4a30f269323aba6a107f9eB14C',
 };
 const routerAbi = parseAbi([
+  'function isExpired() external view returns (bool)',
+  'function readState(address router) external view returns (uint pt, uint sy, uint lp)',
   'function getLpToAssetRate(address market) external view returns (uint256)',
 ]);
 
 export const getPendleCommonPrices = async (chainId, pools, tokenPrices, lpPrices) => {
   let prices = {};
 
+  const isExpired = await Promise.all(
+    pools.map(pool => fetchContract(pool.address, routerAbi, chainId).read.isExpired())
+  );
   const supplyCalls = pools.map(pool =>
     fetchContract(pool.address, ERC20Abi, chainId).read.totalSupply()
   );
-  const lpRatesCalls = pools.map(pool =>
-    fetchContract(routerStatic[chainId], routerAbi, chainId).read.getLpToAssetRate([pool.address])
-  );
+  const lpRatesCalls = pools.map(async (pool, i) => {
+    const router = routerStatic[chainId];
+    const market = pool.address;
+    if (isExpired[i]) {
+      const [pt, sy, lp] = await fetchContract(market, routerAbi, chainId).read.readState([router]);
+      return new BigNumber(pt).plus(new BigNumber(sy)).times('1e18').div(new BigNumber(lp));
+    }
+    return fetchContract(router, routerAbi, chainId).read.getLpToAssetRate([market]);
+  });
   const [supplyResults, lpRates] = await Promise.all([
     Promise.all(supplyCalls),
     Promise.all(lpRatesCalls),
@@ -35,6 +46,7 @@ export const getPendleCommonPrices = async (chainId, pools, tokenPrices, lpPrice
   for (let i = 0; i < pools.length; i++) {
     const pool = pools[i];
     const lpRate = poolsData[i].lpRate;
+    // console.log(pool.name, 'lpRate', lpRate.div('1e18').valueOf());
     const underlyingPrice = getUnderlyingPrice(pool, tokenPrices, lpPrices);
     const price = lpRate.times(underlyingPrice).div(pool.decimals).toNumber();
     const totalSupply = poolsData[i].totalSupply.div('1e18').toString(10);
