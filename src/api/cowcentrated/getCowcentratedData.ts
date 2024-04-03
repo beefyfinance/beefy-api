f51de0cbc39aeb3a979a998b18d0c957dd253ac9
chebiN <17909998+seguido@users.noreply.github.com>
2024-04-03 15:17:46 -0300

    handle multiple chains + new vault

--- src/api/cowcentrated/getCowcentratedData.ts
+++ src/api/cowcentrated/getCowcentratedData.ts
@@ -1,42 +1,79 @@
 import { getKey, setKey } from '../../utils/cache';
+import optimismPools from '../../data/optimism/beefyCowVaults.json';
+import basePools from '../../data/base/beefyCowVaults.json';
+import arbitrumPools from '../../data/arbitrum/beefyCowVaults.json';
 
 let cowData;
 
 const CACHE_KEY = 'COWCENTRATED_DATA';
 const INIT_DELAY = 5000;
+const REFRESH_DELAY = 60000;
 
-const addressToVaultIdMapping = {
-  ['0x8f6d2f9301305840deb366946a5e9e01279483b3'.toLowerCase()]: 'uniswap-cow-op-susd-usdc',
-  ['0x9ba9232ae8b159558082642bdf77378cfc0306fb'.toLowerCase()]: 'uniswap-cow-op-op-eth',
-  ['0xf01e9f9535e834092f1d3b1712a5dc1b1b92cd07'.toLowerCase()]: 'uniswap-cow-op-moobifi-eth',
+type CLMVault = {
+  address: `0x${string}`;
+  lpAddress: `0x${string}`;
+  tokens: [`0x${string}`, `0x${string}`];
+  tokenOracleIds: [string, string];
+  decimals: [number, number];
+  oracleId: string;
 };
 
-const updateCowcentratedData = async () => {
+const reducePoolsToMap = (pools: CLMVault[]) =>
+  pools.reduce((acc, pool) => ({ ...acc, [pool.address.toLowerCase()]: pool.oracleId }), {});
+
+const subgraphPositionMapping: Record<string, Record<string, string>> = {
+  'https://api.0xgraph.xyz/subgraphs/name/beefyfinance/clm-optimism': reducePoolsToMap(
+    optimismPools as CLMVault[]
+  ),
+  'https://api.0xgraph.xyz/subgraphs/name/beefyfinance/clm-base': reducePoolsToMap(
+    basePools as CLMVault[]
+  ),
+  'https://api.0xgraph.xyz/subgraphs/name/beefyfinance/clm-arbitrum': reducePoolsToMap(
+    arbitrumPools as CLMVault[]
+  ),
+};
+
+const updateChainCowcentratedData = async (
+  subgraphUrl: string,
+  vaultAddressToIdMapping: Record<string, string>
+) => {
   try {
-    const a: any = await fetch('https://api.0xgraph.xyz/subgraphs/name/beefyfinance/clm-optimism', {
+    const response: any = await fetch(subgraphUrl, {
       headers: {
-        accept: 'application/graphql-response+json, application/json, multipart/mixed',
-        'accept-language': 'en-US,en;q=0.9',
         'content-type': 'application/json',
       },
       body: '{"query":"query BeefyCLss {\\n  beefyCLVaults {\\n    id\\n    priceRangeMinUSD\\n    currentPriceOfToken0InToken1\\n    priceRangeMaxUSD\\n    priceRangeMin1\\n    priceRangeMax1\\n  }\\n}","operationName":"BeefyCLss","extensions":{}}',
       method: 'POST',
     }).then(response => response.json());
-    a.data.beefyCLVaults.forEach((vault: any) => {
-      if (addressToVaultIdMapping[vault.id.toLowerCase()]) {
-        cowData[addressToVaultIdMapping[vault.id.toLowerCase()]] = {
+
+    response.data.beefyCLVaults.forEach((vault: any) => {
+      if (vaultAddressToIdMapping[vault.id.toLowerCase()]) {
+        cowData[vaultAddressToIdMapping[vault.id.toLowerCase()]] = {
           currentPrice: vault.currentPriceOfToken0InToken1,
           priceRangeMin: vault.priceRangeMin1,
           priceRangeMax: vault.priceRangeMax1,
         };
       }
     });
+
+    console.log(`> CLM data for ${subgraphUrl} updated`);
+  } catch (err) {
+    console.log(`> CLM data update failed for ${subgraphUrl}: ${err.message}`);
+  }
+};
+
+const updateCowcentratedData = async () => {
+  try {
+    for (const [subgraphUrl, clmVaults] of Object.entries(subgraphPositionMapping)) {
+      updateChainCowcentratedData(subgraphUrl, clmVaults);
+    }
     await saveToRedis();
   } catch (err) {
     console.log(err.message);
   }
 
   console.log('> Cowcentrated data updated');
+  setTimeout(updateCowcentratedData, REFRESH_DELAY);
 };
 
 export const getCowcentratedData = () => cowData;
