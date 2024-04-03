9afe9c9549cf37be29b653815a056ec642f9c5c6
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2024-04-03 14:35:02 +0100

    bump ab

--- packages/address-book/address-book/fraxtal/tokens/tokens.ts
+++ packages/address-book/address-book/fraxtal/tokens/tokens.ts
@@ -8,7 +8,7 @@ const frxETH = {
   oracleId: 'wfrxETH',
   decimals: 18,
   chainId: 252,
-  website: 'https://app.frax.finance/frxeth/mint',
+  website: 'https://app.frax.finance/frxeth/mint/',
   description:
     'frxETH acts as a stablecoin loosely pegged to ETH, so that 1 frxETH always represents 1 ETH and the amount of frxETH in circulation matches the amount of ETH in the Frax ETH system. When ETH is sent to the frxETHMinter, an equivalent amount of frxETH is minted. Holding frxETH on its own is not eligible for staking yield and should be thought of as analogous as holding ETH.',
   bridge: 'fraxtal-canonical',
