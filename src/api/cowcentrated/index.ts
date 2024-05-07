34fe849a3296f977033a72ea80bea24d472827f4
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-05-07 11:55:30 +0100

    Add /cow-merkl-campaigns/:chainId (#1442)

--- src/api/cowcentrated/index.ts
+++ src/api/cowcentrated/index.ts
@@ -1,4 +1,6 @@
 import { getCowcentratedData } from './getCowcentratedData';
+import { getCowMerklCampaignsForChain } from './getMerkleCampaigns';
+import { isApiChain } from '../../utils/chain';
 
 export const getCowcentratedVaultData = ctx => {
   const chainTokens = getCowcentratedData();
@@ -9,4 +11,22 @@ export const getCowcentratedVaultData = ctx => {
     ctx.status = 500;
     ctx.body = 'Not available yet';
   }
+};
+
+export const getCowcentratedMerklCampaigns = ctx => {
+  if (!isApiChain(ctx.params.chainId)) {
+    ctx.status = 404;
+    ctx.body = 'Invalid chain';
+    return;
+  }
+
+  const campaigns = getCowMerklCampaignsForChain(ctx.params.chainId);
+  if (!campaigns) {
+    ctx.status = 500;
+    ctx.body = 'Not available yet';
+    return;
+  }
+
+  ctx.status = 200;
+  ctx.body = campaigns;
 };
