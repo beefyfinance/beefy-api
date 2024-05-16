a2e26c08bb8b12e1e7ac830067b8f0ca8f1d085c
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2024-05-16 16:27:53 +0100

    Add Mode Zap

--- packages/address-book/address-book/mode/platforms/beefyfinance.ts
+++ packages/address-book/address-book/mode/platforms/beefyfinance.ts
@@ -17,4 +17,6 @@ export const beefyfinance = {
   voter: '0x5e1caC103F943Cd84A1E92dAde4145664ebf692A',
   beefyFeeConfig: '0xEE9b6Fb73FDef41a8865A968410C0cD104209D20',
   vaultFactory: '0xBC4a342B0c057501E081484A2d24e576E854F823',
+  zap: '0xf2068e1FE1A80E7f5Ba80D6ABD6e8618aD4E959E',
+  zapTokenManager: '0x19e730Ab391b79F03CFA2ffc2C5e3fc617F0eEfe',
 } as const;
