5e98b826f859e3bff21dfea802ca380fd935ac87
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2024-06-14 13:37:42 +0100

    Switch Hop rewards to ARB

--- src/api/stats/arbitrum/getHopArbPrices.js
+++ src/api/stats/arbitrum/getHopArbPrices.js
@@ -1,9 +1,10 @@
 const getStableSwapPrices = require('../common/getStableSwapPrices');
 const hopPools = require('../../../data/arbitrum/hopPools.json');
 const rplPools = require('../../../data/arbitrum/hopRplPools.json');
+const arbPools = require('../../../data/arbitrum/hopArbPools.json');
 const { ARBITRUM_CHAIN_ID } = require('../../../constants');
 
-const pools = [...hopPools, ...rplPools];
+const pools = [...hopPools, ...rplPools, ...arbPools];
 
 const getHopArbPrices = async tokenPrices => {
   return await getStableSwapPrices(ARBITRUM_CHAIN_ID, pools, tokenPrices);
