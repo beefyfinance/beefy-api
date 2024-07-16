aca6b44770d44fa853b0531431bf3ca8ac77517a
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2024-07-16 10:30:01 -0400

    Add CLM to Mantle ab

--- packages/address-book/src/address-book/mantle/platforms/beefyfinance.ts
+++ packages/address-book/src/address-book/mantle/platforms/beefyfinance.ts
@@ -16,7 +16,18 @@ export const beefyfinance = {
   bifiMaxiStrategy: '0x0000000000000000000000000000000000000000', // Not used
   voter: '0x5e1caC103F943Cd84A1E92dAde4145664ebf692A',
   beefyFeeConfig: '0x8e0B63846ebEFf857EE35eF3BD3A2df9EF7D6456',
-  vaultFactory: '0xfc69704cC3cAac545cC7577009Ea4AA04F1a61Eb', // todo
+  vaultFactory: '0xfc69704cC3cAac545cC7577009Ea4AA04F1a61Eb',
   zap: '0x445d3D1E75824197bd6A7F46434F67D754FA25C3',
   zapTokenManager: '0x71c1203fD77fbB109aA06ef5F07f3CE592E96370',
+
+  /// CLM Contracts
+  clmFactory: '0xfBf758dbe280bD988ba0E13713b40ff8cea8565E',
+  clmStrategyFactory: '0xEDFBeC807304951785b581dB401fDf76b4bAd1b0',
+  clmRewardPoolFactory: '0x0d4d90b8D7E1AA1AAE5B80EEdCD5b431Eb3c739D',
+
+  /// Beefy Swapper Contracts
+  beefySwapper: '0x9C18deE5290925f596fbEfee2f6745b640f3A4C6',
+  beefyOracle: '0x61129dCBd8944aB2D1C6792428603b0845f60c5B',
+  beefyOracleUniswapV3: '0x992Ccc9D9b8b76310E044660E96171116820F019',
+  beefyOraclePyth: '0x70Ed6174d8425332F7D9AD2d26C86902977307c0',
 } as const;
