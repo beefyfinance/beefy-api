71841b61f35dba52dc76019175ff6015b5fc183b
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-07-18 16:11:45 +0100

    remove debug logging

--- src/api/zap/proxy/kyber.ts
+++ src/api/zap/proxy/kyber.ts
@@ -42,24 +42,18 @@ const getProxiedQuote = async (
 };
 
 export async function proxyKyberSwap(ctx: Koa.Context) {
-  const start = Date.now();
   const chain = ctx.params.chainId;
-  console.log('proxyKyberSwap... ' + chain);
   const requestObject: SwapRequest = ctx.request['body'] as any; // koa-bodyparser adds parsed json to body
   const proxiedSwap = await postProxiedSwap(requestObject, chain);
-  console.log(`proxyKyberSwap took ${(Date.now() - start) / 1000}s`);
   setNoCacheHeaders(ctx);
   ctx.status = proxiedSwap.code;
   ctx.body = isSuccessApiResponse(proxiedSwap) ? proxiedSwap.data : proxiedSwap.message;
 }
 
 export async function proxyKyberQuote(ctx: Koa.Context) {
-  const start = Date.now();
   const chain = ctx.params.chainId;
-  console.log('proxyKyberQuote... ' + chain);
   const requestObject: QuoteRequest = ctx.query as any;
   const proxiedQuote = await getProxiedQuote(requestObject, chain);
-  console.log(`proxyKyberQuote took ${(Date.now() - start) / 1000}s`);
   setNoCacheHeaders(ctx);
   ctx.status = proxiedQuote.code;
   ctx.body = isSuccessApiResponse(proxiedQuote) ? proxiedQuote.data : proxiedQuote.message;
