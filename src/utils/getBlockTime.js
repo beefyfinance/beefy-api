803b3128a5ea61bbc3c0630d129bf2211a0e295e
MirthFutures <MirthFutures@gmail.com>
2024-08-18 13:58:38 +0100

    try catch get block and fix address

--- src/utils/getBlockTime.js
+++ src/utils/getBlockTime.js
@@ -6,25 +6,31 @@ const blockPeriod = 1000n;
 
 let cache = {};
 const getBlockTime = async chainId => {
-  const cacheKey = Math.floor(Date.now() / updateDelay);
+  try {
+    const cacheKey = Math.floor(Date.now() / updateDelay);
 
-  if (cache[chainId]?.hasOwnProperty(cacheKey)) {
-    return cache[chainId][cacheKey];
+    if (cache[chainId]?.hasOwnProperty(cacheKey)) {
+      return cache[chainId][cacheKey];
+    }
+
+    const client = getRPCClient(chainId);
+
+    const latestBlock = await client.getBlock({ blockTag: 'latest' });
+    const fromBlock = await client.getBlock({ blockNumber: latestBlock.number - blockPeriod });
+
+    const blockTimePromise = new BigNumber(
+      Number(latestBlock.timestamp - fromBlock.timestamp) / Number(blockPeriod)
+    );
+
+    cache[chainId] = {
+      [cacheKey]: blockTimePromise,
+    };
+    return blockTimePromise;
+  } catch (err) {
+    console.error(`getBlockTime error on ${chainId}:`);
+    // console.error(err);
+    return new BigNumber(0);
   }
-
-  const client = getRPCClient(chainId);
-
-  const latestBlock = await client.getBlock({ blockTag: 'latest' });
-  const fromBlock = await client.getBlock({ blockNumber: latestBlock.number - blockPeriod });
-
-  const blockTimePromise = new BigNumber(
-    Number(latestBlock.timestamp - fromBlock.timestamp) / Number(blockPeriod)
-  );
-
-  cache[chainId] = {
-    [cacheKey]: blockTimePromise,
-  };
-  return blockTimePromise;
 };
 
 module.exports = getBlockTime;
