fb64b5a0cfb3edb40229959ccf1f3bad2fb257f3
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-09-05 01:05:03 +0100

    address-book: only checksum staged chain tokens and/or platforms

--- packages/address-book/.lintstagedrc.cjs
+++ packages/address-book/.lintstagedrc.cjs
@@ -14,4 +14,52 @@ module.exports = {
     }
     return [withArgs('ts-node', '--transpileOnly', './scripts/checkDuplicates.ts', chains)];
   }),
+  './src/address-book/**/*.ts': ifStaged(stagedFiles => {
+    const changed = stagedFiles.reduce(
+      (acc, file) => {
+        const matches = file.match(
+          /^src\/address-book\/(?<chain>[^/]+)\/((?<type>[^/]+)\/)?(?<file>[^.]+)\.ts$/
+        );
+        if (!matches) {
+          throw new Error('Matched files do not match expected path structure');
+        }
+        if (!matches.groups.type) {
+          acc.all.add(matches.groups.chain);
+        } else if (matches.groups.type === 'tokens') {
+          acc.tokens.add(matches.groups.chain);
+        } else if (matches.groups.type === 'platforms') {
+          acc.platforms.add(matches.groups.chain);
+        }
+        return acc;
+      },
+      { all: new Set(), tokens: new Set(), platforms: new Set() }
+    );
+
+    const all = changed.all.size
+      ? withArgs('ts-node', '--transpileOnly', './scripts/checksum.ts', Array.from(changed.all))
+      : undefined;
+    for (const chain of changed.all) {
+      changed.tokens.delete(chain);
+      changed.platforms.delete(chain);
+    }
+    const tokens = changed.tokens.size
+      ? withArgs(
+          'ts-node',
+          '--transpileOnly',
+          './scripts/checksum.ts',
+          '--tokens',
+          Array.from(changed.tokens)
+        )
+      : undefined;
+    const platforms = changed.platforms.size
+      ? withArgs(
+          'ts-node',
+          '--transpileOnly',
+          './scripts/checksum.ts',
+          '--platforms',
+          Array.from(changed.platforms)
+        )
+      : undefined;
+    return [all, tokens, platforms].filter(v => !!v);
+  }),
 };
