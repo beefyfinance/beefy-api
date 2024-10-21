fcdc9e18877d84d40ceb610b84028665ce5ed11a
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2024-10-21 17:10:14 +0100

    Skip 0x0 address when determining zap fee receiver

--- src/api/zap/fees.ts
+++ src/api/zap/fees.ts
@@ -1,6 +1,7 @@
 import { addressBook } from '../../../packages/address-book/src/address-book';
 import { ApiChain } from '../../utils/chain';
 import { ProviderId } from './swap/providers';
+import { ZERO_ADDRESS } from '../../utils/address';
 
 export type ZapFee = {
   value: number;
@@ -8,18 +9,32 @@ export type ZapFee = {
 };
 
 const DEFAULT_ZAP_FEE = 0.0005;
-export const getZapProviderFee = (provider: ProviderId, chain: ApiChain) => {
-  if (provider === 'odos')
+
+export const getZapProviderFee = (provider: ProviderId, chain: ApiChain): ZapFee => {
+  if (provider === 'odos') {
+    // It's static to the odos code config, we can't make it dynamic
     return {
       value: DEFAULT_ZAP_FEE,
-    }; //It's static to the odos code config, we can't make it dynamic
+    };
+  }
 
   const beefyPlatform = addressBook[chain].platforms.beefyfinance;
   if (!beefyPlatform) {
-    throw new Error('No Beefy Platform found for chain ' + chain);
+    throw new Error(`No Beefy Platform found for chain ${chain}`);
   }
 
-  const receiver = beefyPlatform.treasurySwapper || beefyPlatform.treasuryMultisig || beefyPlatform.treasury;
+  const receiver = [
+    beefyPlatform.treasurySwapper,
+    beefyPlatform.treasuryMultisig,
+    beefyPlatform.treasury,
+  ].find(a => !!a && a !== ZERO_ADDRESS);
+
+  if (!receiver) {
+    throw new Error(
+      `No fee receiver (treasurySwapper, or treasuryMultisig, or treasury) found for ${provider} on ${chain}`
+    );
+  }
+
   return {
     value: DEFAULT_ZAP_FEE,
     receiver: receiver,
