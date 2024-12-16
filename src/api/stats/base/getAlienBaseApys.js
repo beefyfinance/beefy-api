355221f6c8faac4eec68a44677b6afeb491b3e62
RM <roman-monk@users.noreply.github.com>
2024-12-16 17:50:50 +0800

    Add Alienbase trading aprs

--- src/api/stats/base/getAlienBaseApys.js
+++ src/api/stats/base/getAlienBaseApys.js
@@ -1,5 +1,6 @@
 import { BASE_CHAIN_ID as chainId } from '../../../constants';
 import { getMultiRewardMasterChefApys } from '../common/getMultiRewardMasterChefApys';
+import BigNumber from 'bignumber.js';
 
 const lpPools = require('../../../data/base/alienBaseLpPools.json');
 const v3Pools = require('../../../data/base/alienBaseBunniPools.json');
@@ -15,7 +16,24 @@ const getAlienBaseApys = async () =>
     oracle: 'tokens',
     decimals: '1e18',
     liquidityProviderFee: 0.0016,
+    tradingAprs: await getTradingAprs(v3Pools),
     // log: true,
   });
+
+async function getTradingAprs(pools) {
+  let aprs = {};
+  try {
+    const response = await fetch(
+      'https://nri7mhlmac.execute-api.eu-central-1.amazonaws.com/farmsData/aprData'
+    ).then(res => res.json());
+    pools.forEach(p => {
+      const poolData = response.body.find(r => r.pool.toLowerCase() === p.address.toLowerCase());
+      aprs[p.address.toLowerCase()] = new BigNumber(poolData.apyBase).div(100);
+    });
+  } catch (e) {
+    console.error('AlienBase trading aprs error ', e.message);
+  }
+  return aprs;
+}
 
 module.exports = getAlienBaseApys;
