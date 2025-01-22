d3d3e43f85a2d4737e09ff86ba5325c497b41155
RM <roman-monk@users.noreply.github.com>
2025-01-22 20:23:50 +0800

    Fix sUSD price on OP

--- packages/address-book/src/address-book/optimism/tokens/tokens.ts
+++ packages/address-book/src/address-book/optimism/tokens/tokens.ts
@@ -169,7 +169,7 @@ export const tokens = {
   sUSD: {
     name: 'Synth sUSD',
     symbol: 'sUSD',
-    oracleId: 'sUSD',
+    oracleId: 'opsUSD',
     address: '0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9',
     chainId: 10,
     decimals: 18,
