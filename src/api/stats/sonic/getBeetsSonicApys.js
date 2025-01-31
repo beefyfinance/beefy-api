d6a6484b40b49a583c758e75c0763787de14d558
MirthFutures <MirthFutures@gmail.com>
2025-01-31 11:06:57 -0500

    Add BeefyUSDC-scUSD Beets

--- src/api/stats/sonic/getBeetsSonicApys.js
+++ src/api/stats/sonic/getBeetsSonicApys.js
@@ -3,11 +3,12 @@ const { getBalancerApys } = require('../common/balancer/getBalancerApys');
 import { addressBook } from '../../../../packages/address-book/src/address-book';
 
 const pools = require('../../../data/sonic/beetsPools.json');
+const beetsV3Pools = require('../../../data/sonic/beetsV3Pools.json');
 
 const getBeetsSonicApys = async () => {
   return getBalancerApys({
     chainId: chainId,
-    pools: pools,
+    pools: [...pools, ...beetsV3Pools],
     balancerVault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
     // log: true,
   });
--- src/api/stats/sonic/getBeetsSonicPrices.js
+++ src/api/stats/sonic/getBeetsSonicPrices.js
@@ -1,11 +1,14 @@
 import getBalancerPrices from '../common/balancer/getBalancerPrices';
+import getBalancerV3Prices from '../common/balancer/getBalancerV3Prices';
 import { SONIC_CHAIN_ID as chainId } from '../../../constants';
 import beetsPools from '../../../data/sonic/beetsPools.json';
-
-const pools = [...beetsPools];
+import beetsV3Pools from '../../../data/sonic/beetsV3Pools.json';
 
 const getBeetsSonicPrices = async tokenPrices => {
-  return await getBalancerPrices(chainId, pools, tokenPrices);
+  const data = await getBalancerPrices(chainId, beetsPools, tokenPrices);
+  const dataV3 = await getBalancerV3Prices(chainId, beetsV3Pools, tokenPrices);
+
+  return { ...data, ...dataV3 };
 };
 
 export default getBeetsSonicPrices;
