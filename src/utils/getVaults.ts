35cb2eb26beef472303f83ff1288a8746d251f28
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2025-04-08 14:45:39 +0100

    TEMP add beefy-besonic vault

--- src/utils/getVaults.ts
+++ src/utils/getVaults.ts
@@ -18,6 +18,42 @@ export async function getVaults(chainId: ApiChain): Promise<AnyVault[]> {
     throw new Error(`Invalid vaults data for ${endpoint}`);
   }
 
+  // TODO beSonic - for testing, remove once live
+  if (chainId === 'sonic') {
+    const exists = vaults.find(v => v.id === 'beefy-besonic');
+    if (!exists) {
+      vaults.push({
+        id: 'beefy-besonic',
+        name: 'beSonic',
+        type: 'erc4626',
+        subType: 'erc7540:withdraw',
+        token: 'WS',
+        tokenAddress: '0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38',
+        tokenDecimals: 18,
+        earnContractAddress: '0x871A101Dcf22fE4fE37be7B654098c801CBA1c88',
+        earnedToken: 'beS',
+        earnedTokenAddress: '0x871A101Dcf22fE4fE37be7B654098c801CBA1c88',
+        oracle: 'tokens',
+        oracleId: 'WS',
+        status: 'active',
+        createdAt: 1744038202,
+        platformId: 'beefy',
+        assets: ['WS'],
+        migrationIds: [],
+        risks: ['COMPLEXITY_LOW', 'IL_NONE', 'MCAP_MEDIUM', 'AUDIT', 'CONTRACTS_VERIFIED'],
+        strategyTypeId: 'besonic',
+        pointStructureIds: ['sonic-points'],
+        network: 'sonic',
+        zaps: [
+          {
+            strategyId: 'single',
+            disableWithdraw: true,
+          },
+        ],
+      });
+    }
+  }
+
   // Backwards compatibility
   return vaults.map(vault => {
     if ('type' in vault) {
