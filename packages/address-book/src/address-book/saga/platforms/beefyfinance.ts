c31356ac21a19b33a1ea2362783daec7145741e8
MirthFutures <MirthFutures@gmail.com>
2025-05-15 20:04:35 -0400

    update saga contracts

--- packages/address-book/src/address-book/saga/platforms/beefyfinance.ts
+++ packages/address-book/src/address-book/saga/platforms/beefyfinance.ts
@@ -1,38 +1,38 @@
-const devMultisig = '0x04db327e5d9A0c680622E2025B5Be7357fC757f0'; // TODO set correct saga address
-const treasuryMultisig = '0x10E13f11419165beB0F456eC8a230899E4013BBD'; // TODO set correct saga address
+const devMultisig = '0x000000a151650b85742d8c286E09ABa7bE9BDB82';
+const treasuryMultisig = '0xA55e75C4815Ff39eFD76C257857441d9FD99b45b';
 
 export const beefyfinance = {
-  devMultisig, // TODO set correct saga address
-  treasuryMultisig, // TODO set correct saga address
-  strategyOwner: '0x73F97432Adb2a1c39d0E1a6e554c7d4BbDaFC316', // TODO set correct saga address
-  vaultOwner: '0xC35a456138dE0634357eb47Ba5E74AFE9faE9a98', // TODO set correct saga address
-  keeper: '0x4fED5491693007f0CD49f4614FFC38Ab6A04B619', // TODO set correct saga address
-  treasurer: treasuryMultisig, // TODO set correct saga address
-  launchpoolOwner: devMultisig, // TODO set correct saga address
-  rewardPool: '0x0000000000000000000000000000000000000000', // TODO set correct saga address
-  treasury: '0x0000000000000000000000000000000000000000', // TODO set correct saga address
-  beefyFeeRecipient: '0x02Ae4716B9D5d48Db1445814b0eDE39f5c28264B', // TODO set correct saga address
-  multicall: '0x0D17eE2ed4e67b626B43C3695586E5aE6a039949', // TODO set correct saga address
-  bifiMaxiStrategy: '0x0000000000000000000000000000000000000000', // TODO set correct saga address
-  voter: '0x5e1caC103F943Cd84A1E92dAde4145664ebf692A', // TODO set correct saga address
-  beefyFeeConfig: '0x2b0C9702A4724f2BFe7922DB92c4082098533c62', // TODO set correct saga address
-  vaultFactory: '0x9818dF1Bdce8D0E79B982e2C3a93ac821b3c17e0', // TODO set correct saga address
-  strategyFactory: '0x8e0B63846ebEFf857EE35eF3BD3A2df9EF7D6456', // TODO set correct saga address
-  wrapperFactory: '0x0000000000000000000000000000000000000000', // TODO set correct saga address
-  zap: '0xe103ab2f922aa1a56EC058AbfDA2CeEa1e95bCd7', // TODO set correct saga address
-  zapTokenManager: '0xd0019D13d2F2eb3BA81bca1650cD45cD2Db6526E', // TODO set correct saga address
-  treasurySwapper: '0x0000000000000000000000000000000000000000', // TODO set correct saga address
+  devMultisig,
+  treasuryMultisig,
+  strategyOwner: '0x09D19184F46A32213DF06b981122e06882B61309',
+  vaultOwner: '0xf2EeC1baC39306C0761c816d1D33cF7C9Ad6C0Fe',
+  keeper: '0x4fED5491693007f0CD49f4614FFC38Ab6A04B619',
+  treasurer: treasuryMultisig,
+  launchpoolOwner: devMultisig,
+  rewardPool: '0x0000000000000000000000000000000000000000',
+  treasury: '0x0000000000000000000000000000000000000000',
+  beefyFeeRecipient: '0x02Ae4716B9D5d48Db1445814b0eDE39f5c28264B',
+  multicall: '0x448a3539a591dE3Fb9D5AAE407471D21d40cD315',
+  bifiMaxiStrategy: '0x0000000000000000000000000000000000000000',
+  voter: '0x5e1caC103F943Cd84A1E92dAde4145664ebf692A',
+  beefyFeeConfig: '0x2ec5d5e9aaf3c3f56eBeF2fC46A5af9e42810b41',
+  vaultFactory: '0xD19ab62F83380908D65E344567378cF104cE46c2',
+  strategyFactory: '0x43d05dB4C288e11096eD1e3e3a749Ef209AF5b2F',
+  wrapperFactory: '0x0000000000000000000000000000000000000000',
+  zap: '0xcA7b72437193665d078aEeaDFFa565E1A2E71c3B',
+  zapTokenManager: '0xFeEFCc6CEB8dB58575f7075Ff91D322037e43808',
+  treasurySwapper: '0x0000000000000000000000000000000000000000',
 
   /// CLM Contracts
-  clmFactory: '0x70127945067E4224d7B6ABfDc6f57e3ea45d5CA4', // TODO set correct saga address
-  clmStrategyFactory: '0x7cac900B2f504047b40F5554F633248f78bD960A', // TODO set correct saga address
-  clmRewardPoolFactory: '0x542Bf9f89c3Ba0edb7aE5EB4Cf582d349fCdC608', // TODO set correct saga address
-  positionMulticall: '0xE6e5732245b3e886DD8897a93D21D29bb652d683', // TODO set correct saga address
+  clmFactory: '0xc26314091EB7a9c75E5536f7f54A8F63e829547D',
+  clmStrategyFactory: '0x5dAEd8dEe03CF685D072414cBc82Bf3933D9ed7f',
+  clmRewardPoolFactory: '0xF08cc9422a43Be7c1d6663DF2890Cf7E36E79307',
+  positionMulticall: '0x354D3d7B61230C88F5f50176d9329d13366FbC28',
 
   /// Beefy Swapper Contracts
-  beefySwapper: '0xBC4a342B0c057501E081484A2d24e576E854F823', // TODO set correct saga address
-  beefyOracle: '0x448a3539a591dE3Fb9D5AAE407471D21d40cD315', // TODO set correct saga address
-  beefyOracleChainlink: '0x07f1ad98b725Af45485646aC431b7757f50C598A', // TODO set correct saga address
-  beefyOracleUniswapV2: '0x2AfB174c22D9eE334895C4e300ab93154d800aA0', // TODO set correct saga address
-  beefyOracleUniswapV3: '0xD19ab62F83380908D65E344567378cF104cE46c2', // TODO set correct saga address
+  beefySwapper: '0xe103ab2f922aa1a56EC058AbfDA2CeEa1e95bCd7',
+  beefyOracle: '0xA5Cd8A60a05571141370D184e255777e5c2d5968',
+  beefyOracleChainlink: '0x542Bf9f89c3Ba0edb7aE5EB4Cf582d349fCdC608',
+  beefyOracleUniswapV2: '0xC2cEE7cf27D2Eda09fEc1743f3953dA77Bf1DA61',
+  beefyOracleUniswapV3: '0x03C2E2e84031d913d45B1F5b5dDC8E50Fcb28652',
 } as const;
--- packages/address-book/src/address-book/saga/tokens/tokens.ts
+++ packages/address-book/src/address-book/saga/tokens/tokens.ts
@@ -1,21 +1,23 @@
 import type { Token } from '../../../types/token.js';
 
 const GAS = {
-  name: 'Wrapped Gas',
-  address: '0xE3dbcD53f4Ce1b06Ab200f4912BD35672e68f1FA', // TODO set correct saga address
-  symbol: 'WGAS',
-  oracleId: 'WGAS',
-  decimals: 18,
+  name: 'USDC',
+  address: '0xfc960C233B8E98e0Cf282e29BDE8d3f105fc24d5',
+  symbol: 'USDC',
+  oracleId: 'USDC',
+  decimals: 6,
   chainId: 5464,
-  website: 'https://www.saga.xyz/',
-  description: 'Meta-token for gas on Saga. Saga is gasless for users.',
-  bridge: 'native',
-  logoURI: '',
-  documentation: 'https://docs.saga.xyz/',
+  website: 'https://www.centre.io/',
+  description:
+    'USDC is a fully collateralized US dollar stablecoin. USDC is issued by regulated financial institutions, backed by fully reserved assets, redeemable on a 1:1 basis for US dollars.',
+  bridge: 'saga-canonical',
+  logoURI: 'https://ftmscan.com/token/images/USDC_32.png',
+  documentation: 'https://www.circle.com/en/usdc-multichain/arbitrum',
 } as const satisfies Token;
 
 export const tokens = {
   GAS,
   WGAS: GAS,
   WNATIVE: GAS,
+  USDC: GAS,
 } as const satisfies Record<string, Token>;
