9beab34e66af3e2828ecc9ae63950b49206ad0f8
MirthFutures <MirthFutures@gmail.com>
2026-01-15 19:56:10 -0500

    update treasury swapper

--- packages/address-book/src/address-book/arbitrum/platforms/beefyfinance.ts
+++ packages/address-book/src/address-book/arbitrum/platforms/beefyfinance.ts
@@ -21,7 +21,7 @@ export const beefyfinance = {
   strategyFactory: '0xeF7746F16e511242e25Ad4FF9732bb5fC35EAB50',
   zap: '0xf49F7bB6F4F50d272A0914a671895c4384696E5A',
   zapTokenManager: '0x3395BDAE49853Bc7Ab9377d2A93f42BC3A18680e',
-  treasurySwapper: '0x8Ccf8606ccf0Aff9937B68e0297967e257eB148b',
+  treasurySwapper: '0xa725268F6c7a6aa544C5E569028f3C64cD484334',
 
   /// CLM Contracts
   clmFactory: '0xD41Ce2c0a0596635FC09BDe2C35946a984b8cB7A',
