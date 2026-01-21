bbd19e18eedb22679f43c92681192950945b237b
kexley <87971154+kexleyBeefy@users.noreply.github.com>
2026-01-21 20:53:24 +0000

    Temp fix Morpho query to 1 hour

--- src/api/stats/common/morpho/getMorphoApys.js
+++ src/api/stats/common/morpho/getMorphoApys.js
@@ -26,7 +26,7 @@ const createGraphQLQuery = (chainId, addresses, isV2) => {
 
   const entityName = isV2 ? 'vaultV2s' : 'vaults';
   const fields = isV2
-    ? 'name address avgNetApy ( lookback: ONE_DAY ) avgApy ( lookback: ONE_DAY ) asset { yield { apr } }'
+    ? 'name address avgNetApy ( lookback: ONE_HOUR ) avgApy ( lookback: ONE_HOUR ) asset { yield { apr } }'
     : 'name address state { netApy netApyWithoutRewards } asset { yield { apr } }';
 
   return {
