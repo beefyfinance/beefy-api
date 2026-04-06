630f1cb2f834e933d2759c1e1da3ba287e6973a7
RM <roman-monk@users.noreply.github.com>
2026-04-06 16:26:37 +0800

    Disable linea ignition

--- src/api/stats/linea/getIgnitionAprs.ts
+++ src/api/stats/linea/getIgnitionAprs.ts
@@ -29,41 +29,4 @@ function isResponseSuccess(response: CampaignInfoResponse): response is Campaign
   return response.err === null;
 }
 
-export const getIgnitionAprs = AsyncCache.wrap(
-  async (protocol: string): Promise<IgnitionAprs> => {
-    try {
-      const result = await getJson<CampaignInfoResponse>({
-        url: 'https://linea-ignition-api.brevis.network/v1/getCampaignInfo',
-        params: {
-          protocol,
-        },
-      });
-
-      if (!isResponseSuccess(result)) {
-        console.error('[Ignition] Error in aprs response', result.err);
-        return {};
-      }
-
-      return result.campaignInfo.reduce((acc, info) => {
-        const match = info.lastWeekApr.match(/^([0-9.]+)%$/);
-        if (!match) {
-          console.warn(`[Ignition] Unexpected APR format: ${info.lastWeekApr} for pool ${info.poolAddr}`);
-          return acc;
-        }
-
-        const apr = parseFloat(match[1]);
-        if (!isFiniteNumber(apr)) {
-          console.warn(`[Ignition] Non-finite APR: ${apr} (${info.lastWeekApr}) for pool ${info.poolAddr}`);
-          return acc;
-        }
-
-        acc[getAddress(info.poolAddr)] = apr / 100;
-        return acc;
-      }, {} as IgnitionAprs);
-    } catch (e) {
-      console.error('[Ignition] Error fetching aprs', e);
-      return {};
-    }
-  },
-  { ttl: 120 }
-);
+export const getIgnitionAprs = (_: string) => ({});
