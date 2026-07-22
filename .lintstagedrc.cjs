e7ca1611c7769f9feb2fd9db8524cb35e24473a2
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2026-07-22 20:26:50 +0100

    ci: lint/typecheck

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,7 +2,7 @@ const ifStaged = fn => stagedFiles => stagedFiles.length === 0 ? [] : fn(stagedF
 
 module.exports = {
   './(src|scripts)/**/*.{ts,js,json}': ifStaged(stagedFiles => [
-    `biome check --reporter concise --staged --no-errors-on-unmatched --diagnostic-level error`,
+    `biome check --reporter concise --staged --fix`,
     `tsc --project tsconfig.json`,
   ]),
   './src/data/**/beefyCowVaults.json': ifStaged(stagedFiles => [
