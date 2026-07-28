2094c87c989a7c9073f030cc47d34ab1bb6fe836
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2026-07-28 19:06:25 +0100

    strict: true

--- src/api/zap/proxy/one-inch.ts
+++ src/api/zap/proxy/one-inch.ts
@@ -1,5 +1,6 @@
 import type Koa from 'koa';
 import type { AnyChain } from '../../../utils/chain.ts';
+import { errorToString } from '../../../utils/error.ts';
 import { getLoggerFor } from '../../../utils/logger/index.ts';
 import { redactSecrets } from '../../../utils/secrets.ts';
 import { type ApiResponse, type ExtraQuoteResponse, isSuccessApiResponse } from '../api/common.ts';
@@ -16,7 +17,7 @@ const getProxiedSwap = async (request: SwapRequest, chain: AnyChain): Promise<Ap
   } catch (err) {
     return {
       code: 500,
-      message: redactSecrets(err.message || 'Unknown error'),
+      message: redactSecrets(errorToString(err)),
     };
   }
 };
@@ -36,7 +37,7 @@ const getProxiedQuote = async (
   } catch (err) {
     return {
       code: 500,
-      message: redactSecrets(err.message || 'Unknown error'),
+      message: redactSecrets(errorToString(err)),
     };
   }
 };
