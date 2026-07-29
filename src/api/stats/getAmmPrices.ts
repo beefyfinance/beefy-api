edf86701b46626109136bda92236f4d6ea08fbf6
RM <roman-monk@users.noreply.github.com>
2026-07-29 14:02:28 +0800

    Remove xSolvBTC price

--- src/api/stats/getAmmPrices.ts
+++ src/api/stats/getAmmPrices.ts
@@ -149,7 +149,6 @@ const coinGeckoCoins: Record<string, string[]> = {
   'stader-ethx': ['ETHx'],
   'liquid-bgt': ['LBGT'],
   'solv-btc': ['SolvBTC'],
-  'solv-protocol-solvbtc-bbn': ['xSolvBTC'],
   'agora-dollar': ['AUSD'],
   'falcon-finance': ['USDf'],
   'ripple-usd': ['RLUSD'],
