87ad0f948dd6870996b07c9ab1473e8503fd11f8
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2026-07-29 09:37:56 +0100

    remove unreferenced apy files

--- src/api/stats/berachain/getBeefyCowBerachainApys.ts
+++
@@ -1,5 +0,0 @@
-import { getCowApys } from '../common/getCowVaultApys.ts';
-
-export const getBeefyCowBerachainApys = async () => {
-  return await getCowApys('berachain');
-};
--- src/api/stats/mantle/getBeefyCowMantleApys.ts
+++
@@ -1,5 +0,0 @@
-import { getCowApys } from '../common/getCowVaultApys.ts';
-
-export const getBeefyCowMantleApys = async () => {
-  return await getCowApys('mantle');
-};
