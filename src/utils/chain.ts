16cd5283871928d404e7c7622d8206027e600479
RM <roman-monk@users.noreply.github.com>
2026-09-22 14:07:01 +0800

    Deprecate moonriver, aurora, fuse, celo, emerald, zkevm

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -7,6 +7,12 @@ export type AppChain = Exclude<ApiChain, 'one'> | 'harmony';
 export type AnyChain = AppChain | ApiChain;
 
 const DEPRECATED_CHAINS = [
+  'moonriver',
+  'aurora',
+  'fuse',
+  'celo',
+  'emerald',
+  'zkevm',
   'rootstock',
   'scroll',
   'heco',
